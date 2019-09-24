a11259210c87dd20a8e7a190b51726fd97949b26
burtonrj <burtonrj@cardiff.ac.uk>
2019-09-24 15:39:43 +0100

    Refactor density and fmo to update child population formation

--- immunova/flow/gating/fmo.py
+++ immunova/flow/gating/fmo.py
@@ -1,4 +1,5 @@
 from immunova.flow.gating.utilities import check_peak, boolean_gate, kde, find_local_minima
+from immunova.flow.gating.density import multidem_density_output
 from immunova.flow.gating.defaults import GateOutput, Geom
 from scipy.signal import find_peaks
 from scipy.stats import norm
@@ -6,16 +7,13 @@ import pandas as pd
 import numpy as np
 
 
-def density_1d_fmo(data: pd.DataFrame, fmo_x: pd.DataFrame, child_name: str,
-                   x: str, bool_gate=False, kde_bw=0.01,
-                   kde_frac=0.5, q=0.99, peak_t=0.01, fmo_z=2):
+def density_1d_fmo(data: pd.DataFrame, fmo_x: pd.DataFrame,
+                   x: str, kde_bw=0.01, kde_frac=0.5, q=0.99, peak_t=0.01, fmo_z=2):
     """
     FMO guided density gating
     :param data:
-    :param fmo:
-    :param child_name:
+    :param fmo_x:
     :param x:
-    :param bool_gate:
     :param kde_bw:
     :param kde_frac:
     :param q:
@@ -55,10 +53,11 @@ def density_1d_fmo(data: pd.DataFrame, fmo_x: pd.DataFrame, child_name: str,
             geom['threshold'] = fmo_threshold
             geom['method'] = 'FMO threshold'
             pos_pop = data[data[x] >= fmo_threshold]
-            pos_pop = boolean_gate(data, pos_pop, bool_gate)
             geom['threshold'] = fmo_threshold
             geom['method'] = 'FMO threshold (absolute)'
-            output.add_child(name=child_name, idx=pos_pop.index.values, geom=geom)
+            output.add_child(name=f'{x}+', idx=pos_pop.index.values, geom=geom)
+            neg_pop = data[~data.index.isin(pos_pop.index.values)]
+            output.add_child(name=f'{x}-', idx=neg_pop.index.values, geom=geom)
             return output
 
         if density['whole']['peaks'].shape[0] > 1:
@@ -84,8 +83,9 @@ def density_1d_fmo(data: pd.DataFrame, fmo_x: pd.DataFrame, child_name: str,
                 geom['threshold'] = ot
                 geom['method'] = 'Local minima; fmo guided'
                 pos_pop = data[data[x] >= ot]
-            pos_pop = boolean_gate(data, pos_pop, bool_gate)
-            output.add_child(name=child_name, idx=pos_pop.index.values, geom=geom)
+            output.add_child(name=f'{x}+', idx=pos_pop.index.values, geom=geom)
+            neg_pop = data[~data.index.isin(pos_pop.index.values)]
+            output.add_child(name=f'{x}-', idx=neg_pop.index.values, geom=geom)
             return output
         else:
             output.error = 1
@@ -96,8 +96,9 @@ def density_1d_fmo(data: pd.DataFrame, fmo_x: pd.DataFrame, child_name: str,
         output.warnings.append(f'No FMO data provided for {x}, density gating calculated from primary data')
         if density['whole']['peaks'].shape[0] == 1:
             pos_pop = data[data[x] >= data[x].quantile(q)]
-            pos_pop = boolean_gate(data, pos_pop, bool_gate)
-            output.add_child(name=child_name, idx=pos_pop.index.values, geom=geom)
+            output.add_child(name=f'{x}+', idx=pos_pop.index.values, geom=geom)
+            neg_pop = data[~data.index.isin(pos_pop.index.values)]
+            output.add_child(name=f'{x}-', idx=neg_pop.index.values, geom=geom)
             return output
         if density['whole']['peaks'].shape[0] > 1:
             # Find the region of minimum density between two highest peaks
@@ -105,8 +106,9 @@ def density_1d_fmo(data: pd.DataFrame, fmo_x: pd.DataFrame, child_name: str,
             geom['threshold'] = whole_threshold
             geom['method'] = 'Local minima from primary data'
             pos_pop = data[data[x] >= whole_threshold]
-            pos_pop = boolean_gate(data, pos_pop, bool_gate)
-            output.add_child(name=child_name, idx=pos_pop.index.values, geom=geom)
+            output.add_child(name=f'{x}+', idx=pos_pop.index.values, geom=geom)
+            neg_pop = data[~data.index.isin(pos_pop.index.values)]
+            output.add_child(name=f'{x}-', idx=neg_pop.index.values, geom=geom)
             return output
         else:
             output.error = 1
@@ -131,99 +133,19 @@ def density_2d_fmo(data, fmo_x, fmo_y, x, y, child_populations: dict,
     """
     output = GateOutput()
     geom = Geom(shape='2d_threshold', x=x, y=y, threshold_x=None, threshold_y=None, method=None)
-    fmo_result_x = density_1d_fmo(data, fmo_x, child_name=f'{x}_fmo', x=x, kde_bw=kde_bw, q=q, peak_t=peak_t)
-    fmo_result_y = density_1d_fmo(data, fmo_y, child_name=f'{y}_fmo', x=y, kde_bw=kde_bw, q=q, peak_t=peak_t)
+    fmo_result_x = density_1d_fmo(data, fmo_x, x=x, kde_bw=kde_bw, q=q, peak_t=peak_t)
+    fmo_result_y = density_1d_fmo(data, fmo_y, x=y, kde_bw=kde_bw, q=q, peak_t=peak_t)
     # Check for errors
-    for x in [fmo_result_x, fmo_result_y]:
-        if x.error == 1:
+    for o in [fmo_result_x, fmo_result_y]:
+        if o.error == 1:
             output.error = 1
             output.error_msg = x.error_msg
             return output
 
     # Update warnings
     output.warnings = fmo_result_x.warnings + fmo_result_y.warnings
-    geom['threshold_x'] = fmo_result_x.child_populations[f'{x}_fmo']['geom']['threshold']
-    geom['threshold_y'] = fmo_result_y.child_populations[f'{y}_fmo']['geom']['threshold']
-    geom['method'] = fmo_result_x.child_populations[f'{x}_fmo']['geom']['method'] + ' ' + \
-                     fmo_result_y.child_populations[f'{y}_fmo']['geom']['method']
-
-    # Name populations
-    x_fmo_idx = fmo_result_x.child_populations[f'{x}_fmo']['index']
-    y_fmo_idx = fmo_result_y.child_populations[f'{y}_fmo']['index']
-    for name, definition in child_populations.items():
-        if definition == '++':
-            pos_idx = np.intersect1d(x_fmo_idx, y_fmo_idx)
-            output.add_child(name=name, idx=pos_idx, geom=geom)
-        elif definition == '--':
-            x_idx = data[~data.index.isin(x_fmo_idx)]
-            y_idx = data[~data.index.isin(y_fmo_idx)]
-            pos_idx = np.intersect1d(x_idx, y_idx)
-        elif definition == '+-':
-            y_idx = data[~data.index.isin(y_fmo_idx)]
-            pos_idx = np.intersect1d(x_fmo_idx, y_idx)
-        elif definition == '-+':
-            x_idx = data[~data.index.isin(x_fmo_idx)]
-            pos_idx = np.intersect1d(x_idx, y_fmo_idx)
-        else:
-            output.error = 1
-            output.error_msg = f'Error: invalid child population definition for {name}, must be one of: ++, +-, -+, --'
-            return output
-        output.add_child(name=name, idx=pos_idx, geom=geom)
-    return output
-
-def density_2d_fmo(data, fmo_x, fmo_y, x, y, child_populations: dict,
-                   kde_bw=0.05, q=0.99, peak_t=0.01):
-    """
-    FMO guided density based gating for two dimensional data
-    :param data:
-    :param fmo_x:
-    :param fmo_y:
-    :param x:
-    :param y:
-    :param child_populations:
-    :param kde_bw:
-    :param q:
-    :param peak_t:
-    :return:
-    """
-    output = GateOutput()
-    geom = Geom(shape='2d_threshold', x=x, y=y, threshold_x=None, threshold_y=None, method=None)
-    fmo_result_x = density_1d_fmo(data, fmo_x, child_name=f'{x}_fmo', x=x, kde_bw=kde_bw, q=q, peak_t=peak_t)
-    fmo_result_y = density_1d_fmo(data, fmo_y, child_name=f'{y}_fmo', x=y, kde_bw=kde_bw, q=q, peak_t=peak_t)
-    # Check for errors
-    for x in [fmo_result_x, fmo_result_y]:
-        if x.error == 1:
-            output.error = 1
-            output.error_msg = x.error_msg
-            return output
-
-    # Update warnings
-    output.warnings = fmo_result_x.warnings + fmo_result_y.warnings
-    geom['threshold_x'] = fmo_result_x.child_populations[f'{x}_fmo']['geom']['threshold']
-    geom['threshold_y'] = fmo_result_y.child_populations[f'{y}_fmo']['geom']['threshold']
-    geom['method'] = fmo_result_x.child_populations[f'{x}_fmo']['geom']['method'] + ' ' + \
-                     fmo_result_y.child_populations[f'{y}_fmo']['geom']['method']
-
-    # Name populations
-    x_fmo_idx = fmo_result_x.child_populations[f'{x}_fmo']['index']
-    y_fmo_idx = fmo_result_y.child_populations[f'{y}_fmo']['index']
-    for name, definition in child_populations.items():
-        if definition == '++':
-            pos_idx = np.intersect1d(x_fmo_idx, y_fmo_idx)
-            output.add_child(name=name, idx=pos_idx, geom=geom)
-        elif definition == '--':
-            x_idx = data[~data.index.isin(x_fmo_idx)].index.values
-            y_idx = data[~data.index.isin(y_fmo_idx)].index.values
-            pos_idx = np.intersect1d(x_idx, y_idx)
-        elif definition == '+-':
-            y_idx = data[~data.index.isin(y_fmo_idx)].index.values
-            pos_idx = np.intersect1d(x_fmo_idx, y_idx)
-        elif definition == '-+':
-            x_idx = data[~data.index.isin(x_fmo_idx)].index.values
-            pos_idx = np.intersect1d(x_idx, y_fmo_idx)
-        else:
-            output.error = 1
-            output.error_msg = f'Error: invalid child population definition for {name}, must be one of: ++, +-, -+, --'
-            return output
-        output.add_child(name=name, idx=pos_idx, geom=geom)
-    return output
+    geom['threshold_x'] = fmo_result_x.child_populations[f'{x}+']['geom']['threshold']
+    geom['threshold_y'] = fmo_result_y.child_populations[f'{y}+']['geom']['threshold']
+    geom['method'] = fmo_result_x.child_populations[f'{x}+']['geom']['method'] + ' ' + \
+                     fmo_result_y.child_populations[f'{y}+']['geom']['method']
+    return multidem_density_output(x, y, fmo_result_x, fmo_result_y, output, geom)
