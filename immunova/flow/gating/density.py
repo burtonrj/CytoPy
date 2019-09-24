5d5518995fc10fc21482185075ae735b4576aecf
burtonrj <burtonrj@cardiff.ac.uk>
2019-09-24 15:39:43 +0100

    Refactor density and fmo to update child population formation
    Former-commit-id: a11259210c87dd20a8e7a190b51726fd97949b26

--- immunova/flow/gating/density.py
+++ immunova/flow/gating/density.py
@@ -6,16 +6,14 @@ import pandas as pd
 import numpy as np
 
 
-def density_gate_1d(data: pd.DataFrame, x: str, child_name: str,
-                    bool_gate=False, q=0.95,
+def density_gate_1d(data: pd.DataFrame, x: str, keep_neg=False, q=0.95,
                     std=None, kde_bw=0.01, kde_sample_frac=0.25,
                     peak_threshold=None, ignore_double_pos=False) -> GateOutput:
     """
     1-Dimensional gating using density based methods
     :param data: pandas dataframe containing compensated and transformed flow cytometry data
     :param x: name of column to gate
-    :param child_name:
-    :param bool_gate: if False, the positive population is returned (>= threshold) else the negative population
+    :param keep_neg: if False, the positive population is returned (>= threshold) else the negative population
     :param q: if only 1 peak is found, quantile gating is performed using this argument as the quantile
     :param std: alternative to quantile gating, the number of standard deviations from the mean can be used to
     determine the threshold
@@ -39,16 +37,23 @@ def density_gate_1d(data: pd.DataFrame, x: str, child_name: str,
     if len(peaks) == 1:
         # If a quantile has been specified, then use quantile gate
         if q:
-            return quantile_gate(data, child_name=child_name, x=x, q=q, bool_gate=bool_gate)
-        if std:
+            threshold = data[x].quantile(q)
+            pos_pop = data[data[x] >= threshold]
+        elif std:
             u = data[x].mean()
             s = data[x].std()
             threshold = u+(s*std)
             pos_pop = data[data[x] >= threshold]
             pos_pop = data[~data.index.isin(pos_pop.index)]
-            geom = Geom(shape='threshold', x=x, y=None, method=f'>= {std} Standard Devs', threshold=threshold)
-            output.add_child(name=child_name, idx=pos_pop.index.values, geom=geom)
+        else:
+            output.error = 1
+            output.error_msg = 'No quantile or standard deviation provided, unable to perform gating'
             return output
+        geom = Geom(shape='threshold', x=x, y=None, method=f'>= {std} Standard Devs', threshold=threshold)
+        output.add_child(name=f'{x}+', idx=pos_pop.index.values, geom=geom)
+        neg_pop = data[~data.index.isin(pos_pop.index.values)]
+        output.add_child(name=f'{x}-', idx=neg_pop.index.values, geom=geom)
+        return output
     if len(peaks) > 1:
         if ignore_double_pos:
             probs_peaks = probs[peaks]
@@ -59,15 +64,16 @@ def density_gate_1d(data: pd.DataFrame, x: str, child_name: str,
         threshold = find_local_minima(probs, xx, peaks)
         geom = Geom(shape='threshold', x=x, y=None, threshold=threshold, method='Local minima; two highest peaks')
         pos_pop = data[data[x] >= threshold]
-        pos_pop = boolean_gate(data, pos_pop, bool_gate)
-        output.add_child(name=child_name, idx=pos_pop.index.values, geom=geom)
+        output.add_child(name=f'{x}+', idx=pos_pop.index.values, geom=geom)
+        neg_pop = data[~data.index.isin(pos_pop.index.values)]
+        output.add_child(name=f'{x}-', idx=neg_pop.index.values, geom=geom)
         return output
     output.error = 1
     output.error_msg = 'No peaks found!'
     return output
 
 
-def density_gate_2d(data, x, y, child_populations: dict, kde_bw=0.05, q=0.99, peak_t=0.01):
+def density_gate_2d(data, x, y, kde_bw=0.05, q=0.99, peak_t=0.01):
         """
         2-dimensional density gating
         :param data:
@@ -83,42 +89,40 @@ def density_gate_2d(data, x, y, child_populations: dict, kde_bw=0.05, q=0.99, pe
         """
         output = GateOutput()
         geom = Geom(shape='2d_threshold', x=x, y=y, threshold_x=None, threshold_y=None, method=None)
-        result_x = density_gate_1d(data, child_name=x, x=x, kde_bw=kde_bw, q=q, peak_threshold=peak_t)
-        result_y = density_gate_1d(data, child_name=y, x=y, kde_bw=kde_bw, q=q, peak_threshold=peak_t)
+        result_x = density_gate_1d(data, x=x, kde_bw=kde_bw, q=q, peak_threshold=peak_t, keep_neg=True)
+        result_y = density_gate_1d(data, x=y, kde_bw=kde_bw, q=q, peak_threshold=peak_t, keep_neg=True)
         # Check for errors
-        for x in [result_x, result_y]:
-            if x.error == 1:
+        for o in [result_x, result_y]:
+            if o.error == 1:
                 output.error = 1
                 output.error_msg = x.error_msg
                 return output
 
         # Update warnings
         output.warnings = result_x.warnings + result_y.warnings
-        geom['threshold_x'] = result_x.child_populations[x]['geom']['threshold']
-        geom['threshold_y'] = result_y.child_populations[y]['geom']['threshold']
-        geom['method'] = result_x.child_populations[x]['geom']['method'] + ' ' + \
-                         result_y.child_populations[y]['geom']['method']
+        geom['threshold_x'] = result_x.child_populations[f'{x}+']['geom']['threshold']
+        geom['threshold_y'] = result_y.child_populations[f'{y}+']['geom']['threshold']
+        geom['method'] = result_x.child_populations[f'{x}+']['geom']['method'] + ' ' + \
+                         result_y.child_populations[f'{y}+']['geom']['method']
+        return multidem_density_output(x, y, result_x, result_y, output, geom)
 
-        # Name populations
-        x_idx = result_x.child_populations[x]['index']
-        y_idx = result_y.child_populations[y]['index']
-        for name, definition in child_populations.items():
-            if definition == '++':
-                pos_idx = np.intersect1d(x_idx, y_idx)
-                output.add_child(name=name, idx=pos_idx, geom=geom)
-            elif definition == '--':
-                x_idx = data[~data.index.isin(x_idx)].index.values
-                y_idx = data[~data.index.isin(y_idx)].index.values
-                pos_idx = np.intersect1d(x_idx, y_idx)
-            elif definition == '+-':
-                y_idx = data[~data.index.isin(y_idx)].index.values
-                pos_idx = np.intersect1d(x_idx, y_idx)
-            elif definition == '-+':
-                x_idx = data[~data.index.isin(x_idx)].index.values
-                pos_idx = np.intersect1d(x_idx, y_idx)
-            else:
-                output.error = 1
-                output.error_msg = f'Error: invalid child population definition for {name}, must be one of: ++, +-, -+, --'
-                return output
-            output.add_child(name=name, idx=pos_idx, geom=geom, merge_options='merge')
-        return output
+
+def multidem_density_output(x: str, y: str, result_x: GateOutput,
+                            result_y: GateOutput, output: GateOutput, geom: Geom):
+    # Name populations
+    x_idx_pos = result_x.child_populations[f'{x}+']['index']
+    y_idx_pos = result_y.child_populations[f'{y}+']['index']
+    x_idx_neg = result_x.child_populations[f'{x}-']['index']
+    y_idx_neg = result_y.child_populations[f'{y}-']['index']
+    for definition in ['++', '--', '+-', '-+']:
+        idx = []
+        if definition == '++':
+            idx = np.intersect1d(x_idx_pos, y_idx_pos)
+        elif definition == '--':
+            idx = np.intersect1d(x_idx_neg, y_idx_neg)
+        elif definition == '+-':
+            idx = np.intersect1d(x_idx_pos, y_idx_neg)
+        elif definition == '-+':
+            idx = np.intersect1d(x_idx_neg, y_idx_pos)
+        output.add_child(name=f'{x}{definition[0]}{y}{definition[1]}', idx=idx, geom=geom)
+    return output
