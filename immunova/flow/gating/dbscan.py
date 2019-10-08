67729b4f7512a957fbfe002bc6e8aa271401e99a
burtonrossj <burtonrj@cardiff.ac.uk>
2019-10-08 17:24:04 +0100

    Completed refactor of dbscan.py and completed documentation; now a class inheriting from base class Gate. This class has methods for dbscan and hdbscan

--- immunova/flow/gating/dbscan.py
+++ immunova/flow/gating/dbscan.py
@@ -1,99 +1,175 @@
-from immunova.flow.gating.defaults import GateOutput, Geom
-from immunova.flow.gating.utilities import density_dependent_downsample
-import numpy as np
+from immunova.flow.gating.base import Gate
+from immunova.flow.gating.defaults import ChildPopulationCollection
 from sklearn.cluster import DBSCAN
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.exceptions import NotFittedError
+import pandas as pd
+import numpy as np
 import collections
+import hdbscan
 
 
-def dbscan_gate(data, x, y, min_pop_size, distance_nn, child_populations, core_only, sample: float = 0.2,
-                sampling_method='uniform', density_sampling_params=None, nn=10):
-    """
-    Cluster based gating using the DBSCAN algorithm
-    :param data: pandas dataframe representing compensated and transformed flow cytometry data
-    :param x:
-    :param y:
-    :param min_pop_size: minimum population size for a population cluster
-    :param distance_nn: nearest neighbour distance (smaller value will create tighter clusters)
-    :param child_populations:
-    :param core_only: if True, only core samples in density clusters will be included
-    :param sample: sample size to perform clustering on (due to computational complexity, N > 50000 is not
-    recommended)
-    :param sampling_method:
-    :param density_sampling_params:
-    :param nn: number of neighbours to use for upsampling with K-nearest neighbours (see documentation for more info)
-    :return: dictionary of gating outputs (see documentation for internal standards)
-    """
-    output = GateOutput()
-    data = data.copy()
-    if data.shape[0] == 0:
-        output.warnings.append('No events in parent population!')
-        for c, _ in child_populations.items():
-            output.add_child(name=c, idx=[], geom=Geom(shape='cluster', x=x, y=y))
-            return output
-    if data.shape[0] < 40000:
-        sampling_method = None
-        s = data[[x, y]]
-    elif sampling_method == 'uniform':
-        s = data.sample(frac=sample)
-    elif sampling_method == 'density':
-        try:
-            if not density_sampling_params:
-                density_sampling_params = dict()
-            s = density_dependent_downsample(data, features=[x, y], **density_sampling_params)
-        except TypeError or KeyError as e:
-            output.error = 1
-            output.error_msg = f'Error: invalid params for density dependent downsampling; {e}'
-            return output
-    else:
-        s = data[[x, y]]
-    db = DBSCAN(eps=distance_nn, min_samples=min_pop_size, algorithm='ball_tree', n_jobs=-1).fit(s[[x, y]])
-    db_labels = db.labels_
+class DensityBasedClustering(Gate):
+    def __init__(self, data: pd.DataFrame, x: str, y: str, child_populations: ChildPopulationCollection,
+                 min_pop_size: int, nn: int, frac: float or None = 0.2, downsample_method: str = 'uniform',
+                 density_downsample_kwargs: dict or None = None):
+        """
+        Constructor for DensityBasedClustering gating object
+        :param data: pandas dataframe representing compensated and transformed flow cytometry data
+        :param x: name of X dimension
+        :param y: name of Y dimension
+        :param child_populations: child populations expected as output (ChildPopulationCollection; see docs for info)
+        :param frac: percentage of events to sample for clustering analysis. If None then all event data is clustered
+        (not recommended when n > 40000)
+        :param min_pop_size: minimum population size for a population cluster
+        :param nn: number of neighbours to use for up-sampling with K-nearest neighbours
+        (see documentation for more info)
+        :param downsample_method: methodology to use for down-sampling prior to clustering (either 'uniform' or
+        'density')
+        :param density_downsample_kwargs: arguments to pass to density dependent down-sampling function (if method
+        is 'uniform' leave value as None)
+        """
+        super(DensityBasedClustering, self).__init__(data=data, x=x, y=y, child_populations=child_populations)
+        self.sample = None
+        self.nn = nn
+        self.min_pop_size = min_pop_size
+        if frac:
+            if downsample_method == 'uniform':
+                self.sample = self.unifrom_downsample(frac)
+            elif downsample_method == 'density':
+                try:
+                    assert density_downsample_kwargs
+                    assert type(density_downsample_kwargs) == dict
+                    features = [self.x, self.y]
+                    self.sample = self.density_dependent_downsample(frac=frac, features=features,
+                                                                    **density_downsample_kwargs)
+                except AssertionError:
+                    print('If apply density dependent down-sampling then a dictionary of keyword arguments is required'
+                          ' as input for density_downsample_kwargs')
 
-    if core_only:
-        non_core_mask = np.ones(len(db_labels), np.bool)
-        non_core_mask[db.core_sample_indices_] = 0
-        np.put(db_labels, non_core_mask, -1)
+    @property
+    def knn_model(self):
+        return KNeighborsClassifier(n_neighbors=self.nn, weights='distance', n_jobs=-1)
 
-    if len(set(db_labels)) == 1:
-        output.warnings.append('Failed to identify any distinct populations')
+    def dbscan(self, distance_nn: int, core_only: bool = False,):
+        """
+        Perform gating with dbscan algorithm
+        :param distance_nn: nearest neighbour distance (smaller value will create tighter clusters)
+        :param core_only: if True, only core samples in density clusters will be included
+        :return: Updated child populations with events indexing complete
+        """
+        # If parent is empty just return the child populations with empty index array
+        if self.empty_parent:
+            return self.child_populations
 
-    if len(set([x for x in db_labels if x != -1])) != len(child_populations):
-        output.warnings.append(f'Expected {len(child_populations)} populations, '
-                               f'identified {len(set([x for x in db_labels if x != -1]))}; {set(db_labels)}')
-
-    # Assign remaining events
-    knn = KNeighborsClassifier(n_neighbors=nn, weights='distance', n_jobs=-1)
-    knn.fit(s[[x, y]], db_labels)
-
-    if sampling_method:
-        data['labels'] = knn.predict(data[[x, y]])
-    else:
-        data['labels'] = db_labels
-    # Predict what cluster the mediod of expected populations falls into
-    populations = collections.defaultdict(list)
-    for name, c in child_populations.items():
-        target = c['target']
-        label = knn.predict(np.reshape(target, (1, -1)))
-        populations[label[0]].append(name)
-    # Check for duplicate assignment of expected population or assignment to noise
-    g = Geom(shape='cluster', x=x, y=y)
-    for label, p_id in populations.items():
-        if len(p_id) > 1:
-            weights = [child_populations[x]['weight'] for x in p_id]
-            m = max(weights)
-            i = [i for i, w in enumerate(weights) if w == m]
-            if len(i) > 1:
-                output.error = 1
-                output.error_msg = f'Populations {[p_id[_] for _ in i]} assigned to the ' \
-                                   f'same cluster and are of equal weighting'
-                return output
-            priority = p_id[i[0]]
-            output.warnings.append(f'Populations f{p_id} assigned to the same cluster {label}; '
-                                   f'prioritising {priority} on weighting.')
-            output.add_child(name=priority, idx=data[data['labels'] == label].index.values, geom=g.as_dict())
-        elif label == -1:
-            output.warnings.append(f'Population {p_id} assigned to noise (i.e. population not found)')
+        # Cluster!
+        model = DBSCAN(eps=distance_nn,
+                        min_samples=self.min_pop_size,
+                        algorithm='ball_tree',
+                        n_jobs=-1)
+        if self.sample is not None:
+            model.fit(self.sample[[self.x, self.y]])
         else:
-            output.add_child(name=p_id[0], idx=data[data['labels'] == label].index.values, geom=g.as_dict())
-    return output
+            model.fit(self.data[[self.x, self.y]])
+        db_labels = model.labels_
+
+        # Post clustering error checking
+        if core_only:
+            non_core_mask = np.ones(len(db_labels), np.bool)
+            non_core_mask[model.core_sample_indices_] = 0
+            np.put(db_labels, non_core_mask, -1)
+        if len(set(db_labels)) == 1:
+            self.warnings.append('Failed to identify any distinct populations')
+        n_children = len(self.child_populations.populations.keys())
+        n_clusters = len(set([x for x in db_labels if x != -1]))
+        if n_clusters != n_children:
+            self.warnings.append(f'Expected {n_children} populations, '
+                                                f'identified {n_clusters}; {set(db_labels)}')
+
+        # Up-sample (if necessary)
+        if self.sample is not None:
+            self.__upsample(db_labels)
+        else:
+            self.data['labels'] = db_labels
+        population_predictions = self.__predict_pop_clusters()
+        return self.__assign_clusters(population_predictions)
+
+    def hdbscan(self, inclusion_threshold: float or None = None):
+        # If parent is empty just return the child populations with empty index array
+        if self.empty_parent:
+            return self.child_populations
+        # Cluster!
+        model = hdbscan.HDBSCAN(core_dist_n_jobs=-1, min_cluster_size=self.min_pop_size, prediction_data=True)
+        if self.sample is not None:
+            model.fit(self.sample[[self.x, self.y]])
+            self.data['label'], self.data['label_strength'] = hdbscan.approximate_predict(model,
+                                                                                          self.data[[self.x, self.y]])
+        else:
+            model.fit(self.data[[self.x, self.y]])
+            self.data['label'] = model.labels_
+            self.data['label_strength'] = model.probabilities_
+        # Post clustering checks
+        if inclusion_threshold is not None:
+            mask = self.data['label_strength'] < inclusion_threshold
+            self.data.loc[mask, 'label'] = -1
+        # Predict clusters for child populations
+        population_predictions = collections.defaultdict(list)
+        for name in self.child_populations.populations.keys():
+            label, _ = hdbscan.approximate_predict(model,
+                                                   [self.child_populations.populations[name].properties['target']])
+            population_predictions[label[0]].append(name)
+        return self.__assign_clusters(population_predictions)
+
+    def __upsample(self, labels):
+        """
+        Internal method. Perform up-sampling from clustering labels
+        :param labels: labels from clustering analysis
+        :return: None
+        """
+        self.knn_model.fit(self.sample[[self.x, self.y]], labels)
+        self.data['labels'] = self.knn_model.predict(self.data)
+
+    def __predict_pop_clusters(self):
+        """
+        Internal method. Using KNN model predict which clusters the expected child populations belong to
+        using the their target mediod
+        :return: predictions {labels: [child population names]}
+        """
+        predictions = collections.defaultdict(list)
+        try:
+            for name in self.child_populations.populations.keys():
+                target = self.child_populations.populations[name].properties['target']
+                label = self.knn_model.predict(np.reshape(target, (1, -1)))
+                predictions[label[0]].append(name)
+            return predictions
+        except NotFittedError:
+            # If sampling was performed the above code will work, otherwise a NotFittedError will be thrown. This is
+            # because self.__upsample() (where fitted occurs) was never called. In this case we want to fit the knn
+            # model to all our data (since we have performed clustering on all of our data in this case). So call
+            # fit method and then call self.__predict_pop_clusters() again
+            self.knn_model.fit(self.data[[self.x, self.y]], self.data['labels'])
+            self.__predict_pop_clusters()
+
+    def __assign_clusters(self, population_predictions):
+        """
+        Internal method. Given child population cluster predictions, update index and geom of child populations
+        :param population_predictions: predicted clustering assignments {label: [population names]}
+        :return: child populations with updated index and geom values
+        """
+        for label, p_id in population_predictions.items():
+            idx = self.data[self.data['labels'] == label].index.values
+            if len(p_id) > 1:
+                # Multiple child populations have been associated to one cluster. Use the child population weightings
+                # to choose priority population
+                weights = [self.child_populations.populations[x].properties['weight'] for x in
+                           self.child_populations.populations.keys()]
+                priority_i = weights.index(max(weights))
+                self.warnings.append(f'Populations f{p_id} assigned to the same cluster {label};'
+                                     f'prioritising {p_id[priority_i]} based on weighting.')
+                self.child_populations.populations[p_id[priority_i]].update_index(idx=idx, merge_options='overwrite')
+            elif label == -1:
+                self.warnings.append(f'Population {p_id} assigned to noise (i.e. population not found)')
+            else:
+                self.child_populations.populations[p_id[0]].update_index(idx=idx, merge_options='overwrite')
+            self.child_populations.populations[p_id[0]].update_geom(shape='cluster', x=self.x, y=self.y)
+        return self.child_populations
