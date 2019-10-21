Corrected missing median of knn calculation in desntiy dependent downsampling
Former-commit-id: 615d727cc27a85c8776237b0d173641066b78f4d [formerly c0130a6955fa7f62fdcec356d7d411fdcba88177] [formerly 675607cf7345cf04be90be383937b662d89a2c1f [formerly 675607cf7345cf04be90be383937b662d89a2c1f [formerly 675607cf7345cf04be90be383937b662d89a2c1f [formerly 312520dd5b6c44ee23dade338930457be526d5d7]]]]
Former-commit-id: 675607cf7345cf04be90be383937b662d89a2c1f
Former-commit-id: 9f68c7c3e944ecef601a891a0a56a23dd46c0392
Former-commit-id: 3eb0d954a16c37398ece225c2d8a8cb59aa35470 [formerly ca21150a9a8cc9208d5a8c3c30ca5409b683a89c]
Former-commit-id: c2b7e2fa43f3eadde1b47d732e9997878398b504

immunova/flow/gating/base.py
@@ -56,21 +56,20 @@ class Gate:
         if self.frac is None:
             return None
         if data.shape[0] < threshold:
-            return None
+            return data
         if self.downsample_method == 'uniform':
             return data.sample(frac=self.frac)
         elif self.downsample_method == 'density':
-            try:
-                assert self.density_downsample_kwargs
-                assert type(self.density_downsample_kwargs) == dict
-                features = [self.x]
-                if self.y is not None:
-                    features.append(self.y)
+            features = [self.x]
+            if self.y is not None:
+                features.append(self.y)
+            if self.density_downsample_kwargs is not None:
+                if not type(self.density_downsample_kwargs) == dict:
+                    raise GateError('If applying density dependent down-sampling then a dictionary of '
+                                    'keyword arguments is required as input for density_downsample_kwargs')
                 return self.density_dependent_downsample(frac=self.frac, features=features,
                                                          **self.density_downsample_kwargs)
-            except AssertionError:
-                print('If apply density dependent down-sampling then a dictionary of keyword arguments is required'
-                      ' as input for density_downsample_kwargs')
+            return self.density_dependent_downsample(frac=self.frac, features=features)
         else:
             GateError('Invalid input, downsample_method must be either `uniform` or `density`')
 
@@ -191,7 +190,7 @@ class Gate:
         mmd_sample = df.sample(mmd_sample_n)
         tree = KDTree(mmd_sample[features], metric='manhattan')
         dist, _ = tree.query(mmd_sample[features], k=2)
-        dist = [x[1] for x in dist]
+        dist = np.median([x[1] for x in dist])
         dist_threshold = dist * alpha
         ld = tree.query_radius(df[features], r=dist_threshold)
         od = np.percentile(ld, q=outlier_dens)

immunova/flow/gating/defaults.py
@@ -1,4 +1,9 @@
 import numpy as np
+import mongoengine
+
+
+class ChildConstructError(Exception):
+    pass
 
 
 class ChildPopulationCollection:
@@ -29,7 +34,6 @@ class ChildPopulationCollection:
                 self.gate_type = gate_type
             except AssertionError:
                 print('Invalid gate type, must be one of: threshold_1d, threshold_2d, cluster, geom')
-
 
     def serialise(self):
         serialised = dict(gate_type=self.gate_type, populations=list())
@@ -62,7 +66,9 @@ class ChildPopulationCollection:
                 to the gate's resulting geom
             """
             self.gate_type = gate_type
-            self.__validate_input(gate_type, **kwargs)
+            x = self.__validate_input(gate_type, **kwargs)
+            if x is False:
+                raise ChildConstructError('Aborting; invalid child population construct')
             self.index = np.array([])
             self.geom = None
 
@@ -91,7 +97,7 @@ class ChildPopulationCollection:
             """
             self.geom = self.Geom(shape, x, y, **kwargs)
 
-        def __validate_input(self, gate_type, **kwargs) -> None:
+        def __validate_input(self, gate_type, **kwargs) -> bool:
             """
             Internal method. Called on init to validate input for child population defintion. If valid, kwargs will
             population the properties attribute of this child population. If invalid an AssertationError will be generated.
@@ -102,23 +108,46 @@ class ChildPopulationCollection:
             """
             try:
                 if gate_type == 'threshold_1d' or gate_type == 'geom':
-                    assert kwargs.keys() == {'definition', 'name'}
-                    assert kwargs['definition'] in ['-', '+']
+                    if not kwargs.keys() == {'definition', 'name'}:
+                        raise ChildConstructError(f'For a threshold_1d gate child population must'
+                                                  f' be defined with keys `definition` and `name`; {kwargs.keys()} '
+                                                  f'provided')
+                    if not kwargs['definition'] in ['-', '+']:
+                        raise ChildConstructError(f'For a threshold_1d `definition` must be one of [+, -]'
+                                                  f' not {kwargs["definition"]}')
                 if gate_type == 'threshold_2d':
-                    assert kwargs.keys() == {'definition', 'name'}
-                    if type(kwargs['definition']) == list:
-                        assert all([x in ['++', '--', '-+', '+-'] for x in kwargs['definition']])
+                    if not kwargs.keys() == {'definition', 'name'}:
+                        raise ChildConstructError(f'For a threshold_2d gate child population must'
+                                                  f' be defined with keys `definition` and `name`; {kwargs.keys()} '
+                                                  f'provided')
+                    if type(kwargs['definition']) == list or \
+                            type(kwargs['definition']) == mongoengine.base.datastructures.BaseList:
+                        for x in kwargs['definition']:
+                            if x not in ['++', '--', '-+', '+-']:
+                                raise ChildConstructError(f'Invalid definition {x}, must be one of '
+                                                          f'[++, --, -+, +-]')
                     else:
-                        assert kwargs['definition'] in ['++', '--', '-+', '-+']
+                        if not kwargs['definition'] in ['++', '--', '-+', '-+']:
+                            raise ChildConstructError(f'Invalid definition {kwargs["definition"]}, must be '
+                                                      f'one of [++, --, -+, +-]')
                 if gate_type == 'cluster':
-                    assert kwargs.keys() == {'target', 'weight', 'name'}
-                    assert len(kwargs['target']) == 2
-                    assert all([isinstance(x, int) or isinstance(x, float) for x in kwargs['target']])
-                    assert isinstance(kwargs['weight'], int)
-                self.properties = kwargs
-            except AssertionError:
-                print(f'Invalid input for child population construction for gate type {gate_type}; '
-                      f'keyword arguments given: {kwargs}')
+                    if not kwargs.keys() == {'target', 'weight', 'name'}:
+                        ChildConstructError(f'For cluster gating child population must contain keys [target, weight, '
+                                            f'name]; {kwargs.keys()} provided')
+                    if len(kwargs['target']) != 2:
+                        ChildConstructError('Invalid target provided for child population, must be a tuple or list '
+                                            f'of length two, not {kwargs["target"]}')
+                    if not all([isinstance(x, int) or isinstance(x, float) for x in kwargs['target']]):
+                        ChildConstructError('Invalid data type for target provided for child population, must be list '
+                                            f'or tuple of floats or integers not {kwargs["target"]}')
+                    if not isinstance(kwargs['weight'], int):
+                        ChildConstructError('Invalid data type provided for weight of child population, must be '
+                                            f'of type Integer not {kwargs["weight"]}')
+            except ChildConstructError as e:
+                print(e)
+                return False
+            self.properties = kwargs
+            return True
 
         class Geom(dict):
             """