5d48b6b20f551cc25a428b239a65c9955add7dae
burtonrossj <burtonrj@cardiff.ac.uk>
2019-11-01 17:35:29 +0000

    Tested mait performance and experimented with phenograph

--- immunova/data/mongo_setup.py
+++ immunova/data/mongo_setup.py
@@ -15,3 +15,11 @@ def test_init():
     :return:
     """
     mongoengine.register_connection(alias='core', name='test_server')
+
+
+def pd_test_init():
+    """
+    MongoDB setup for immunova database
+    :return:
+    """
+    mongoengine.register_connection(alias='core', name='pd_test_server')
