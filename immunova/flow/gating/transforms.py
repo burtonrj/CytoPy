d884ba8209e592184acf6450439a89d3eb151aac
burtonrossj <burtonrj@cardiff.ac.uk>
2019-11-08 11:58:04 +0000

    Moved validation logic for apply_transform to the transform module as opposed to the actions module
    Former-commit-id: c4be01ae6b2795c79c4824917239259f08db0597 [formerly 8b4d38e4c38465d6c2c4270183c82a4c3a083ce4] [formerly e2b6cda44d00a9f24bf193341317400170cb884c [formerly 971f6bad322284bf845f036950212890c3ef8c10]] [formerly dd94c81bc5e8bac1f4472056d72cda4b0a4e3d3e [formerly bb2868170633b5b191b7696dcb6cc54e038d50ec] [formerly 1579e6c2890c493dc48eaded2d6716fd2db84a59 [formerly 3d35941e9f30b157bba3f97b584ecc8ec754e0c9] [formerly 30589c5551365fc40549ac9aa46da494fd9290c0] [formerly bb2868170633b5b191b7696dcb6cc54e038d50ec [formerly bb2868170633b5b191b7696dcb6cc54e038d50ec [formerly 7198eae8fb3eeb55bcf706fe880a1f9e2d15b2d5]]]]]
    Former-commit-id: dd94c81bc5e8bac1f4472056d72cda4b0a4e3d3e [formerly bb2868170633b5b191b7696dcb6cc54e038d50ec]
    Former-commit-id: c1b2e68f93dc9696a0d230260d3ef6df91a36855 [formerly b490bf19dacfef3941a9125373caf74699f66000]
    Former-commit-id: 46e8dddad9c47ab912074b70b3302b68bda2aad8 [formerly bb3c59ef6d65a4a3044d0a911a5e3a20b085c94c [formerly 93b84a457bbcab9ca35a517b3de49e655849d2de]]
    Former-commit-id: bd6cabd3107a66ca85bc71054646905758291628
    Former-commit-id: 8aca80dc83033ed963e3b3787929b3cd0e5a4e87

--- immunova/flow/gating/transforms.py
+++ immunova/flow/gating/transforms.py
@@ -2,8 +2,8 @@ from flowutils.transforms import logicle, hyperlog, log_transform, asinh
 import pandas as pd
 
 
-def apply_transform(data: pd.DataFrame, features_to_transform: list,
-                    transform_method: str, prescale=1) -> pd.DataFrame:
+def apply_transform(data: pd.DataFrame, features_to_transform: list or str = 'all',
+                    transform_method: str = 'logicle', prescale=1) -> pd.DataFrame:
     """
     Apply a transformation to the given dataset; valid transformation methods are:
     logicle, hyperlog, log_transform, or asinh
@@ -13,6 +13,21 @@ def apply_transform(data: pd.DataFrame, features_to_transform: list,
     :param prescale: if using asinh transformaion this value is passed as the scalling argument
     :return: transformed pandas dataframe
     """
+
+    if features_to_transform == 'all':
+        features_to_transform = data.columns
+    elif features_to_transform == 'fluorochromes':
+        features_to_transform = [x for x in data.columns if all([y not in x for y in ['FSC', 'SSC', 'Time']])]
+    elif type(features_to_transform) != list:
+        print('Error: invalid argument provided for `features_to_transform`, expected one of: `all`, `fluorochromes`,'
+              ' or list of valid column names, proceeding with transformation of entire dataframe as precaution.')
+        features_to_transform = data.columns
+    elif not all([x in data.columns for x in features_to_transform]):
+        print('Error: invalid argument provided for `features_to_transform`, list must contain column names that '
+              f'correspond to the provided dataframe. Valid input would be one or several of: {data.columns} '
+              'proceeding with transformation of entire dataframe as precaution.')
+        features_to_transform = data.columns
+
     if transform_method == 'logicle':
         return logicle(data, features_to_transform)
     if transform_method == 'hyperlog':
