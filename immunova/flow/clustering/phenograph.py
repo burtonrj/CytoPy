1f63fc43a401cdcdac465c89ee98877d0d3d730e
burtonrossj <burtonrj@cardiff.ac.uk>
2019-11-08 19:21:37 +0000

    Creating clutering.py
    Former-commit-id: c3e3cfb1a1ef7bd6ae3f4d4730c7eaeb0f1db670 [formerly 2780d65099e3d9f34a26a3224a106a234e21e7c1] [formerly ee5b5563e839ad36e7e14e0fe89a8047afadca36 [formerly 970208e63a09b2bff0ae5553f6c42bbf73dfc84f]] [formerly 947be9c12086cfa151ac8c02dbf2a345d5f14a40 [formerly 7cd5aa7dd6725574ae6ee3f0cbcbddd01e66e170] [formerly 964a44c7f8eea885d00396ad38b159b62f7ea25a [formerly fd52ebfb606610cc8033ba767134a2ff03c9d5bc] [formerly 770b9c21f2b93f4c707a1bb6f55e8f98a21c3a47] [formerly 7cd5aa7dd6725574ae6ee3f0cbcbddd01e66e170 [formerly 7cd5aa7dd6725574ae6ee3f0cbcbddd01e66e170 [formerly 1ef7567e3c7368ecaad08dd952693314bda7812c]]]]]
    Former-commit-id: 947be9c12086cfa151ac8c02dbf2a345d5f14a40 [formerly 7cd5aa7dd6725574ae6ee3f0cbcbddd01e66e170]
    Former-commit-id: 90a741534d47e9244ce2aeaa2039ee80c406ef1c [formerly 19adfcb7b5c44d48664cfa9ced7788ddbf2ad757]
    Former-commit-id: 8ec056bf047971662a232b8ca7f61a537c9d3b77 [formerly f55448e700614f0d31b5ada7e3cbd035824a257f [formerly 0edcf4ce22b634d57d9ed510ef04169e8b534066]]
    Former-commit-id: 9882399b82d2f81095dfe53989839fe90524c7c7
    Former-commit-id: 881c49dbd0d1e9dee89f9def65816a2e2da495b9

--- immunova/flow/clustering/phenograph.py
+++ immunova/flow/clustering/phenograph.py
@@ -1,23 +1,15 @@
+from immunova.flow.clustering.clustering import Clustering, ClusteringError
 import phenograph
-from immunova.flow.gating.actions import Gating
-import umap
-from bokeh.io import curdoc
-from bokeh.layouts import row, column
-from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import PreText, Select
-from bokeh.plotting import figure
 
 
-class PhenoGraph:
-    def __init__(self, exp, k=30, n_jobs=-1):
-        self.experiment = exp
+class PhenoGraph(Clustering):
+    def __init__(self, k=30,  **kwargs):
+        super().__init__(**kwargs)
         self.k = k
-        self.n_jobs=n_jobs
-        self.cluster_cache = dict()
 
     def cluster_sample(self, sample_id, features, root_population):
         if sample_id in self.cluster_cache.keys():
-            return self.cluster_cache[sample_id]
+            return self.cluster_cache
         if sample_id not in self.experiment.list_samples():
             print(f'Invalid sample ID, must be one of: {self.experiment.list_samples()}')
             return None
