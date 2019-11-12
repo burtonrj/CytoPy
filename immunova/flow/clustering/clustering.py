a645a21779d6f7966465e5ad50e2c08d691083e3
burtonrj <burtonrj@cardiff.ac.uk>
2019-11-12 16:29:36 +0000

    Created clustering classes and static plots module
    Former-commit-id: 16a4feec369d0d3b6c1f5df0bce34af1fe63a05c [formerly 37922aff5dcff7c6571a317a606cac6b2eca9b38] [formerly b78feb423447f6b7b5fca317a1da9e157221281d [formerly b78feb423447f6b7b5fca317a1da9e157221281d [formerly 1ae9658e1e303e590a06cda7b2ad2fba52409ca5]]]
    Former-commit-id: b78feb423447f6b7b5fca317a1da9e157221281d
    Former-commit-id: 2c63064b23ffa283ef886155d55bb0b7010fc7af
    Former-commit-id: 0a9bdc96ffe1fc812eb0cc9b177bcf314a86dece

--- immunova/flow/clustering/clustering.py
+++ immunova/flow/clustering/clustering.py
@@ -1,4 +1,6 @@
 from immunova.flow.gating.actions import Gating
+from immunova.flow.clustering.plotting import static, interative
+import phenograph
 
 
 class ClusteringError(Exception):
@@ -7,21 +9,47 @@ class ClusteringError(Exception):
 
 class Clustering:
     def __init__(self, experiment, sample_id, root_population, n_jobs=-1, transform: bool = True,
-                 transform_method: str = 'logicle', transform_features: str or list = 'all'):
-        self.experiment = experiment
+                 transform_method: str = 'logicle', features='all'):
         self.n_jobs = n_jobs
-        self.cluster_cache = dict()
-        g = Gating(experiment, sample_id)
-        self.data = g.get_population_df(root_population,
-                                        transform=transform,
-                                        transform_method=transform_method,
-                                        transform_features=transform_features)
+        self.clusters = dict()
+        self.parameters = dict()
+        self.method = None
+        self.transform_method = transform_method
+        self.root_population = root_population
+        self.sample = Gating(experiment, sample_id)
+        self.data = self.sample.get_population_df(root_population,
+                                                  transform=transform,
+                                                  transform_method=transform_method,
+                                                  transform_features=features)[features]
         if self.data is None:
             raise ClusteringError(f'Error: was unable to retrieve data for {sample_id} invalid root population, '
                                   f'{g.populations.keys()}')
+
+    def add_cluster(self, name, indicies):
+        self.clusters[name] = dict(indicies=indicies,
+                                   n_events=len(indicies),
+                                   prop_of_root=len(indicies)/self.data.shape[0])
+
+    def static_plot_clusters(self, method: str, title: str, save_path: str or None = None, n_components=2, sample_n=100000,
+                             sample_method='uniform', heatmap=None):
+        static.plot_clusters(self.data, self.clusters, method=method,
+                             sample_n=sample_n, sample_method=sample_method,
+                             n_components=n_components, save_path=save_path,
+                             title=title)
+        if heatmap == 'standard':
+            static.heatmap(self.data, self.clusters, title, save_path)
+        if heatmap == 'cluster':
+            static.clustermap(self.data, self.clusters, title, save_path)
+
+
+    def interactive_plots(self):
+        interative.bokeh_heatmap_umap(self.data, self.clusters)
 
     def save(self):
         pass
 
     def load(self):
         pass
+
+gated_populations = self.sample.find_dependencies(self.root_population)
+gated_populations = {pop: node for pop, node in self.sample.populations.items() if pop in gated_populations}
--- immunova/flow/deepcytof/classifier.py
+++ immunova/flow/deepcytof/classifier.py
@@ -4,7 +4,9 @@ import keras.optimizers
 from keras.regularizers import l2
 from keras import callbacks as cb
 from keras.callbacks import LearningRateScheduler
-from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, multilabel_confusion_matrix, \
+    roc_auc_score, label_ranking_average_precision_score, label_ranking_loss
+import pandas as pd
 import numpy as np
 import math
 
@@ -24,7 +26,8 @@ def cell_classifier(train_x, train_y, hidden_layer_sizes, l2_penalty=1e-4,
                     activation='softplus', loss='sparse_categorical_crossentropy',
                     output_activation='softmax'):
     # Expand labels, to work with sparse categorical cross entropy.
-    train_y = np.expand_dims(train_y, -1)
+    if loss == 'sparse_categorical_crossentropy':
+        train_y = np.expand_dims(train_y, -1)
 
     # Construct a feed-forward neural network.
     input_layer = Input(shape=(train_x.shape[1],))
@@ -37,7 +40,6 @@ def cell_classifier(train_x, train_y, hidden_layer_sizes, l2_penalty=1e-4,
     num_classes = len(np.unique(train_y)) - 1
     output_layer = Dense(num_classes, activation=output_activation)(hidden3)
 
-    encoder = Model(input=input_layer, output=output_layer)
     net = Model(input=input_layer, output=output_layer)
     lrate = LearningRateScheduler(step_decay)
     optimizer = keras.optimizers.rmsprop(lr=0.0)
@@ -50,43 +52,41 @@ def cell_classifier(train_x, train_y, hidden_layer_sizes, l2_penalty=1e-4,
     return net
 
 
-def f1score(confusionMatrix):
-    '''
-    Calculate the F1 score of a given confusion matrix.
-    '''
-    col1 = confusionMatrix[1:, :1]
-    confusionMatrix = confusionMatrix[1:, 1:]
-    temp = np.zeros(confusionMatrix.shape)
-
-    for i in range(0, confusionMatrix.shape[0]):
-        for j in range(0, confusionMatrix.shape[1]):
-            if col1[i, 0] > 0:
-                temp[i, j] = np.random.randint(0, col1[i, 0])
-                col1[i, 0] = col1[i, 0] - temp[i, j]
-
-    confusionMatrix = confusionMatrix + temp
-    confusionMatrix = confusionMatrix.astype(int)
-
-    sum_C = np.sum(confusionMatrix, axis=1)  # sum of each row
-    sum_K = np.sum(confusionMatrix, axis=0)  # sum of each column
-
-    Pr = np.divide(confusionMatrix, np.matlib.repmat(np.array([sum_C]).T, 1,
-                                                     confusionMatrix.shape[0]))
-    Re = np.divide(confusionMatrix,
-                   np.matlib.repmat(sum_K, confusionMatrix.shape[1], 1))
-
-    F = np.divide(2 * np.multiply(Pr, Re), Pr + Re)
-
-    for i in range(0, F.shape[0]):
-        for j in range(0, F.shape[1]):
-            if np.isnan(F[i, j]):
-                F[i, j] = 0
-
-    F = np.max(F, axis=1)
-    return np.dot(sum_C, F) / np.sum(sum_C)
+def predict_class(y_probs, threshold):
+    """
+    Returns the predicted class given the probabilities of each class. If threshold = None, the class with
+    the highest probability is returned for each value in y, otherwise assumed to be multi-class prediction
+    and converts output to one-hot-encoded multi-label output using the given threshold.
+    :param y_probs:
+    :param threshold:
+    :return:
+    """
+    def convert_ml(y):
+        if y > threshold:
+            return 1
+        return 0
+    if threshold is not None:
+        y_hat = list()
+        for x in y_probs:
+            y_hat.append(list(map(lambda i: convert_ml(i), x)))
+        return y_hat
+    return list(map(lambda j: np.argmax(j), y_probs))
 
 
-def evaluate_model(classifier, x, y):
-    y_hat = classifier.predict(x)
-    conf_matrix = confusion_matrix(y_true=y, y_pred=y_hat)
+def multi_label_performance(y, y_probs):
+    return pd.DataFrame(dict(ranking_avg_precision=label_ranking_average_precision_score(y_true=y,
+                                                                                         y_score=y_probs),
+                             label_ranking_loss=label_ranking_loss(y_true=y, y_score=y_probs)))
+
+
+def evaluate_model(classifier, x, y, multi_label, threshold=None):
+    y_probs = classifier.predict(x)
+    y_hat = predict_class(y_probs, threshold)
+    if multi_label:
+        return multi_label_performance(y, y_hat)
+    return pd.DataFrame(dict(f1_score=f1_score(y_true=y, y_pred=y_hat, average='weighted'),
+                             accuracy=accuracy_score(y_true=y, y_pred=y_hat),
+                             precision=precision_score(y_true=y, y_pred=y_hat, average='weighted'),
+                             recall=recall_score(y_true=y, y_pred=y_hat, average='weighted'),
+                             auc_score=roc_auc_score(y_true=y, y_score=[x[i] for i, x in zip(y_hat, y_probs)])))
 
