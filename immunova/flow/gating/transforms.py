f3182a2439906eb276daee58967dbca473f4c48a
burtonrossj <burtonrj@cardiff.ac.uk>
2019-11-26 12:33:22 +0000

    Created percetile rank transform function

--- immunova/flow/gating/transforms.py
+++ immunova/flow/gating/transforms.py
@@ -6,6 +6,25 @@ class TransformError(Exception):
     pass
 
 
+def percentile_rank_transform(data: pd.DataFrame, features_to_transform: list) -> pd.DataFrame:
+    """
+    Calculate percentile rank transform of data-frame. Each event is ranked as the average according to the
+    column, then divided by the total number of events and multiplied by 100 to give the percentile.
+    :param data: pandas dataframe of events
+    :param features_to_transform: features to perform transformation on
+    :return: Transformed dataframe
+    """
+    not_transform = [f for f in data.columns if f not in features_to_transform]
+    data_transform = data[features_to_transform]
+    if not_transform:
+        not_transform = data[not_transform]
+    data_transform = data_transform.rank(axis=1, method='average')
+    data_transform = (data_transform/data_transform.shape[0]) * 100
+    if type(not_transform) is not list:
+        return pd.concat([data_transform, not_transform])
+    return data_transform
+
+
 def apply_transform(data: pd.DataFrame, features_to_transform: list or str = 'all',
                     transform_method: str = 'logicle', prescale=1) -> pd.DataFrame:
     """
@@ -40,5 +59,7 @@ def apply_transform(data: pd.DataFrame, features_to_transform: list or str = 'al
         return log_transform(data, features_to_transform)
     if transform_method == 'asinh':
         return asinh(data, features_to_transform, prescale)
+    if transform_method == 'percentile rank':
+        return percentile_rank_transform(data, features_to_transform)
     raise TransformError("Error: invalid transform_method, must be one of: 'logicle', 'hyperlog', 'log_transform',"
                          " 'asinh'")
