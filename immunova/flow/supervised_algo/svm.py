f7da0d2f7c6d7bebe4abcface89ce00f24ec1b75
burtonrossj <burtonrj@cardiff.ac.uk>
2019-11-28 23:00:58 +0000

    Created SVM module
    Former-commit-id: 0c6be33277cebaa650544b60b3b0a04d72a29ef8 [formerly 44e75201f9ba1a2072eebfb34eed25c2bade4b60] [formerly 0ba01497739ca7ab8d33040bdd8d86ca30ab5501] [formerly 62896293577e43373604dbcc7424eb9993569fb3 [formerly 07fd093345edae49a297820c6c9635d54580210a] [formerly 0ba01497739ca7ab8d33040bdd8d86ca30ab5501 [formerly 0ba01497739ca7ab8d33040bdd8d86ca30ab5501 [formerly cfd5aa72506d73f0226520bb93326cf90182914d]]]]
    Former-commit-id: 0fb49cc9c732fd777de6079fa0cebb2c16739b56 [formerly db2bd1484f88447e56dec090a6095d09713f51b4] [formerly c6b413f0bacf2956c957d05cd42d47a665701da1 [formerly dc8303009979a976d8e1c58631a92fc53415fe1a]]
    Former-commit-id: ba511b5d3356202d9dbb365d6909429bc6750a24 [formerly c6b413f0bacf2956c957d05cd42d47a665701da1]
    Former-commit-id: 240d3c217f5265c86147c1fd34bf38795da4853c
    Former-commit-id: c43660a282bec01091f93fe56da31c878229cbc7

--- immunova/flow/supervised_algo/svm.py
+++ immunova/flow/supervised_algo/svm.py
@@ -1,10 +1,27 @@
+from immunova.flow.supervised_algo.cell_classifier import CellClassifier, CellClassifierError
 from sklearn.svm import LinearSVC, SVC
 
 
-class SupportVectorMachine:
+class SupportVectorMachine(CellClassifier):
     """
-    Implement a Support Vector Machine for the purpose of predicting cell classification in FCS data
+    Implement a Support Vector Machine for the purpose of predicting cell classification in flow cytometry data
     """
-    def __init__(self):
-        pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def build_linear(self, **kwargs):
+        if self.class_weights is None:
+            self.classifier = LinearSVC(**kwargs)
+        else:
+            self.classifier = LinearSVC(class_weight=self.class_weights, **kwargs)
+
+    def build_nonlinear(self, kernel: str = 'poly', cache_size: float = 4000, **kwargs):
+        if kernel not in ['poly', 'rbf', 'sigmoid']:
+            raise CellClassifierError("Error: unsupported kernel type, must be one of 'poly', 'rbf', or 'sigmoid'")
+        if self.class_weights is None:
+            self.classifier = SVC(kernel=kernel, cache_size=cache_size, **kwargs)
+        else:
+            self.classifier = SVC(kernel=kernel, cache_size=cache_size,
+                                  class_weight=self.class_weights, **kwargs)
+
 
