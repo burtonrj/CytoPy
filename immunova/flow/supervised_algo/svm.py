3c911a302078846b9fcc53ebcba598ff0f90db1b
burtonrossj <burtonrj@cardiff.ac.uk>
2019-11-28 21:38:20 +0000

    Created functions for sampling to handle unbalanced datasets
    Former-commit-id: 7b4a9b9c0e8cdf0063f78b5c1e8fdcfd9d8bce81 [formerly 82af2703fec7ad3e87eb8100434ddaf42a8764ec] [formerly b8506e9e81fe54eceb11fb7f95658ca24549c44a [formerly 7f6e17277198a7be08a18074185f2657d88c3c2d]] [formerly ccc16ca19e3a3432a725ade8d6b9903a22d82589 [formerly 67468362112d696896d0ec649ea6c5fc91de826b]] [formerly b12c912f9707db1ceab81e780dc61ee4938fa2ea [formerly b88ef968cb9300557da2aa984200f684e5ce31f4] [formerly 0696678d998cd2f8c5ecefb4b9a509d4983ea45d [formerly 051fdc2ccc886bef5db495cfc7c9a6e379d1d9e7]] [formerly ccc16ca19e3a3432a725ade8d6b9903a22d82589 [formerly 67468362112d696896d0ec649ea6c5fc91de826b] [formerly 2fc79e34cda97c9389b534585c3a6511f650f23e [formerly 85dc3265ab4f7f188e2b5195b137d225ced9e3c0] [formerly 4aa885e012b8732a3c447aeaf9bf758fc4845a85] [formerly 67468362112d696896d0ec649ea6c5fc91de826b [formerly 67468362112d696896d0ec649ea6c5fc91de826b [formerly 38328e20b2f2e4f736de324a71d22d3ce9546f78]]]]]]
    Former-commit-id: 10b4b1a2a42cd62379b365c5d1424d705acae226 [formerly e59f28032e62f11cb0390e7ba8cb4729e3524ecc] [formerly 8c0c033676bc15b9736852dafb8022c2cd56a575 [formerly 09819429168f5e418830e13efecf7245ad0d0cfc]] [formerly b2d75dd6949b2b9f24400c7690120c8ebf43f6bd [formerly 7e7da6a300e3c64903ec7bd22d988012b0953261] [formerly 360e4ecede8dd1b7dc3ba57cc739143056625404 [formerly 1b96760d770821ade524780ce053f1e9c2f80a0a]]]
    Former-commit-id: b2d75dd6949b2b9f24400c7690120c8ebf43f6bd [formerly 7e7da6a300e3c64903ec7bd22d988012b0953261]
    Former-commit-id: e90b55c056aef0df67d4c097692cf4fd891521e4 [formerly 41d22935f44f6767e570e11cbbcf596a2227c995]
    Former-commit-id: 74b5641539d9cfd07dcd5346ce3a408fa093111c [formerly 56aba414ec792de2004288c76412df2055c41cab [formerly c6703e854824d052c06e3d7300efaad962be2004]]
    Former-commit-id: 93701d983bacb9b4040d4158a1baa83590a14449
    Former-commit-id: 1715f8deb1f0190cbf608e0b4e49f3fc11f2b217

--- immunova/flow/supervised_algo/svm.py
+++ immunova/flow/supervised_algo/svm.py
@@ -1,7 +1,10 @@
 from sklearn.svm import LinearSVC, SVC
 
-class SupportVectorMachine:
 
+class SupportVectorMachine:
+    """
+    Implement a Support Vector Machine for the purpose of predicting cell classification in FCS data
+    """
     def __init__(self):
         pass
 
