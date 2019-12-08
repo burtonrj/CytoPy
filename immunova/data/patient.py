23bb645435a07e80796bc4a453fc592586f693b2
burtonrj <burtonrj@cardiff.ac.uk>
2019-12-08 15:04:13 +0000

    Load meta data methods in explorer

--- immunova/data/patient.py
+++ immunova/data/patient.py
@@ -18,12 +18,12 @@ class Bug(mongoengine.EmbeddedDocument):
     Document representation of isolated pathogen. Single document instance represents one pathogen.
     """
     gram_status = mongoengine.StringField(required=False)
-    hmbpp_status = mongoengine.StringField(required=False)
-    ribo_status = mongoengine.StringField(required=False)
+    hmbpp_status = mongoengine.BooleanField(required=False)
+    ribo_status = mongoengine.BooleanField(required=False)
     org_name = mongoengine.StringField(required=False)
     id_method = mongoengine.StringField(required=False)
     culture_source = mongoengine.StringField(required=False)
-    organism_type = mongoengine.StringField(required=False)
+    organism_type = mongoengine.StringField(required=False, choices=['bacteria', 'fungi', 'virus'])
     report_date = mongoengine.DateTimeField(required=False)
     notes = mongoengine.StringField(required=False)
 
