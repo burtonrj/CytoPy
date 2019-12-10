81870bbf910adda73d1003e5ae1a40f36409da75
burtonrj <burtonrj@cardiff.ac.uk>
2019-12-10 10:12:50 +0000

    Continuation of explorer experiment, modifications must be made to scatter_plot method

--- immunova/flow/supervised/cell_classifier.py
+++ immunova/flow/supervised/cell_classifier.py
@@ -7,10 +7,9 @@ from immunova.flow.supervised.utilities import standard_scale, norm_scale, find_
     predict_class, random_oversampling
 from immunova.flow.gating.utilities import density_dependent_downsample, check_downstream_overlaps
 from immunova.flow.supervised.evaluate import evaluate_model
+from immunova.flow.utilities import progress_bar
 from sklearn.model_selection import train_test_split, KFold
 from multiprocessing import Pool, cpu_count
-from IPython import get_ipython
-from tqdm import tqdm_notebook, tqdm
 from functools import partial
 import pandas as pd
 import numpy as np
@@ -18,34 +17,6 @@ import numpy as np
 
 class CellClassifierError(Exception):
     pass
-
-
-def which_environment():
-    """
-    Test if module is being executed in the Jupyter environment.
-    :return:
-    """
-    try:
-        ipy_str = str(type(get_ipython()))
-        if 'zmqshell' in ipy_str:
-            return 'jupyter'
-        if 'terminal' in ipy_str:
-            return 'ipython'
-    except:
-        return 'terminal'
-
-
-def progress_bar(x: iter, **kwargs) -> callable:
-    """
-    Generate a progress bar using the tqdm library. If execution environment is Jupyter, return tqdm_notebook
-    otherwise used tqdm.
-    :param x: some iterable to pass to tqdm function
-    :param kwargs: additional keyword arguments for tqdm
-    :return: tqdm or tqdm_notebook, depending on environment
-    """
-    if which_environment() == 'jupyter':
-        return tqdm_notebook(x, **kwargs)
-    return tqdm(x, **kwargs)
 
 
 def multi_process_ordered(func: callable, x: iter, chunksize: int = 100) -> list:
