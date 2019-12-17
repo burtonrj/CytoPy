a685a9413cf58e7dde0ff73c03d9a3fa03cfdbe6
burtonrossj <burtonrj@cardiff.ac.uk>
2019-12-17 10:36:22 +0000

    Clustering definition model now located in data.fcs
    Former-commit-id: cf492f3e7190e0fdde6501c913c31ec8895fc317 [formerly 072f94f0a02da0b474020509a0cc4b5b5b3174ee] [formerly ea6420a5a32e0ea87ec1649b5e9f1bf355c2e9ed [formerly 000eb2a8c3f7306084926832ef193abcdf35e667]] [formerly 2795316b468e79a52eae9165c312e8ff74f1ea59 [formerly 2e715c3533c68eedde2049395f58ebf006a341fc]] [formerly 48aa306c7c58d0ee0b80611bedd2e5f67cbd2315 [formerly 3912bdf5aeed095c36187a0795429eee8b9397ff] [formerly dbf4f9fea85e065f56baf95519f891df79792093 [formerly 0d8079316cae3c169cf03d4a44f5941babf4cc2e]] [formerly 2795316b468e79a52eae9165c312e8ff74f1ea59 [formerly 2e715c3533c68eedde2049395f58ebf006a341fc] [formerly 049d2bc35c4b8f94a1cfde5ab1a8481d95f05fd1 [formerly 9f4355015b9637fa49e39ae3ab2ca5c61dda93a7] [formerly 1fe102c33f3b037fc7315458a49247f12b4fe8cf] [formerly 2e715c3533c68eedde2049395f58ebf006a341fc [formerly 2e715c3533c68eedde2049395f58ebf006a341fc [formerly 1441a0c3ec0228984825f8242c3cf4ece4257dec]]]]]]
    Former-commit-id: 74e9ca75b1fb5c16f802ccfcea1e278f7b93817f [formerly e045a98deae85070cc9a19916e50d35be4443229] [formerly ddf516835f3dc058dbfe0ba6f48e2b5123460b5f [formerly 35b30616025b98a0a6b4a3bc60116fda617b4176]] [formerly 78c2257f633b524fa3805fdc46b861e35f2efecd [formerly 10d1df423b10716232f78d658c7daa756cf28d98] [formerly 12df5f5cc6bd5e71afad42f28dd99493d863f2c6 [formerly 8b7c52bc47e88d42a0218e0b4c478aea7d7e1804]]]
    Former-commit-id: 27a0fbf615b05080a64edd8e1271100a4cb7b1fc [formerly bfe89a1260e6a36f28b808e256167ce87ec640a0] [formerly 78c2257f633b524fa3805fdc46b861e35f2efecd [formerly 10d1df423b10716232f78d658c7daa756cf28d98]]
    Former-commit-id: 1d62d9cd9704421e82404fa96b3c5d6e63946cfc [formerly e2bd26abcd9c0a044f9eb27801afbf44de013bd4]
    Former-commit-id: 10b573f6fbfbb8b90fb67a13a32b7fca82e9ce70 [formerly 08815f2443847f07770f0405fa0e586bf6583e47 [formerly 776779c3858df238cb994e7d316513035a638c53]]
    Former-commit-id: 2f92a537d037bbdd4f66607565d818de4232d1b6
    Former-commit-id: 47773ce73b39fa3e626a87b9b5242504ebad7935

--- immunova/data/fcs.py
+++ immunova/data/fcs.py
@@ -1,6 +1,5 @@
 from immunova.data.panel import ChannelMap
 from immunova.data.gating import Gate
-from immunova.data.clustering import ClusteringDefinition
 from bson.binary import Binary
 import numpy as np
 import mongoengine
@@ -12,6 +11,30 @@ def generate_sample(data, n) -> np.array:
         idx = np.random.randint(data.shape[0], size=n)
         return data[idx, :]
     return data
+
+
+class ClusteringDefinition(mongoengine.Document):
+    """
+    Defines the methodology and parameters of clustering to apply to an FCS File Group, or in the case of
+    meta-clustering, a collection of FCS File Groups from the same FCS Experiment
+
+    Parameters
+    clustering_uid: unique identifier
+    method: type of clustering performed, either PhenoGraph or FlowSOM
+    parameters: parameters passed to clustering algorithm
+    features: list of channels/markers that clustering is performed on
+    transform_method: type of transformation to be applied to data prior to clustering
+    root_population: population that clustering is performed on (default = 'root')
+    cluster_prefix: a prefix to add to the name of each resulting cluster
+    meta_clustering: refers to whether the clustering is 'meta-clustering'
+    """
+    clustering_uid = mongoengine.StringField(required=True, unique=True)
+    method = mongoengine.StringField(required=True, choices=['PhenoGraph', 'FlowSOM'])
+    parameters = mongoengine.ListField(required=True)
+    features = mongoengine.ListField(required=True)
+    transform_method = mongoengine.StringField(required=False, default='logicle')
+    root_population = mongoengine.StringField(required=True, default='root')
+    cluster_prefix = mongoengine.StringField(required=True, default='cluster')
 
 
 class Cluster(mongoengine.EmbeddedDocument):
