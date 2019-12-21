54f23b04df9a27bee5c75b37140f7998d23428cf
burtonrj <burtonrj@cardiff.ac.uk>
2019-12-21 21:07:23 +0000

    Updated documentaiton for FlowSOM module. Also moved ConsensusClustering to a new module named meta_cluster.py. Unlike the MetaClustering Class found in main.py, the classes in meta_cluster.py are for consensus clustering algorithms

--- immunova/flow/clustering/flowsom.py
+++ immunova/flow/clustering/flowsom.py
@@ -1,130 +1,20 @@
 from immunova.flow.utilities import progress_bar
+from immunova.flow.clustering.meta_cluster import ConsensusCluster
 from sklearn.preprocessing import MinMaxScaler
 from minisom import MiniSom
-from itertools import combinations
 import pandas as pd
-import numpy as np
-import bisect
-
-
-class ConsensusCluster:
-    """
-      Implementation of Consensus clustering, following the paper
-      https://link.springer.com/content/pdf/10.1023%2FA%3A1023949509487.pdf
-      Args:
-        * cluster -> clustering class
-                    needs fit_predict method called with parameter n_clusters
-        * L -> smallest number of clusters to try
-        * K -> biggest number of clusters to try
-        * H -> number of resamplings for each cluster number
-        * resample_proportion -> percentage to sample
-        * Mk -> consensus matrices for each k (shape =(K,data.shape[0],data.shape[0]))
-                (NOTE: every consensus matrix is retained, like specified in the paper)
-        * Ak -> area under CDF for each number of clusters
-                (see paper: section 3.3.1. Consensus distribution.)
-        * deltaK -> changes in ares under CDF
-                (see paper: section 3.3.1. Consensus distribution.)
-        * self.bestK -> number of clusters that was found to be best
-      """
-
-    def __init__(self, cluster, smallest_cluster_n, largest_cluster_n, n_resamples, resample_proportion=0.5):
-        assert 0 <= resample_proportion <= 1, "proportion has to be between 0 and 1"
-        self.cluster_ = cluster
-        self.resample_proportion_ = resample_proportion
-        self.L_ = smallest_cluster_n
-        self.K_ = largest_cluster_n
-        self.H_ = n_resamples
-        self.Mk = None
-        self.Ak = None
-        self.deltaK = None
-        self.bestK = None
-
-    @staticmethod
-    def _internal_resample(data, proportion):
-        """
-        Args:
-          * data -> (examples,attributes) format
-          * proportion -> percentage to sample
-        """
-        resampled_indices = np.random.choice(
-            range(data.shape[0]), size=int(data.shape[0]*proportion), replace=False)
-        return resampled_indices, data[resampled_indices, :]
-
-    def fit(self, data, verbose=False):
-        """
-        Fits a consensus matrix for each number of clusters
-        Args:
-          * data -> (examples,attributes) format
-          * verbose -> should print or not
-        """
-        Mk = np.zeros((self.K_-self.L_, data.shape[0], data.shape[0]))
-        Is = np.zeros((data.shape[0],)*2)
-        for k in range(self.L_, self.K_):  # for each number of clusters
-            i_ = k-self.L_
-            if verbose:
-                print("At k = %d, aka. iteration = %d" % (k, i_))
-            for h in range(self.H_):  # resample H times
-                if verbose:
-                    print("\tAt resampling h = %d, (k = %d)" % (h, k))
-                resampled_indices, resample_data = self._internal_resample(
-                    data, self.resample_proportion_)
-                Mh = self.cluster_(n_clusters=k).fit_predict(resample_data)
-                # find indexes of elements from same clusters with bisection
-                # on sorted array => this is more efficient than brute force search
-                id_clusts = np.argsort(Mh)
-                sorted_ = Mh[id_clusts]
-                for i in range(k):  # for each cluster
-                    ia = bisect.bisect_left(sorted_, i)
-                    ib = bisect.bisect_right(sorted_, i)
-                    is_ = id_clusts[ia:ib]
-                    ids_ = np.array(list(combinations(is_, 2))).T
-                    # sometimes only one element is in a cluster (no combinations)
-                    if ids_.size != 0:
-                        Mk[i_, ids_[0], ids_[1]] += 1
-                # increment counts
-                ids_2 = np.array(list(combinations(resampled_indices, 2))).T
-                Is[ids_2[0], ids_2[1]] += 1
-            Mk[i_] /= Is+1e-8  # consensus matrix
-            # Mk[i_] is upper triangular (with zeros on diagonal), we now make it symmetric
-            Mk[i_] += Mk[i_].T
-            Mk[i_, range(data.shape[0]), range(
-                data.shape[0])] = 1  # always with self
-            Is.fill(0)  # reset counter
-        self.Mk = Mk
-        # fits areas under the CDFs
-        self.Ak = np.zeros(self.K_-self.L_)
-        for i, m in enumerate(Mk):
-            hist, bins = np.histogram(m.ravel(), density=True)
-            self.Ak[i] = np.sum(h*(b-a)
-                             for b, a, h in zip(bins[1:], bins[:-1], np.cumsum(hist)))
-        # fits differences between areas under CDFs
-        self.deltaK = np.array([(Ab-Aa)/Aa if i > 2 else Aa
-                                for Ab, Aa, i in zip(self.Ak[1:], self.Ak[:-1], range(self.L_, self.K_-1))])
-        self.bestK = np.argmax(self.deltaK) + \
-            self.L_ if self.deltaK.size > 0 else self.L_
-
-    def predict(self):
-        """
-        Predicts on the consensus matrix, for best found cluster number
-        """
-        assert self.Mk is not None, "First run fit"
-        return self.cluster_(n_clusters=self.bestK).fit_predict(
-            1-self.Mk[self.bestK-self.L_])
-
-    def predict_data(self, data):
-        """
-        Predicts on the data, for best found cluster number
-        Args:
-          * data -> (examples,attributes) format
-        """
-        assert self.Mk is not None, "First run fit"
-        return self.cluster_(n_clusters=self.bestK).fit_predict(
-            data)
 
 
 class FlowSOM:
     """
     Python implementation of FlowSOM algorithm, adapted from https://github.com/Hatchin/FlowSOM
+
+    Arguments:
+        * data: training data (Pandas DataFrame)
+        * features: list of columns to include
+        * neighborhood_function: name of distribution for initialising weights (default = 'gaussian')
+        * normalisation: if True, data is normalised prior to initialising weights (recommended if initialising
+        weights using PCA).
     """
     def __init__(self, data: pd.DataFrame,
                  features: list,
@@ -148,11 +38,21 @@ class FlowSOM:
         self.meta_class = None
 
     def train(self, som_dim: tuple = (250, 250),
-              sigma: float = 0.1,
+              sigma: float = 1.0,
               learning_rate: float = 0.5,
               batch_size: int = 500,
               seed: int = 42,
               weight_init: str = 'random'):
+        """
+        Train self-organising map.
+        :param som_dim: dimensions of SOM embedding (number of nodes)
+        :param sigma: the radius of the different neighbors in the SOM, default = 1.0
+        :param learning_rate: alters the rate at which weights are updated
+        :param batch_size: size of batches used in training (alters number of total iterations)
+        :param seed: random seed
+        :param weight_init: how to initialise weights: either 'random' or 'pca' (Initializes the weights to span the
+        first two principal components)
+        """
 
         som = MiniSom(som_dim[0], som_dim[1],
                       self.dims, sigma=sigma,
@@ -162,6 +62,9 @@ class FlowSOM:
         if weight_init == 'random':
             som.random_weights_init(self.data)
         elif weight_init == 'pca':
+            if not self.normalisation:
+                print('Warning: It is strongly recommended to normalize the data before initializing '
+                      'the weights if using PCA.')
             som.pca_weights_init(self.data)
         else:
             print('Warning: invalid value provided for "weight_init", valid input is either "random" or "pca". '
@@ -183,6 +86,17 @@ class FlowSOM:
                      max_n: int,
                      iter_n: int,
                      resample_proportion: float = 0.5):
+        """
+        Perform meta-clustering. Implementation of Consensus clustering, following the paper
+        https://link.springer.com/content/pdf/10.1023%2FA%3A1023949509487.pdf
+
+        :param cluster_class: clustering object (must follow Sklearn standard; needs fit_predict method called with
+        parameter n_clusters)
+        :param min_n: the min proposed number of clusters
+        :param max_n: the max proposed number of clusters
+        :param iter_n: the iteration times for each number of clusters
+        :param resample_proportion: within (0, 1), the proportion of re-sampling when computing clustering
+        """
 
         if self.map is None:
             raise ValueError('Error: SOM must be trained prior to meta-clustering.')
@@ -200,7 +114,11 @@ class FlowSOM:
         self.meta_class = self.meta_flatten.reshape(self.xn, self.yn)
 
     def predict(self):
-        if self.map is None:
+        """
+        Predict the cluster allocation for each cell in the associated dataset.
+        (Requires that train and meta_cluster have been called previously)
+        """
+        if self.map is None or self.meta_class is None:
             raise ValueError('Error: SOM must be trained prior to predicting cell clustering allegation.')
         labels = []
         print('---------- Predicting Labels ----------')
