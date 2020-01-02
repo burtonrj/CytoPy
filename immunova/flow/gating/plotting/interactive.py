bff3edfdd53a7f61de863de5b215eb6d7f68fbe7
burtonrj <burtonrj@cardiff.ac.uk>
2020-01-02 15:25:43 +0000

    Removed scatter_3d_fmo from InteractivePlotting because non-functional. Need to return and implement later if required. Also completed docs in InteractivePlotting
    Former-commit-id: bb63d1009ece3c0fe53a11b7ff1162a406d1c8a8 [formerly bb63d1009ece3c0fe53a11b7ff1162a406d1c8a8 [formerly 0e681f4cd91b92c31162f10d548648614d953d70]]
    Former-commit-id: 8d21d5fb45a0cf29dbd4418cdb7cc57f8923deed
    Former-commit-id: 699f7bc3f1e4f199fe832375b38c2a4d0e5add2b

--- immunova/flow/gating/plotting/interactive.py
+++ immunova/flow/gating/plotting/interactive.py
@@ -10,6 +10,7 @@ class InteractivePlotting:
 
     Attributes:
         gating: Gating object (see flow.gating.actions.Gating)
+        scatter_3d: Generate an interactive plotly 3D scatter plot
     """
     def __init__(self, gating: Gating):
         self.gating = gating
@@ -17,7 +18,7 @@ class InteractivePlotting:
     def _get_data(self, population_name, fmo_x: str or None = None, fmo_y: str or None = None,
                   fmo_z: str or None = None) -> pd.DataFrame:
         """
-        Fetch a Pandas DataFrame of single cell data for a given population
+        Internal method. Fetch a Pandas DataFrame of single cell data for a given population
         :param population_name: required population
         :param fmo_x: name of FMO for x-axis dimension (optional)
         :param fmo_y: name of FMO for y-axis dimension (optional)
@@ -33,12 +34,28 @@ class InteractivePlotting:
         return data
 
     @staticmethod
-    def __transform_data(data: dict, axes_vars: dict, transforms: dict):
+    def _transform_data(data: dict, axes_vars: dict, transforms: dict or None = None) -> dict:
+        """
+        Internal method. Transform axis.
+        :param data: dictionary of data; key value paris corresponding to either primary data or each FMO
+        :param axes_vars: dictionary of axis variable names
+        :param transforms: dicitonary of transform methods to apply (if None, defaults to logicle transform)
+        :return: dictionary of transformed data
+        """
         if transforms is None:
             transforms = dict(x='logicle', y='logicle', z='logicle')
         return {k: transform_axes(v, axes_vars, transforms) for k, v in data.items()}
 
-    def scatter_3d(self, population_name, x, y, z, transforms):
+    def scatter_3d(self, population_name: str, x: str, y: str, z: str, transforms: dict or None = None):
+        """
+        Generate a 3D scatter plot
+        :param population_name: name of population to plot
+        :param x: vairable to plot on x-axis
+        :param y: vairable to plot on y-axis
+        :param z: vairable to plot on z-axis
+        :param transforms: dictionary of transforms to be applied to each axis
+        :return: None
+        """
         if population_name in self.gating.populations.keys():
             data = self.gating.get_population_df(population_name).copy()
         else:
@@ -52,17 +69,4 @@ class InteractivePlotting:
         fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
         fig.show()
 
-    def scatter_3d_fmo(self, population_name, x, y, z, transforms,
-                       fmo_x: str or None = None, fmo_y: str or None = None,
-                       fmo_z: str or None = None):
-        data = self._get_data(population_name=population_name, fmo_x=fmo_x, fmo_y=fmo_y, fmo_z=fmo_z)
-        data = self.__transform_data(data=data, axes_vars={'x': x, 'y': y, 'z': z}, transforms=transforms)
-        d = pd.DataFrame()
-        for k, v in data.items():
-            v['origin'] = k
-            d = pd.concat([d, v])
-        fig = px.scatter_3d(d, x=x, y=y, z=z, opacity=0.4, color='origin')
-        fig.update_traces(marker=dict(size=3), selector=dict(mode='markers'))
-        fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
-        fig.show()
 
