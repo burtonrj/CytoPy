76a29cf3d312c1ca6b90448c0e72ccf9f93ade02
burtonrj <burtonrj@cardiff.ac.uk>
2020-01-02 13:34:17 +0000

    Updated docs for data.patient
    Former-commit-id: a7c36cfc3497f547780845e0a204c544865bd295 [formerly 9fbc9626bfe5b069c3e05cbbede4d7fdfd3aaced] [formerly 15c616225b40f78294a3153340897f0179da5325] [formerly b91d2b46257ea4f600736328e6d58ed3eadcf8eb [formerly da6d35268f399560ad9f67a143582710e9097049] [formerly 15c616225b40f78294a3153340897f0179da5325 [formerly 15c616225b40f78294a3153340897f0179da5325 [formerly a33edb8e80fc8bc0ab42f555919fb175fc794e04]]]]
    Former-commit-id: 4fa0e3b957e048b3a690c4c09f5bf3d8b6948529 [formerly b4bbcccb5b1932337a4543ec070e47fec46384c1] [formerly 2a4e9bf92831cc5a2d3a2d8217794a9cb1330a99 [formerly 258127c26b4e760d88c2865fedd30942b31025cc]]
    Former-commit-id: 83da0c281593d8e162b04e2212c277c0aa6a66ec [formerly 2a4e9bf92831cc5a2d3a2d8217794a9cb1330a99]
    Former-commit-id: 0de5366abb2ce073cff88271bc82daf1ae6440a6
    Former-commit-id: 294db1e41b3c375eb9243394ab0ea98337835220

--- immunova/data/patient.py
+++ immunova/data/patient.py
@@ -3,6 +3,14 @@ import mongoengine
 
 
 class MetaDataDictionary(mongoengine.Document):
+    """
+    Model for a custom dictionary that can be used for given descriptions to meta-data. Helpful when exploring single cell data that has been
+    associated to meta-data in the Explorer object; see flow.clustering.main.Explorer)
+
+    Attributes:
+        key - name of meta-data (column name)
+        desc - string value of wrriten description
+    """
     key = mongoengine.StringField()
     desc = mongoengine.StringField()
 
@@ -11,6 +19,11 @@ class Drug(mongoengine.EmbeddedDocument):
     """
     Embedded document -> Patient
     Document representation of drug administration. Single document instance represents one event.
+
+    Attributes:
+        name - name of therapy/drug
+        init_date - date that therapy/drug started
+        end_data - date that therapy/drug ended
     """
     name = mongoengine.StringField(required=True)
     init_date = mongoengine.DateTimeField(required=False)
@@ -21,10 +34,21 @@ class Bug(mongoengine.EmbeddedDocument):
     """
     Embedded document -> Patient
     Document representation of isolated pathogen. Single document instance represents one pathogen.
+
+    Attributes:
+        gram_status - value of organisms gram status, valid choices are  ['P+ve', 'N-ve', 'Unknown']
+        hmbpp_status - value of hmbpp status, valid choices are  ['P+ve', 'N-ve', 'Unknown']
+        ribo_status - value of organisms ribo status, valid choices are  ['P+ve', 'N-ve', 'Unknown']
+        org_name - name of the organism
+        id_method - method used to identify organism
+        culture_source - site of isolated organism
+        organism_type - type of organism isolated, valid choices are ['bacteria', 'fungi', 'virus']
+        report_date - date that organism was reported
+        notes - string value for free text notes
     """
-    gram_status = mongoengine.StringField(required=False)
-    hmbpp_status = mongoengine.StringField(required=False)
-    ribo_status = mongoengine.StringField(required=False)
+    gram_status = mongoengine.StringField(required=False, choices=['P+ve', 'N-ve', 'Unknown'])
+    hmbpp_status = mongoengine.StringField(required=False, choices=['P+ve', 'N-ve', 'Unknown'])
+    ribo_status = mongoengine.StringField(required=False, choices=['P+ve', 'N-ve', 'Unknown'])
     org_name = mongoengine.StringField(required=False)
     id_method = mongoengine.StringField(required=False)
     culture_source = mongoengine.StringField(required=False)
@@ -37,6 +61,14 @@ class Biology(mongoengine.EmbeddedDocument):
     """
     Embedded document -> Patient
     Document representation of biological test (blood pathology). Single document instance represents one test.
+
+    Attributes:
+        test_date - date that test was performed
+        test - name of pathology test
+        result - value of pathology test
+        unit - units reported
+        ref_range - reported reference range
+        test_category - category of test
     """
     test_date = mongoengine.DateTimeField()
     test = mongoengine.StringField()
