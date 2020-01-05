d84daa7dff57d516347aeb9a6408828850f2875c
burtonrj <burtonrj@cardiff.ac.uk>
2020-01-05 10:28:10 +0000

    Added report_card to supervised evaluation
    Former-commit-id: 774af872da21df35ad46a249f55176aa2b12f269 [formerly 2602d92f9425aa91dd5f8216e4581dd322b51887] [formerly 62ec31c9b40766cebb3294a2e5f6c3e96c1095f7 [formerly a4e368ee39ec79f6f816a224c9cc962fd0cac571]] [formerly 7d3d8eac6816473e4aa00f26f11874bd162d7c98 [formerly b8894fe6ef3bb7a0ac3a74951915526130763369] [formerly a4e368ee39ec79f6f816a224c9cc962fd0cac571 [formerly a4e368ee39ec79f6f816a224c9cc962fd0cac571 [formerly 3ada6b88903b0628da540ebcded1e05e21fb82ea]]]]
    Former-commit-id: 3bf24527b6c014f1d4219fdab1a41e15af9a6f99 [formerly a50e398cf7968a78be9765186dd134591d330434] [formerly b726689f46925ae08333d672e6af797d3be2aad4 [formerly 96bac3f89c1df2ae265bf50c3a11e4d596ccae0e]]
    Former-commit-id: b726689f46925ae08333d672e6af797d3be2aad4
    Former-commit-id: 5dab40138534ffc6cd85826187df9a8623cf5f08
    Former-commit-id: 5ee49a313091af7469b66d93eb174d91614c8c63

--- immunova/flow/supervised/evaluate.py
+++ immunova/flow/supervised/evaluate.py
@@ -1,7 +1,8 @@
-from immunova.flow.supervised.utilities import  predict_class
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, multilabel_confusion_matrix, \
-    roc_auc_score, label_ranking_average_precision_score, label_ranking_loss
+from immunova.flow.supervised.utilities import predict_class
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, \
+    label_ranking_average_precision_score, label_ranking_loss, classification_report
 import pandas as pd
+import pprint
 
 
 def multi_label_performance(y, y_probs):
@@ -17,3 +18,16 @@ def evaluate_model(classifier, x, y, threshold=None):
                              accuracy=[accuracy_score(y_true=y, y_pred=y_hat)],
                              precision=[precision_score(y_true=y, y_pred=y_hat, average='weighted')],
                              recall=[recall_score(y_true=y, y_pred=y_hat, average='weighted')]))
+
+
+def report_card(classifier, x, y, mappings=None, threshold=None, sample_weights=None):
+    y_probs = classifier.predict_proba(x)
+    y_hat = predict_class(y_probs, threshold)
+    print('--------- CLASSIFICATION REPORT ---------')
+    print(classification_report(y, y_hat, sample_weight=sample_weights))
+    if mappings is not None:
+        print('\n')
+        print('Class Mappings...')
+        pprint.pprint(mappings)
+    print('-----------------------------------------')
+
