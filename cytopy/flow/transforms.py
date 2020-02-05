0d50cb35fc589dd23c1d2609e60ef1dbb277e66b
burtonrossj <burtonrj@cardiff.ac.uk>
2020-02-05 11:38:35 +0000

    Renamed project
    Former-commit-id: fe705222289eff44242f65248572370b1751f603 [formerly 0da1c4c37341889841a0287224cc703c91462517] [formerly ea4ec171967138e4cbb3cadda983705ab98f40a6 [formerly 5b1ed1d88c219793326bc0f9734071495b5cd613]] [formerly d1077f073afdefc03614b1ce01c33524ea93ccee [formerly dc7a57e7c7b4cdb1076e97b8b40e76400e6c84a4]] [formerly 6e91bafd616590aa78ef7e3b603278fa72eaa4ff [formerly da6356edccff7715cba16760b479a0f696900b37] [formerly 9378db068e851b7a1560bc523a9b7bb0506b2240 [formerly ddfef43cbaa4e98153aa0f6bb8e2ae7cce96820b]] [formerly d1077f073afdefc03614b1ce01c33524ea93ccee [formerly dc7a57e7c7b4cdb1076e97b8b40e76400e6c84a4] [formerly f543cdb9daa2432c32360cdc7dd58979755d092c [formerly c783e0472261852d9671357cf0d08701316f4a8d] [formerly 06087a0a90c4057be457257ecb989cf2a3f6785d] [formerly dc7a57e7c7b4cdb1076e97b8b40e76400e6c84a4 [formerly dc7a57e7c7b4cdb1076e97b8b40e76400e6c84a4 [formerly 7eef0fd21bfeb84cb76d12dccb048ad4a19d685d]]]]]]
    Former-commit-id: e7d5a13cb7c046213235364778e12fcdba8f49d7 [formerly e193247e3848171c73e3b059f0ab3a99fd6b2b6a] [formerly 7424d1c052bbfb6a29216032624f0dc220251fc3 [formerly e81b80f3de339757c3f6e98952d225f6ba4a458a]] [formerly faf38229ae559bb372d3694c701462660c89dab1 [formerly 13caea85a1e0744ff3423ae2ca419eba44e19b1a] [formerly 6db7a7ec35dc32309850c0172273c1b230245d6f [formerly 29930635b8f914da7cb82b0110359e548153b2cc]]]
    Former-commit-id: 91c33668ceb01a6362db10175e58081971f0a9bd [formerly c65acb06a38350c2d2516e0f6694622ebb48be0b] [formerly faf38229ae559bb372d3694c701462660c89dab1 [formerly 13caea85a1e0744ff3423ae2ca419eba44e19b1a]]
    Former-commit-id: 7b979cc10c0c4c1bbaf99415caf72dc799c2598f [formerly a94999b2596fec8b64acb956c176f699043bbd3e]
    Former-commit-id: 665548b40d63a4174c94a3c6fd2009bde564795d [formerly 5a668125be7e16ca7f87c132391bf78a0fb2636c [formerly 3661da80afdaada9894495fc547c8de418898c1f]]
    Former-commit-id: 8a578eabca448e03050a163e79c181b43db7cc7c
    Former-commit-id: 6753b02e6fd9f55d913da125192bcbc3f5962c7c

---
+++ cytopy/flow/transforms.py
@@ -0,0 +1,73 @@
+from flowutilspd.transforms import logicle, hyperlog, log_transform, asinh
+from cytopy.flow.supervised.utilities import scaler
+import pandas as pd
+
+
+class TransformError(Exception):
+    pass
+
+
+def percentile_rank_transform(data: pd.DataFrame, features_to_transform: list) -> pd.DataFrame:
+    """
+    Calculate percentile rank transform of data-frame. Each event is ranked as the average according to the
+    column, then divided by the total number of events and multiplied by 100 to give the percentile.
+    :param data: pandas dataframe of events
+    :param features_to_transform: features to perform transformation on
+    :return: Transformed dataframe
+    """
+    data = data.copy()
+    transform = data[features_to_transform].rank(axis=0, method='average')
+    transform = (transform / transform.shape[0]) * 100
+    data[features_to_transform] = transform
+    return data
+
+
+def apply_transform(data: pd.DataFrame, features_to_transform: list or str = 'all',
+                    transform_method: str = 'logicle', prescale=1) -> pd.DataFrame:
+    """
+    Apply a transformation to the given dataset; valid transformation methods are:
+    logicle, hyperlog, log_transform, or asinh
+    :param data: pandas dataframe
+    :param features_to_transform: a list of features to transform
+    :param transform_method: string value indicating transformation method
+    :param prescale: if using asinh transformaion this value is passed as the scalling argument
+    :return: transformed pandas dataframe
+    """
+
+    if features_to_transform == 'all':
+        features_to_transform = data.columns
+    elif features_to_transform == 'fluorochromes':
+        features_to_transform = [x for x in data.columns if all([y not in x for y in ['FSC', 'SSC', 'Time']])]
+    elif type(features_to_transform) != list:
+        print('Error: invalid argument provided for `features_to_transform`, expected one of: `all`, `fluorochromes`,'
+              ' or list of valid column names, proceeding with transformation of entire dataframe as precaution.')
+        features_to_transform = data.columns
+    elif not all([x in data.columns for x in features_to_transform]):
+        print('Error: invalid argument provided for `features_to_transform`, list must contain column names that '
+              f'correspond to the provided dataframe. Valid input would be one or several of: {data.columns} '
+              'proceeding with transformation of entire dataframe as precaution.')
+        features_to_transform = data.columns
+
+    if transform_method == 'logicle':
+        return logicle(data, features_to_transform)
+    if transform_method == 'hyperlog':
+        return hyperlog(data, features_to_transform)
+    if transform_method == 'log_transform':
+        return log_transform(data, features_to_transform)
+    if transform_method == 'asinh':
+        return asinh(data, features_to_transform, prescale)
+    if transform_method == 'percentile rank':
+        return percentile_rank_transform(data, features_to_transform)
+    if transform_method == 'Yeo-Johnson':
+        return sklearn_scaler(data, features_to_transform, scale_method='power', method='yeo-johnson')
+    if transform_method == 'RobustScale':
+        return sklearn_scaler(data, features_to_transform, scale_method='robust')
+    raise TransformError("Error: invalid transform_method, must be one of: 'logicle', 'hyperlog', 'log_transform',"
+                         " 'asinh', 'percentile rank', 'Yeo-Johnson', 'RobustScale'")
+
+
+def sklearn_scaler(data, features_to_transform, scale_method, **kwargs):
+    data = data.copy()
+    transform, _ = scaler(data[features_to_transform], scale_method=scale_method, **kwargs)
+    data[features_to_transform] = transform
+    return data
