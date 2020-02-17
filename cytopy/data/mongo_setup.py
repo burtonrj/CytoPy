9dadb5310c0bcf37fcddae590d83d38d8cab6b1d
burtonrj <burtonrj@cardiff.ac.uk>
2020-02-17 14:28:31 +0000

    Refactor data.panel to remove hidden methods

--- cytopy/data/mongo_setup.py
+++ cytopy/data/mongo_setup.py
@@ -5,6 +5,6 @@ def global_init(database_name: str) -> None:
     """
     Global initializer for mongogengine ORM
     :param database_name: name of database to establish connection with
-    :return:
+    :return: None
     """
     mongoengine.register_connection(alias='core', name=database_name)
