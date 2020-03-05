b7b8f84f1f54d3be9575375f7f9648f0d73540a2
burtonrossj <burtonrj@cardiff.ac.uk>
2020-03-05 16:48:40 +0000

    Complete overhaul of documenation underway. Need to change format to Numpy format. Also started creating sphinx template
    Former-commit-id: ae7d523eee295cf84bdbead8543f9e3163ea9697

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,7 @@
+import sys
+import os
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+sys.path.insert(0, root_dir)
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -31,6 +35,9 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'autodocsumm'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,9 +54,19 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    "collapse_navigation" : True
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = '../build/logo_sm.png'
+
+autodoc_default_options = {
+    'autosummary': True,
+}
+
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,16 +1,11 @@
-.. CytoPy documentation master file, created by
-   sphinx-quickstart on Thu Mar  5 13:18:36 2020.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to CytoPy's documentation!
+CytoPy - cytometry but in Python
 ==================================
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+    :maxdepth: 2
 
-
+    Reference
+    License
 
 Indices and tables
 ==================
