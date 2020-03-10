7a58bed510b5577f3f56bd45d6d9b541ada5f2e4
burtonrj <burtonrj@cardiff.ac.uk>
2020-03-10 11:40:18 +0000

    Refactored docstrings in gating.py
    Former-commit-id: cc50b7e16afb64206e4d5fcbd73609c6bdfe8cdc [formerly 1b50ca0c1a04fe6890ec1744bd4e968417c27403] [formerly 07a6e96ea67420a934c1624ad49271ef104db4fa] [formerly 2fe0c7937452f3862ef9d93de9da094145cc5b21 [formerly f710b27d0f3748487dd9d2d223cfe43a9931308f] [formerly 07a6e96ea67420a934c1624ad49271ef104db4fa [formerly 07a6e96ea67420a934c1624ad49271ef104db4fa [formerly 26f813344ea2708ac2f56dda58dfcdcd99934dc9]]]]
    Former-commit-id: 41eb3193935488a93b6caa2f6d559016d9e6c62b [formerly 9d235c32d901acd879487329c8de203e3679d551] [formerly 407da185694209439b583da3341df0a27949b93a [formerly ca41df33d5de6e93777ac5d8a3126d1d89eaa778]]
    Former-commit-id: 407da185694209439b583da3341df0a27949b93a
    Former-commit-id: d14e8565b93fcb5a01135f0ce252e1b36f6058b2
    Former-commit-id: a096ee793c9dedf40d9212853a04d270e64c760e

--- cytopy/data/gating.py
+++ cytopy/data/gating.py
@@ -4,21 +4,23 @@ import datetime
 
 class Gate(mongoengine.EmbeddedDocument):
     """
-    Embedded document -> GatingStrategy
-    Ref -> FCSExperiment
-
     Document representation of a Gate
 
-    Attributes:
-        gate_name - unique identifier for this gate (within the scope of a GatingStrategy)
-        children - list of population names; populations derived from application of this gate
-        parent - name of parent population; the population this gate acts upon
-        class_ - name of gating class used to generate gate (see flow.gating.actions)
-        method - name of class method used to generate gate (see flow.gating.actions)
-        kwargs - list of keyword arguments (list of tuples; first element = key, second element = value) passed to
+    Parameters
+    -----------
+    gate_name: str, required
+        Unique identifier for this gate (within the scope of a GatingStrategy)
+    children: list
+        list of population names; populations derived from application of this gate
+    parent: str, required
+        name of parent population; the population this gate acts upon
+    class_: str, required
+        name of gating class used to generate gate (see flow.gating.actions)
+    method: str, required
+        name of class method used to generate gate (see flow.gating.actions)
+    kwargs: list
+        list of keyword arguments (list of tuples; first element = key, second element = value) passed to
         class/method to generate gate
-    Methods:
-        to_python - convert gate document to a Python dictionary
     """
     gate_name = mongoengine.StringField(required=True)
     children = mongoengine.ListField()
@@ -33,7 +35,11 @@ class Gate(mongoengine.EmbeddedDocument):
     def to_python(self) -> dict:
         """
         Convert document to Python dictionary object
-        :return: Dictionary representation of document
+
+        Returns
+        --------
+        dict
+            Dictionary representation of document
         """
         return dict(gate_name=self.gate_name, children=self.children,
                     class_=self.class_, method=self.method, kwargs=self.kwargs)
@@ -44,13 +50,20 @@ class GatingStrategy(mongoengine.Document):
     Document representation of a gating template; a gating template is a collection of gating objects
     that can be applied to multiple fcs files or an entire experiment in bulk
 
-    Attributes:
-        template_name - unique identifier for template
-        gates - list of Gate documents; see Gate
-        creation_date - date of creation
-        last_edit - date of last edit
-        flags - warnings associated to this gating template
-        notes - free text comments
+    Parameters
+    -----------
+    template_name: str, required
+        unique identifier for template
+    gates: EmbeddedDocumentList
+        list of Gate documents; see Gate
+    creation_date: DateTime
+        date of creation
+    last_edit: DateTime
+        date of last edit
+    flags: str, optional
+        warnings associated to this gating template
+    notes: str, optional
+        free text comments
     """
     template_name = mongoengine.StringField(required=True)
     gates = mongoengine.EmbeddedDocumentListField(Gate)
