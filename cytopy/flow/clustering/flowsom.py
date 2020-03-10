5bb70a6c55d06165808b80dcd92a5345911fa75e
burtonrj <burtonrj@cardiff.ac.uk>
2020-03-10 15:58:33 +0000

    Refactored docstrings in flowsom.pu
    Former-commit-id: 95ccd1f911cc4c5498cf70a5d4bbca43e047bc13

--- cytopy/flow/clustering/flowsom.py
+++ cytopy/flow/clustering/flowsom.py
@@ -6,22 +6,18 @@ import pandas as pd
 
 
 class FlowSOM:
-    """
-    Python implementation of FlowSOM algorithm, adapted from https://github.com/Hatchin/FlowSOM
+    """Python implementation of FlowSOM algorithm, adapted from https://github.com/Hatchin/FlowSOM
 
-    Arguments:
-        - data: training data (Pandas DataFrame)
-        - features: list of columns to include
-        - neighborhood_function: name of distribution for initialising weights (default = 'gaussian')
-        - normalisation: if True, data is normalised prior to initialising weights (recommended if initialising
-        weights using PCA).
-
-    Methods:
-        - train: train nodes of self-organising map
-        - meta-cluster: using Consensus Clustering (see flow.clustering.consensus.ConsensusClustering) perform meta-clustering; finds the optimal number of
-        meta clusters in a given range
-        - predict: returns a list of clustering allocations where each row corresponds to the rows in the training data, predicted using the constructed SOM and
-        results of meta-clustering (requires that 'train' and 'meta-cluster' have been called prior)
+    Parameters
+    ----------
+    data : Pandas.DataFrame
+        training data
+    features : list
+        list of columns to include
+    neighborhood_function : str
+        name of distribution for initialising weights
+    normalisation : bool
+        if True, min max normalisation applied prior to computation
     """
     def __init__(self, data: pd.DataFrame,
                  features: list,
@@ -33,6 +29,9 @@ class FlowSOM:
         if normalisation:
             self.data = MinMaxScaler().fit_transform(self.data)
         self.dims = len(features)
+        assert neighborhood_function in ['gaussian', 'mexican_hat', 'bubble', 'triangle'], 'Invalid neighborhood function, must ' \
+                                                                                           'be one of "gaussian", "mexican_hat", ' \
+                                                                                           '"bubble", or "triangle"'
         self.nf = neighborhood_function
         self.xn = None
         self.yn = None
@@ -50,15 +49,27 @@ class FlowSOM:
               batch_size: int = 500,
               seed: int = 42,
               weight_init: str = 'random'):
-        """
-        Train self-organising map.
-        :param som_dim: dimensions of SOM embedding (number of nodes)
-        :param sigma: the radius of the different neighbors in the SOM, default = 1.0
-        :param learning_rate: alters the rate at which weights are updated
-        :param batch_size: size of batches used in training (alters number of total iterations)
-        :param seed: random seed
-        :param weight_init: how to initialise weights: either 'random' or 'pca' (Initializes the weights to span the
-        first two principal components)
+        """Train self-organising map.
+
+        Parameters
+        ----------
+        som_dim : tuple, (default=(250, 250))
+            dimensions of SOM embedding (number of nodes)
+        sigma : float, (default=1.0)
+            the radius of the different neighbors in the SOM, default = 1.0
+        learning_rate : float, (default=0.5)
+            alters the rate at which weights are updated
+        batch_size : int, (default=500)
+            size of batches used in training (alters number of total iterations)
+        seed : int, (default=42)
+            random seed
+        weight_init : str, (default='random')
+            how to initialise weights: either 'random' or 'pca' (Initializes the weights to span the
+            first two principal components)
+
+        Returns
+        -------
+        None
         """
 
         som = MiniSom(som_dim[0], som_dim[1],
@@ -93,16 +104,26 @@ class FlowSOM:
                      max_n: int,
                      iter_n: int,
                      resample_proportion: float = 0.5):
-        """
-        Perform meta-clustering. Implementation of Consensus clustering, following the paper
+        """Perform meta-clustering. Implementation of Consensus clustering, following the paper
         https://link.springer.com/content/pdf/10.1023%2FA%3A1023949509487.pdf
 
-        :param cluster_class: clustering object (must follow Sklearn standard; needs fit_predict method called with
-        parameter n_clusters)
-        :param min_n: the min proposed number of clusters
-        :param max_n: the max proposed number of clusters
-        :param iter_n: the iteration times for each number of clusters
-        :param resample_proportion: within (0, 1), the proportion of re-sampling when computing clustering
+        Parameters
+        ----------
+        cluster_class :
+            clustering object (must follow Sklearn standard; needs fit_predict method called with
+            parameter n_clusters)
+        min_n : int
+            the min proposed number of clusters
+        max_n : int
+            the max proposed number of clusters
+        iter_n : int
+            the iteration times for each number of clusters
+        resample_proportion : float, (Default value = 0.5)
+            within (0, 1), the proportion of re-sampling when computing clustering
+
+        Returns
+        -------
+        None
         """
 
         assert self.map is not None, 'SOM must be trained prior to meta-clustering; call train before meta_cluster'
@@ -120,9 +141,16 @@ class FlowSOM:
         self.meta_class = self.meta_flatten.reshape(self.xn, self.yn)
 
     def predict(self):
-        """
-        Predict the cluster allocation for each cell in the associated dataset.
+        """Predict the cluster allocation for each cell in the associated dataset.
         (Requires that train and meta_cluster have been called previously)
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        Numpy.array
+            Predicted labels
         """
         err_msg = 'SOM must be trained prior to predicting cell clustering allegation; call train followed ' \
                   'by meta_cluster'
