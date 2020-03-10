d2c48d14d7eac0415533a3744704db5835f134f4
burtonrj <burtonrj@cardiff.ac.uk>
2020-03-10 11:41:04 +0000

    Refactored docstrings in mongo_setup.py
    Former-commit-id: d5e2253e04b553f70d331400abd86dac163752c7 [formerly 511411624d6d9e5a37decbd8703e5404ce892d92] [formerly e75238f300f550b12e0c5eac2cfc4bebcbeb8333 [formerly 5b879ef641e6b9f3dd8c7bd0a44669ec566ae55f]] [formerly 3578d47a43768be26159758988d7b0bd2fe27574 [formerly b4c9ab0799eb203bffb9ba66953b7fa192840b17] [formerly 07d63efa86435c887ba62cb7d319e49d8a39ffae [formerly 88c783a33817e4344a9eb22013e36cd3e48ff010]]] [formerly 2ca6de1af27efd39e5ab93d9f735d38c56e79bd1 [formerly 16f6b379e4b10bb8e00765ca044a553c1b70c3a8] [formerly f496549a3dd2ab601015c99b2d453571569f2051 [formerly b6f24586343fc2e14cb6421c9b6ced082e1b2b97]] [formerly 07d63efa86435c887ba62cb7d319e49d8a39ffae [formerly 88c783a33817e4344a9eb22013e36cd3e48ff010] [formerly e928c67b8da4ace6e37caf65bdeb66256778be4a [formerly 78e5bee47a3e5030db893601819f143e7e37b62d] [formerly 4674a8147b22e2e5fb1438980c1297be3c68cb9d] [formerly 88c783a33817e4344a9eb22013e36cd3e48ff010 [formerly 88c783a33817e4344a9eb22013e36cd3e48ff010 [formerly 58040e97cf13f1bbd5fd987417c1569b2b5fe30f]]]]]]
    Former-commit-id: 73199a292f2c4a9fc2a6c54e16fffd51df11250b [formerly dc7eec3c11b52d2ea279ffd81b3ecc7ebb18dcbd] [formerly 9b51c69912bd224789d026b79c8f5d91e354a93a [formerly 0b33f48c3242d31321591f6a12ff6fd753e1ecff]] [formerly 7ee11469d9096844347eeef417c74879f0a2dcb6 [formerly 4934a87abdfa3f188c1821b7cf5abc6019c1a0ef] [formerly 93690dbd6a1a4caf09bfeeecd35b48768ad68c03 [formerly bbbadb9dfaa1cde37a4745909e70c1f646a80e8c]]]
    Former-commit-id: 7ee11469d9096844347eeef417c74879f0a2dcb6 [formerly 4934a87abdfa3f188c1821b7cf5abc6019c1a0ef]
    Former-commit-id: 311eace1cc8fe9494c9ba573cb44fadd91e3f8dd [formerly cb3932b6b1b621b5325ea143388d53cd0a940659]
    Former-commit-id: 6aa2c2841da6e5317d00bc29686f6102b9de60fc [formerly 32a8ce42886e0900acd525df2f6acc2557d858e1 [formerly 56b15acccb9224edf8fa37c91ad3bae6c8bb921e]]
    Former-commit-id: 116a6d0efb341e299e77700a978ac2d04b520fba
    Former-commit-id: 1157126a7b25f2b423751d813e620f657dba3d57

--- cytopy/data/mongo_setup.py
+++ cytopy/data/mongo_setup.py
@@ -4,7 +4,14 @@ import mongoengine
 def global_init(database_name: str) -> None:
     """
     Global initializer for mongogengine ORM
-    :param database_name: name of database to establish connection with
-    :return: None
+
+    Parameters
+    -----------
+    database_name: str
+        name of database to establish connection with
+
+    Returns
+    --------
+    None
     """
     mongoengine.register_connection(alias='core', name=database_name)
