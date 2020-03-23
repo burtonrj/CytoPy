3bc3f94421136405faa21c06eda52bc9f179e8e8
burtonrossj <burtonrj@cardiff.ac.uk>
2020-03-23 19:12:02 +0000

    Refactored docs for gating.base
    Former-commit-id: ac2ea47ce58725c4448d044e984fa8902e3ea097 [formerly cfe4946dfc3ad004131ae7d5bed5324bcea8d874] [formerly 174c16b604f2b25026cd27ae876b87178452cdbe] [formerly 0d46500031415fad8017ab8cea2d82b84fbd57ef [formerly 5cf18e66662a57269a3bcd586cadf0447e1f49c5] [formerly 174c16b604f2b25026cd27ae876b87178452cdbe [formerly 174c16b604f2b25026cd27ae876b87178452cdbe [formerly 59c3fcd088e49635dce8c881315c963e956ae774]]]]
    Former-commit-id: 61391e3ca3f4e8bc44db8714f456a102dadec40d [formerly 5993eb39004f3e340994ccdf8f50e8d02ee90730] [formerly 308fe3366d93401661e8d1f7467dd26e9bd6f2f5 [formerly d9d55d8a2fb90aee771962b652fb7dfc20c2c526]]
    Former-commit-id: bbdf7509a565d1c6976354751c0265684e7effc0 [formerly 308fe3366d93401661e8d1f7467dd26e9bd6f2f5]
    Former-commit-id: cf11ba0f9e84d865970ff86a57db542054551601
    Former-commit-id: 1e1a71b3caefefca396ac0610638f196bae0a220

--- cytopy/flow/gating/base.py
+++ cytopy/flow/gating/base.py
@@ -18,12 +18,28 @@ class Gate:
     """
     Base class for gate definition.
 
-    Attributes:
-        - data: pandas dataframe of fcs data for gating
-        - x: name of X dimension
-        - y: name of Y dimension (optional)
-        - child populations: ChildPopulationCollection (see docs)
-        - output: GateOutput object for standard gating output
+    Parameters
+    ----------
+    data: Pandas.DataFrame
+        FCS data for gating
+    x: str
+        name of X dimension
+    y: str, optional
+        name of Y dimension (optional)
+    child populations: ChildPopulationCollection
+        Collection of populations that gate generates
+    frac: float, optional
+        Fraction of data to sample prior to gating (if None, sampling not performed)
+    downsample_method: str, (default='uniform')
+        If frac is not None, method used for downsampling (must be either 'uniform' or 'density')
+    density_downsample_kwargs: dict, optional
+        Keyword arguments to pass to flow.utilitis.density_dependent_downsample if downsample_method is 'density'
+    transform_x: str, (default='logicle')
+        Method used to transform x-axis
+    transform_y: str, (default='logicle')
+        Method used to transform y-axis
+    low_memory: bool, (default=False)
+        If True, frac is adjusted according to the size of the DataFrame
     """
     def __init__(self,
                  data: pd.DataFrame,
@@ -36,13 +52,6 @@ class Gate:
                  transform_x: str or None = 'logicle',
                  transform_y: str or None = 'logicle',
                  low_memory: bool = False):
-        """
-        Constructor for Gate definition
-        :param data: pandas dataframe of fcs data for gating
-        :param x: name of X dimension
-        :param y: name of Y dimension (optional)
-        :param child_populations: ChildPopulationCollection (see docs)one
-        """
         self.data = data.copy()
         self.x = x
         self.y = y
@@ -54,7 +63,7 @@ class Gate:
             self.data = apply_transform(self.data, features_to_transform=[self.y], transform_method=transform_y)
         self.child_populations = child_populations
         self.warnings = list()
-        self.empty_parent = self.__empty_parent()
+        self.empty_parent = self._empty_parent()
         self.frac = frac
         if low_memory:
             if self.data.shape[0] > 20000:
@@ -67,12 +76,23 @@ class Gate:
                                     'keyword arguments is required as input for density_downsample_kwargs')
         self.density_downsample_kwargs = density_downsample_kwargs
 
-    def sampling(self, data, threshold):
+    def sampling(self,
+                 data: pd.DataFrame,
+                 threshold: float) -> pd.DataFrame or None:
         """
         For a given dataset perform down-sampling
-        :param data: pandas dataframe of events data to downsample
-        :param threshold: threshold below which sampling is not necessary
-        :return: down-sampled data
+
+        Parameters
+        -----------
+        data: Pandas.DataFrame
+            Events data to downsample
+        threshold: float
+            threshold below which sampling is not necessary
+
+        Returns
+        --------
+        Pandas.DataFrame or None
+            Down-sampled data
         """
         if self.frac is None:
             return None
@@ -97,25 +117,40 @@ class Gate:
         else:
             GateError('Invalid input, down-sample_method must be either `uniform` or `density`')
 
-    def __empty_parent(self):
+    def _empty_parent(self):
         """
         Test if input data (parent population) is empty. If the population is empty the child population data will
         be finalised and any gating actions terminated.
-        :return: True if empty, else False.
+
+        Returns
+        --------
+        bool
+            True if empty, else False.
         """
         if self.data.shape[0] == 0:
-            raise GateError('No events in parent population!')
+            raise ValueError('No events in parent population!')
 
-    def child_update_1d(self, threshold: float, method: str, merge_options: str) -> None:
+    def child_update_1d(self, threshold: float,
+                        method: str,
+                        merge_options: str) -> None:
         """
         Internal method. Given a threshold and method generated from 1 dimensional threshold gating, update the objects child
         population collection.
-        :param threshold: threshold value for gate
-        :param method: method used for generating threshold
-        :param merge_options: must have value of 'overwrite' or 'merge'. Overwrite: existing index values in child
-        populations will be overwritten by the results of the gating algorithm. Merge: index values generated from
-        the gating algorithm will be merged with index values currently associated to child populations
-        :return: None
+
+        Parameters
+        -----------
+        threshold: float
+            threshold value for gate
+        method: str
+            method used for generating threshold
+        merge_options: str
+            must have value of 'overwrite' or 'merge'. Overwrite: existing index values in child
+            populations will be overwritten by the results of the gating algorithm. Merge: index values generated from
+            the gating algorithm will be merged with index values currently associated to child populations
+
+        Returns
+        --------
+        None
         """
         neg = self.child_populations.fetch_by_definition('-')
         pos = self.child_populations.fetch_by_definition('+')
@@ -130,14 +165,26 @@ class Gate:
         self.child_populations.populations[pos].update_index(idx=pos_pop.index.values, merge_options=merge_options)
         self.child_populations.populations[neg].update_index(idx=neg_pop.index.values, merge_options=merge_options)
 
-    def child_update_2d(self, x_threshold: float, y_threshold: float, method: str) -> None:
+    def child_update_2d(self,
+                        x_threshold: float,
+                        y_threshold: float,
+                        method: str) -> None:
         """
         Internal method. Given thresholds and method generated from 2 dimensional threshold gating,
         update the objects child population collection.
-        :param x_threshold: threshold value for gate in x-dimension
-        :param y_threshold: threshold value for gate in y-dimension
-        :param method: method used for generating threshold
-        :return: None
+
+        Parameters
+        -----------
+        x_threshold: float
+            threshold value for gate in x-dimension
+        y_threshold: float
+            threshold value for gate in y-dimension
+        method: str
+            method used for generating threshold
+
+        Returns
+        --------
+        None
         """
         xp_idx = self.data[self.data[self.x].round(decimals=2) >= round(x_threshold, 2)].index.values
         yp_idx = self.data[self.data[self.y].round(decimals=2) >= round(y_threshold, 2)].index.values
@@ -172,12 +219,23 @@ class Gate:
                                                                  threshold_y=float(y_threshold), definition=definition,
                                                                  transform_x=self.transform_x, transform_y=self.transform_y)
 
-    def uniform_downsample(self, sample_size: int or float = 0.1,
-                           data: pd.DataFrame or None = None):
+    def uniform_downsample(self,
+                           sample_size: int or float = 0.1,
+                           data: pd.DataFrame or None = None) -> pd.DataFrame:
         """
         Sample associated events data
-        :param frac: fraction of dataset to return as a sample
-        :return: sampled pandas dataframe
+
+        Parameters
+        -----------
+        sample_size: int or float, (default=0.1)
+            fraction or number of events to sample from dataset
+        data: Pandas.DataFrame, optional
+            Optional, if given overrides call to self.data
+
+        Returns
+        --------
+        Pandas.DataFrame
+            sampled pandas dataframe
         """
         if data is None:
             data = self.data
@@ -185,12 +243,21 @@ class Gate:
             return data.sample(n=sample_size)
         return data.sample(frac=sample_size)
 
-    def generate_chunks(self, chunksize):
+    def generate_chunks(self,
+                        chunksize: int) -> list:
         """
         Generate a list of dataframes (chunks) from original data of a target chunksize
-        :param chunksize: target size of chunks (might be smaller or larger than intended value depending on the size
-        of the data)
-        :return: list of pandas dataframes, one for each chunk
+
+        Parameters
+        -----------
+        chunksize: int
+            target size of chunks (might be smaller or larger than intended value depending on the size
+            of the data)
+
+        Returns
+        --------
+        List
+            List of pandas dataframes, one for each chunk
         """
         chunks = list()
         d = np.ceil(self.data.shape[0] / chunksize)
@@ -217,11 +284,21 @@ class Gate:
             chunks.append(sample)
         return chunks
 
-    def generate_polygons(self, data: pd.DataFrame or None = None) -> dict:
+    def generate_polygons(self,
+                          data: pd.DataFrame or None = None) -> dict:
         """
         Generate a dictionary of polygon coordinates and shapely Polygon from clustered data
         objects
-        :return: dictionary of polygon coordinates and dictionary of Polygon shapely objects
+
+        Parameters
+        -----------
+        data: Panda.DataFrame, optional
+            Optional, if DataFrame given, overwrites call to self.data
+
+        Returns
+        --------
+        dict
+            Dictionary of polygon coordinates and dictionary of Polygon shapely objects
         """
         if data is None:
             df = self.data.copy()
