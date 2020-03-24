7c5c98ea7b17f07016ea69f727c6a0bbcf34cfb9
burtonrossj <burtonrj@cardiff.ac.uk>
2020-03-24 17:47:13 +0000

    batch_effects.py docs
    Former-commit-id: 42e3e6663e76d0533dd86178a7b738f171c85869

--- cytopy/flow/batch_effects.py
+++ cytopy/flow/batch_effects.py
@@ -17,7 +17,20 @@ import math
 np.random.seed(42)
 
 
-def jsd_divergence(x, y):
+def jenson_shannon_dist(x: np.array,
+                        y: np.array):
+    """
+    Wrapper for scipy.spatial.distance.jensenshannon
+    Parameters
+    ----------
+    x: Numpy.array
+    y: Numpy.array
+
+    Returns
+    -------
+    float
+        Jenson Shannon Distance
+    """
     div = jsd(x, y)
     assert div is not None, 'JSD is null'
     if div in [np.inf, -np.inf]:
@@ -25,20 +38,71 @@ def jsd_divergence(x, y):
     return div
 
 
-def kl_divergence(x, y):
+def kl_distance(x, y):
+    """
+    Wrapper for scipy.stats.entropy
+    Parameters
+    ----------
+    x: Numpy.array
+    y: Numpy.array
+
+    Returns
+    -------
+    float
+        Kullback–Leibler divergence
+    """
     div = kl(x, y)
     assert div is not None, 'KL divergence is Null'
     return div
 
 
-def indexed_kde(named_x: tuple, kde_f: callable):
+def indexed_kde(named_x: tuple,
+                kde_f: callable):
+    """
+    Utility function for mapping KDE function across multiple data-sets; returns tuple
+    (key, PDF) so that the resulting PDFs can be sorted after calculation.
+
+    Parameters
+    ----------
+    named_x: tuple
+        (key, data)
+    kde_f: callable
+        Some function for estimating PDF
+
+    Returns
+    -------
+    tuple
+        (key, PDF)
+    """
     q = kde_f(named_x[1])
     return named_x[0], q
 
 
 class EvaluateBatchEffects:
-    def __init__(self, experiment: FCSExperiment, root_population: str, samples: list or None = None,
-                 transform: str = 'logicle', scale: str or None = None,
+    """
+    Evaluate the batch effects present in an experiment. Contains utility functions for visualising the
+    total variation amongst collected samples.
+
+    Parameters
+    -----------
+    experiment: FCSExperiment
+        Experiment to evaluate
+    root_population: str
+        Root population; population to be retrieved from each sample for comparison
+    samples: list, optional
+        List of samples to include in analysis, if None given all samples will be included
+    transform: str, (default='logicle')
+        Method used to transform data prior to analysis
+    scale: str, optional
+        Optional parameter to scale data prior to analysis
+    sample_n: int, optional, (default=10000)
+        Number of events to sample from each file in experiment
+    """
+    def __init__(self, experiment: FCSExperiment,
+                 root_population: str,
+                 samples: list or None = None,
+                 transform: str = 'logicle',
+                 scale: str or None = None,
                  sample_n: str or None = 10000):
         self.experiment = experiment
         self.sample_ids = samples or experiment.list_samples()
@@ -52,7 +116,11 @@ class EvaluateBatchEffects:
     def load_data(self) -> dict:
         """
         Load new dataset from given FCS Experiment
-        :return:
+
+        Returns
+        --------
+        dict
+            {sample_id: Pandas.DataFrame}
         """
         self.kde_cache = dict()
         lt = partial(ordered_load_transform,
@@ -72,16 +140,29 @@ class EvaluateBatchEffects:
         pool.join()
         return samples
 
-    def marker_variance(self, reference_id: str, comparison_samples: list, markers: list or None = None,
+    def marker_variance(self,
+                        reference_id: str,
+                        comparison_samples: list,
+                        markers: list or None = None,
                         figsize: tuple = (10, 10)):
         """
         For a given reference sample and a list of markers of interest, create a grid of KDE plots with the reference
         sample given in red and comparison samples given in blue.
-        :param reference_id: this sample will appear in red on KDE plots
-        :param markers: list of valid marker names to plot in KDE grid
-        :param comparison_samples: list of valid sample names in the current experiment
-        :param figsize: size of resulting figure
-        :return: Matplotlib figure
+
+        Parameters
+        -----------
+         reference_id: str 
+            this sample will appear in red on KDE plots
+         markers: list, optional
+            list of valid marker names to plot in KDE grid
+         comparison_samples: list 
+            list of valid sample names in the current experiment
+        figsize: tuple, (default=(10,10))
+            size of resulting figure
+        
+        Returns
+        --------
+        Matplotlib figure
         """
         fig = plt.figure(figsize=figsize)
         assert reference_id in self.sample_ids, 'Invalid reference ID for experiment currently loaded'
@@ -112,18 +193,34 @@ class EvaluateBatchEffects:
         fig.tight_layout()
         fig.show()
 
-    def dim_reduction_grid(self, reference_id, comparison_samples: list, features: list, figsize: tuple = (10, 10),
-                           method: str = 'PCA', kde: bool = False):
+    def dim_reduction_grid(self,
+                           reference_id: str,
+                           comparison_samples: list,
+                           features: list,
+                           figsize: tuple = (10, 10),
+                           method: str = 'PCA',
+                           kde: bool = False):
         """
         Generate a grid of embeddings using a valid dimensionality reduction technique, in each plot a reference sample
         is shown in blue and a comparison sample in red. The reference sample is conserved across all plots.
-        :param reference_id:
-        :param comparison_samples:
-        :param features:
-        :param figsize:
-        :param method:
-        :param kde:
-        :return:
+
+        reference_id: str
+            This sample will appear in red as a comparison
+        comparison_samples: list
+            List of samples to compare to reference (blue)
+        features: list
+            List of features to use for dimensionality reduction
+        figsize: tuple, (default=(10,10))
+            Size of figure
+        method: str, (default='PCA')
+            Method to use for dimensionality reduction (see flow.dim_reduction)
+        kde: bool, (default=False)
+            If True, overlay with two-dimensional PDF estimated by KDE
+
+        Returns
+        -------
+        None
+            Plot printed to stdout
         """
         fig = plt.figure(figsize=figsize)
         nrows = math.ceil(len(comparison_samples)/3)
@@ -165,11 +262,38 @@ class EvaluateBatchEffects:
         fig.tight_layout()
         fig.show()
 
-    def divergence_barplot(self, target_id: str, comparisons: list,
-                           figsize: tuple = (8, 8),kde_kernel: str = 'gaussian',
+    def divergence_barplot(self,
+                           target_id: str,
+                           comparisons: list,
+                           figsize: tuple = (8, 8),
+                           kde_kernel: str = 'gaussian',
                            divergence_method: str = 'hellinger',
                            verbose: bool = False,
                            **kwargs):
+        """
+        Generate a barplot of statistical distance to some reference sample (target).
+
+        Parameters
+        ----------
+        target_id: str
+            Sample to use as reference
+        comparisons: list
+            List of samples to be included in analysis and compared to reference
+        figsize: tuple, (default=(8,8))
+            Size of barplot figure
+        kde_kernel: str, (default='gaussian')
+            Name of kernel to use for kernel density estimation that generates PDF for statistical distance calculation
+        divergence_method: str, (default='hellinger')
+            Name of method to use for statistical distance
+        verbose: bool, (default=False)
+            Feedback
+        kwargs:
+            Keyword arguments to pass to Seaborn.barplot
+
+        Returns
+        -------
+        None
+        """
         divergence = self.calc_divergence(target_id=target_id,
                                           kde_kernel=kde_kernel,
                                           divergence_method=divergence_method,
@@ -191,25 +315,39 @@ class EvaluateBatchEffects:
         ax.set_ylabel('Sample ID')
         fig.show()
 
-    def divergence_matrix(self, exclude: list or None = None, figsize: tuple = (12, 12),
-                          kde_kernel: str = 'gaussian', divergence_method: str = 'jsd',
-                          clustering_method: str = 'average', **kwargs):
+    def divergence_matrix(self,
+                          exclude: list or None = None,
+                          figsize: tuple = (12, 12),
+                          kde_kernel: str = 'gaussian',
+                          divergence_method: str = 'jsd',
+                          clustering_method: str = 'average',
+                          **kwargs):
         """
         Generate a clustered heatmap of pairwise statistical distance comparisons. This can be used to find
         samples of high similarity and conversely demonstrates samples that greatly differ.
         Returns a linkage matrix that can be given to scipy.cluster.hierarchy.cut_tree to subset samples.
         Also returns Seaborn ClusterGrid instance.
-        :param exclude: list of sample IDs to be omitted from plot
-        :param figsize: size of resulting Seaborn clusterplot figure
-        :param kde_kernel: name of kernel to use for density estimation (default = 'gaussian')
-        :param divergence_method: name of statistical distance metric to use; valid choices are:
+
+        Parameters
+        -----------
+        exclude: list, optional
+            list of sample IDs to be omitted from plot
+        figsize: tuple, (default=(12,12)
+            size of resulting Seaborn clusterplot figure
+        kde_kernel: str, (default='guassian')
+            name of kernel to use for density estimation (default = 'gaussian')
+        divergence_method: str, (default='jsd')
+            name of statistical distance metric to use; valid choices are:
             *jsd: squared Jensen-Shannon Divergence (default)
             *kl: Kullback–Leibler divergence (relative entropy); warning, asymmetrical
             *hellinger: squared Hellinger Divergence
-        :param clustering_method: method for hierarchical clustering, see scipy.cluster.hierarchy.linkage for details
-        :param kwargs: additional keyword arguments to be passed to Seaborn ClusterPlot
-        (seaborn.pydata.org/generated/seaborn.clustermap.html#seaborn.clustermap)
-        :return: (hierarchical clustering encoded as a linkage matrix, array of sample IDs, Seaborn ClusterGrid instance)
+        clustering_method: str, (default='average')
+            method for hierarchical clustering, see scipy.cluster.hierarchy.linkage for details
+        kwargs:
+            additional keyword arguments to be passed to Seaborn ClusterPlot
+            (seaborn.pydata.org/generated/seaborn.clustermap.html#seaborn.clustermap)
+            (hierarchical clustering encoded as a linkage matrix, array of sample IDs,
+            Seaborn ClusterGrid instance)
         """
         samples = self.sample_ids
         if exclude is not None:
@@ -249,24 +387,36 @@ class EvaluateBatchEffects:
         ax.set_ylabel('')
         return row_linkage, divergence_df.sample_id.values, g
 
-    def calc_divergence(self, target_id: str, comparisons: list, kde_kernel: str = 'gaussian',
-                        divergence_method: str = 'jsd', verbose: bool = False) -> np.array:
+    def calc_divergence(self,
+                        target_id: str,
+                        comparisons: list,
+                        kde_kernel: str = 'gaussian',
+                        divergence_method: str = 'jsd',
+                        verbose: bool = False) -> np.array:
         """
         Calculate the statistical distance between the probability density function of a target sample and one or many
         comparison samples.
-        :param target_id: sample ID for PDF p
-        :param root_population: name of population to retrieve samples from
-        :param comparisons: list of sample IDs that will form PDF q
-        :param sample_n: number of cells to sample from each (optional but recommended; default = 10000)
-        :param transform: method used to transform the data prior to processing (default = 'logicle')
-        :param scale: scaling function to apply to data post-transformation (optional; default = None)
-        :param kde_kernel: name of kernel to use for density estimation (default = 'gaussian')
-        :param divergence_method: name of statistical distance metric to use; valid choices are:
+
+        Parameters
+        -----------
+        target_id: str
+            sample ID for PDF p
+        comparisons: list
+            list of sample IDs that will form PDF q
+        kde_kernel: str, (default='gaussian')
+            name of kernel to use for density estimation (default = 'gaussian')
+        divergence_method: str, (default='jsd')
+            name of statistical distance metric to use; valid choices are:
             *jsd: squared Jensen-Shannon Divergence (default)
             *kl: Kullback–Leibler divergence (relative entropy); warning, asymmetrical
             *hellinger: squared Hellinger distance
-        :param verbose: If True, function will return regular feedback (default = False)
-        :return: List of tuples in format (SAMPLE_NAME, DIVERGENCE)
+        verbose: bool, (default=False)
+            If True, function will return regular feedback (default = False)
+
+        Returns
+        --------
+        list
+            List of tuples in format (SAMPLE_NAME, DIVERGENCE)
         """
         assert divergence_method in ['jsd', 'kl', 'hellinger'], 'Invalid divergence metric must be one of ' \
                                                                 '[jsd, kl, hellinger]'
@@ -299,7 +449,7 @@ class EvaluateBatchEffects:
         for name, q in q_:
             self.kde_cache[name] = q
         if divergence_method == 'jsd':
-            return [(name, jsd_divergence(self.kde_cache[target_id], q)) for name, q in self.kde_cache.items()]
+            return [(name, jenson_shannon_dist(self.kde_cache[target_id], q)) for name, q in self.kde_cache.items()]
         if divergence_method == 'kl':
-            return [(name, kl_divergence(self.kde_cache[target_id], q)) for name, q in self.kde_cache.items()]
+            return [(name, kl_distance(self.kde_cache[target_id], q)) for name, q in self.kde_cache.items()]
         return [(name, hellinger_dot(self.kde_cache[target_id], q)) for name, q in self.kde_cache.items()]
