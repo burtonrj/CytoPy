0f11006b3374b07c5f5e783a32821ba4cff76ae9
burtonrossj <burtonrj@cardiff.ac.uk>
2020-03-24 14:51:22 +0000

    Refactor docs in KNN
    Former-commit-id: 600240944c4c1cf0aa657b11e77dbdd51128d286 [formerly 813003726ce55d0eb0237d4851138eab656a3b61] [formerly cbb33234ae241936f936d71f3fffee5aeeadd026] [formerly de7f8ca91c4a62e4de73d13b3de6cf9f8fa24dd7 [formerly e035570d1d74d5ee0dbe50ae365155049d260565] [formerly cbb33234ae241936f936d71f3fffee5aeeadd026 [formerly cbb33234ae241936f936d71f3fffee5aeeadd026 [formerly f3ec8d873b642a1c2502b5ca038fa0dc080227cd]]]]
    Former-commit-id: af1394e786cd957f7bbe5de40d4965ec93260b01 [formerly eda9ab0fdd69da229adc2ac1a66885b901f35e77] [formerly ca01514f344f0d80ea4a9340d5caa5048077e912 [formerly 85136bde9aa6fb47e35f41c37351e022a9d746cf]]
    Former-commit-id: 8685d5718eee240912b09c6b832508f38f0a0ee3 [formerly ca01514f344f0d80ea4a9340d5caa5048077e912]
    Former-commit-id: 23e1dcbdbd802a3e8605ae96597b4ffb0e46b608
    Former-commit-id: 6dd550777fdb6b6e7974ea58150e1f9576244673

--- cytopy/flow/supervised/knn.py
+++ cytopy/flow/supervised/knn.py
@@ -4,7 +4,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 class KNN(CellClassifier):
     """
-    Implement a K Nearest Neighbours classifier for the purpose of predicting cell classification in flow cytometry data
+    Implement a K Nearest Neighbours classifier for the purpose of predicting cell classification in cytometry data
     """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -17,7 +17,14 @@ class KNN(CellClassifier):
         """
         Build KNN model. Implements Scikit-Learn's  k-nearest neighbors classifier, see
         https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
-        :param kwargs: additional keyword arguments for KNeighborsClassifier (see sklearn documentation)
-        :return: None
+
+        Parameters
+        -----------
+        kwargs:
+            additional keyword arguments for KNeighborsClassifier (see sklearn documentation)
+
+        Returns
+        --------
+        None
         """
         self.classifier = KNeighborsClassifier(**kwargs)
