1366e729e5b886c990a92cf32048f92c790ff803
burtonrossj <burtonrj@cardiff.ac.uk>
2020-03-24 15:38:03 +0000

    docs for svm.py
    Former-commit-id: 1a3d994a1b550e12523778a599c5c68de2e97e58 [formerly 1a3d994a1b550e12523778a599c5c68de2e97e58 [formerly f804cd153aac7d403f849b2b466ac004ebb8ec9e]]
    Former-commit-id: cdc610e9cc4be3b8ef52add16fba72df6a3bec80
    Former-commit-id: b8983d8c57d43c680382d0739f3fcf911720bcd7

--- cytopy/flow/supervised/svm.py
+++ cytopy/flow/supervised/svm.py
@@ -5,7 +5,7 @@ from sklearn.multiclass import OneVsRestClassifier
 
 class SupportVectorMachine(CellClassifier):
     """
-    Implement a Support Vector Machine for the purpose of predicting cell classification in flow cytometry data
+    Implement a Support Vector Machine for the purpose of predicting cell classification in cytometry data
     """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -16,25 +16,41 @@ class SupportVectorMachine(CellClassifier):
         Build linear SVM. Implements Scikit-Learn's Linear Support Vector Classification, see
         https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html
         Implements a one-verus-rest strategy for handling multi-class classificaiton.
-        :param kwargs: additional keyword arguments for LinearSVC (see sklearn documentation)
-        :return: None
+
+        Parameters
+        -----------
+        kwargs: additional keyword arguments for LinearSVC (see sklearn documentation)
+
+        Returns
+        --------
+        None
         """
         if self.class_weights is None:
             self.classifier = LinearSVC(multi_class='ovr', **kwargs)
         else:
             self.classifier = LinearSVC(multi_class='ovr', class_weight=self.class_weights, **kwargs)
 
-    def build_nonlinear(self, kernel: str or callable = 'poly', cache_size: float = 4000, **kwargs) -> None:
+    def build_nonlinear(self,
+                        kernel: str or callable = 'poly',
+                        cache_size: float = 4000, **kwargs) -> None:
         """
         Build non-linear SVM. Implements Scikit-Learn's C-Support Vector Classification, see
         http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
-
         Implements one-versus-rest strategy for handling multi-class classification.
-        :param kernel: kernel type to be used by the algorithm, must be one of ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’,
-        or a function (custom kernel).
-        :param cache_size: Size of kernel cache (default = 4000 MB)
-        :param kwargs: additional keyword arguments for SVC (see sklearn documentation)
-        :return: None
+
+        Parameters
+        -----------
+        kernel: str or callable, (defauly='poly')
+            kernel type to be used by the algorithm, must be one of ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’,
+            or a function (custom kernel).
+        cache_size: float, (default=4000)
+            Size of kernel cache (default = 4000 MB)
+        kwargs:
+            additional keyword arguments for SVC (see sklearn documentation)
+
+        Returns
+        --------
+        None
         """
         if type(kernel) != callable:
             assert kernel in ['poly', 'rbf', 'sigmoid'], "unsupported kernel type, must be one of " \
