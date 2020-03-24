e2f1e2f786c774a18824e965fb94d6e4dc7b8e2e
burtonrossj <burtonrj@cardiff.ac.uk>
2020-03-24 18:54:13 +0000

    utilites.py docs
    Former-commit-id: 6423b6344fe2ab458831b82d991947e8aeed1b9d [formerly 08a40889bcee0925f9d8fab3c98b3015dbcce2ff] [formerly a09a609ddcde6d5a5f6e9fcf42251b337f898c4b [formerly 55359963a1d47c3363780ad98be44792a328e397]] [formerly 7b1753775fbcc880adc5fb875163d67a9a2e21a4 [formerly 164a20f4bcb9923e7e7cb53a33f8ab604bb60f04] [formerly a299e850b5bd1928c1b99c08cf0b2b62ac5d138c [formerly fc78721eb119cf4afbc7cf024eb1915474972cbc]]] [formerly 3dbea978b35e135423c0ecdb55aa8b163cf31fbd [formerly aa5e2d6c9b30898410b2f01d76d83ecbddb63b31] [formerly 2ae820df81d985d0d91a9a00841d569025d925c5 [formerly e4323939870fd5637f3db10b510cbd0386babca8]] [formerly a299e850b5bd1928c1b99c08cf0b2b62ac5d138c [formerly fc78721eb119cf4afbc7cf024eb1915474972cbc] [formerly 70273c8fb2f9215a2f41693460ebadb3e241a419 [formerly bb159e626c1dc48483c8c4786531985adfaed3cb] [formerly a6327ff925f7e9e0c82e6ffc30fe4c8aa074ad40] [formerly fc78721eb119cf4afbc7cf024eb1915474972cbc [formerly fc78721eb119cf4afbc7cf024eb1915474972cbc [formerly e9b1eb9444b9b461305a9bf74ae039b9604f4a14]]]]]]
    Former-commit-id: a1f0feeca88f5ad9ecc5d4d674c268758e6348d2 [formerly 7ea635f6728ca7d59f162e6282705287f686b138] [formerly c50503d5a0f22c5028501d2131be23f9207881fe [formerly 1affa0c804ec5431b8c7b4feeef05cb86b9c1141]] [formerly 11d6d7b6915659af192ad877e39cdd2c26dd6c5d [formerly a6175a76f3e5677b35e9bbf8886dba53c963acb9] [formerly 51886b2108f8d317414189b26b52c8870ebc3f14 [formerly 42184fde5ae71c8bbf4eafd40165a99d228f3a14]]]
    Former-commit-id: 5e1a8a50ba649dcb01e1274e2b1845fdac0848be [formerly 0f3e3d301cccf3cce63afcb8ddc78c32e0394e6c] [formerly 11d6d7b6915659af192ad877e39cdd2c26dd6c5d [formerly a6175a76f3e5677b35e9bbf8886dba53c963acb9]]
    Former-commit-id: cfbb9c93eaa5fda00262961ec5b113e8b207829d [formerly 6d663e3ed94620edb6daeeeb67d47dc35fbe16ee]
    Former-commit-id: 264e505e28ee8b49cdf2586f294d356a6839ced1 [formerly e2e4b80df80e1ebc7036b3971f161d8088326fbb [formerly c4fa7b2105125593239821bb55639c0981ddfb80]]
    Former-commit-id: 3a0fa392fac97fdaf8172e59918a89226b841cb0
    Former-commit-id: 65913a63a00d9969d8a7f74b02648b9e83078d01

--- cytopy/flow/utilities.py
+++ cytopy/flow/utilities.py
@@ -8,13 +8,24 @@ from .gating.actions import Gating
 from .supervised.utilities import scaler
 
 
-def faithful_downsampling(data: np.array, h: float):
+def faithful_downsampling(data: np.array,
+                          h: float):
     """
     An implementation of faithful downsampling as described in:  Zare H, Shooshtari P, Gupta A, Brinkman R.
-    Data reduction for spectral clustering to analyze high throughput flow cytometry data. BMC Bioinformatics 2010;11:403
-    :param data: numpy array to be downsampled
-    :param h: radius for nearest neighbours search
-    :return: Downsampled array
+    Data reduction for spectral clustering to analyze high throughput flow cytometry data. 
+    BMC Bioinformatics 2010;11:403
+    
+    Parameters
+    -----------
+    data: Numpy.array
+        numpy array to be down-sampled
+    h: float
+        radius for nearest neighbours search
+
+    Returns
+    --------
+    Numpy.array
+        Down-sampled array
     """
     communities = None
     registered = np.zeros(data.shape[0])
@@ -32,38 +43,70 @@ def faithful_downsampling(data: np.array, h: float):
     return communities
 
 
-def hellinger_dot(p: np.array, q: np.array) -> np.float:
+def hellinger_dot(p: np.array,
+                  q: np.array) -> np.float:
     """
     Hellinger distance between two discrete distributions.
     Original code found here: https://nbviewer.jupyter.org/gist/Teagum/460a508cda99f9874e4ff828e1896862
-    :param p: discrete probability distribution, p
-    :param q: discrete probability distribution, q
-    :return: Hellinger Distance
+
+    Parameters
+    -----------
+    p: Numpy.array
+        discrete probability distribution, p
+    q: Numpy.array
+        discrete probability distribution, q
+
+    Returns
+    --------
+    Numpy.float
+        Hellinger Distance
     """
     z = np.sqrt(p) - np.sqrt(q)
     return np.sqrt(z @ z / 2)
 
 
-def jsd_divergence(p: np.array, q: np.array) -> np.float:
+def jsd_divergence(p: np.array,
+                   q: np.array) -> np.float:
     """
     Calculate the Jensen-Shannon Divergence between two PDFs
-    :param p: discrete probability distribution, p
-    :param q: discrete probability distribution, q
-    :return: Jenson-Shannon Divergence
+
+    Parameters
+    -----------
+    p: Numpy.array
+        discrete probability distribution, p
+    q: Numpy.array
+        discrete probability distribution, q
+
+    Returns
+    --------
+    Numpy.float
+        Jenson-Shannon Divergence
     """
     m = (p + q)/2
     divergence = (kl_divergence(p, m) + kl_divergence(q, m)) / 2
     return np.sqrt(divergence)
 
 
-def kde_bandwidth_cv(x, bandwidth_search: tuple or None = None, cv: int = 20):
+def kde_bandwidth_cv(x,
+                     bandwidth_search: tuple or None = None,
+                     cv: int = 20):
     """
     Estimate best bandwidth for KDE using cross validation
-    :param x: data for KDE
-    :param bandwidth_search:  tuple specifying range of bandwidth values to search (start, end) in cross validation;
-    if value is None, 5th and 95th quartile of data is used for lower ad upper limit respectively
-    :param cv: number of folds to use in cross validation (default = 20)
-    :return: Optimal bandwidth
+
+    Parameters
+    -----------
+    x:
+        data for KDE
+    bandwidth_search: tuple, optional
+        tuple specifying range of bandwidth values to search (start, end) in cross validation;
+        if value is None, 5th and 95th quartile of data is used for lower ad upper limit respectively
+    cv: int, (default=20)
+        number of folds to use in cross validation (default = 20)
+
+    Returns
+    --------
+    float
+        Optimal bandwidth
     """
     bandwidth_search = bandwidth_search or (np.quantile(x, 0.05), np.quantile(x, 0.95))
     if bandwidth_search[0] == 0:
@@ -75,22 +118,35 @@ def kde_bandwidth_cv(x, bandwidth_search: tuple or None = None, cv: int = 20):
     return grid.best_estimator_.bandwidth
 
 
-def kde_multivariant(x: np.array, bandwidth: str or float = 'cross_val',
+def kde_multivariant(x: np.array,
+                     bandwidth: str or float = 'cross_val',
                      bandwidth_search: tuple or None = None,
-                     bins: int or None = 1000, return_grid: bool = False,
+                     bins: int or None = 1000,
                      **kwargs) -> np.array:
     """
     Perform Kernel Density Estimation for a multivariant data. Function is a wrapper for methods provided by
     the scikit-learn library. See scikit-learn documentation for available kernels (default = gaussian).
     Cross-validation available for bandwidth search by setting bandwidth argument to 'cross_val' otherwise
     a float value is expected.
-    :param x: data to perform KDE upon; if 1 dimensional data must be reshaped e.g. np.array.reshape(-1, 1)
-    :param bandwidth: either float value for bandwidth or 'cross-val' to estimate bandwidth using cross-validation
-    :param bandwidth_search: tuple specifying range of bandwidth values to search (start, end) in cross validaiton;
-    ignored if bandwidth != 'cross_val'
-    :param bins: bin size for generating grid of sample locations for scoring probability estimate (defaults to 100)
-    :param kwargs: additional keyword arguments to pass to sklearn.neighbors.KernelDensity
-    :return: Probability density estimate
+
+    Parameters
+    -----------
+    x: Numpy.array
+        data to perform KDE upon; if 1 dimensional data must be reshaped e.g. np.array.reshape(-1, 1)
+    bandwidth: str or float, (default='cross_val')
+        either float value for bandwidth or 'cross-val' to estimate bandwidth using cross-validation
+    bandwidth_search: tuple, optional
+        tuple specifying range of bandwidth values to search (start, end) in cross validaiton;
+        ignored if bandwidth != 'cross_val'
+    bins: int, (default=1000)
+        bin size for generating grid of sample locations for scoring probability estimate
+    kwargs:
+        additional keyword arguments to pass to sklearn.neighbors.KernelDensity
+
+    Returns
+    --------
+    Numpy.array
+        Probability density estimate
     """
     if type(bandwidth) == str:
         assert bandwidth == 'cross_val', 'Invalid input for bandwidth, must be either float or "cross_val"'
@@ -106,19 +162,35 @@ def kde_multivariant(x: np.array, bandwidth: str or float = 'cross_val',
     return np.exp(log_pdf)
 
 
-def ordered_load_transform(sample_id: str, experiment: FCSExperiment, root_population: str, transform: str,
-                           scale: str or None = None, sample_n: int or None = None) -> (str, pd.DataFrame or None):
+def ordered_load_transform(sample_id: str,
+                           experiment: FCSExperiment,
+                           root_population: str,
+                           transform: str,
+                           scale: str or None = None,
+                           sample_n: int or None = None) -> (str, pd.DataFrame or None):
     """
     Wrapper function for load_and_transform that adds convenience for multi-processing (data can be ordered post-hoc);
     returns a tuple, first element is the subject ID and the second element the population dataframe.
 
-    :param experiment: Experiment object that sample belongs to
-    :param sample_id: ID for sample to load
-    :param root_population: name of root population to load from sample
-    :param transform: name of transformation method to apply (if None, data is returned untransformed)
-    :param scale: name of scalling method to apply after transformation (if None, no scaling is applied)
-    :param sample_n: number of events to return (sample is uniform; if None, no sampling occurs)
-    :return: sample_id, population dataframe
+    Parameters
+    -----------
+    experiment: FCSExperiment
+        Experiment object that sample belongs to
+    sample_id: str
+        ID for sample to load
+    root_population: str
+        name of root population to load from sample
+    transform: str
+        name of transformation method to apply (if None, data is returned untransformed)
+    scale: str, optional
+        name of scalling method to apply after transformation (if None, no scaling is applied)
+    sample_n: int, optional
+        number of events to return (sample is uniform; if None, no sampling occurs)
+
+    Returns
+    --------
+    str, Pandas.DataFrame or None
+        sample_id, population DataFrame
     """
     try:
         data = load_and_transform(sample_id, experiment, root_population, transform,
@@ -129,17 +201,34 @@ def ordered_load_transform(sample_id: str, experiment: FCSExperiment, root_popul
     return sample_id, data
 
 
-def load_and_transform(sample_id: str, experiment: FCSExperiment, root_population: str, transform: str or None,
-                       scale: str or None = None, sample_n: int or None = None) -> pd.DataFrame or None:
+def load_and_transform(sample_id: str,
+                       experiment: FCSExperiment,
+                       root_population: str,
+                       transform: str or None,
+                       scale: str or None = None,
+                       sample_n: int or None = None) -> pd.DataFrame or None:
     """
     Standard function for loading data from an experiment, transforming, scaling, and sampling.
-    :param experiment: Experiment object that sample belongs to
-    :param sample_id: ID for sample to load
-    :param root_population: name of root population to load from sample
-    :param transform: name of transformation method to apply (if None, data is returned untransformed)
-    :param scale: name of scalling method to apply after transformation (if None, no scaling is applied)
-    :param sample_n: number of events to return (sample is uniform; if None, no sampling occurs)
-    :return: Population DataFrame
+    experiment: Experiment object that sample belongs to
+
+    Parameters
+    -----------
+    sample_id: str
+        ID for sample to load
+    experiment: FCSExperiment
+        Experiment of interest
+    root_population: str
+        name of root population to load from sample
+    transform: str, optional
+        name of transformation method to apply (if None, data is returned untransformed)
+    scale: str, optional
+        name of scalling method to apply after transformation (if None, no scaling is applied)
+    sample_n: int, optional
+        number of events to return (sample is uniform; if None, no sampling occurs)
+
+    Returns
+    --------
+    Population DataFrame or None
     """
     gating = Gating(experiment=experiment, sample_id=sample_id, include_controls=False)
     if transform is None:
