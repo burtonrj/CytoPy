dd46ac68bf7ca9b630683b34911a10ff7f1c6c03
burtonrossj <burtonrj@cardiff.ac.uk>
2020-03-24 14:54:44 +0000

    Docs for XGBoost
    Former-commit-id: 1d7dd5c71e2d3c6097b0314ca6036e109cea76a7 [formerly e67f0c419dbe6843e1c2f3de773267c4f278adcb] [formerly e5c9ddbdadc886d4e65d08f4d4772fad0d267a28] [formerly 4383499632beae2e1f6d1c2d33866fac4f5d91f5 [formerly bfd8cf04da70c7a24c0c2c901574faeb3e8a4bad] [formerly e5c9ddbdadc886d4e65d08f4d4772fad0d267a28 [formerly e5c9ddbdadc886d4e65d08f4d4772fad0d267a28 [formerly ec9d7cba1210d2f6e40cc187fae7b4d3c1fc7bbe]]]]
    Former-commit-id: 45f757e554e964cac4700ad48e149e321855d1b8 [formerly 13995e6e9a7dc14a1036f729a0efabbfd2d0acf9] [formerly a20e4f0e4e0e933d6e5d6c27176eaf219ed3dc26 [formerly 19777e93ca151e403df6a5a1f8b44418b81f143b]]
    Former-commit-id: 98567a945c04fa597165ba8022362ea53d21b5b6 [formerly a20e4f0e4e0e933d6e5d6c27176eaf219ed3dc26]
    Former-commit-id: 75b846690c4ba53f4f4ec76a3f44d29c76505cb3
    Former-commit-id: 05ca0537900db98dc077a005ae55117ab521229c

--- cytopy/flow/supervised/xgboost.py
+++ cytopy/flow/supervised/xgboost.py
@@ -1,10 +1,11 @@
 from .cell_classifier import CellClassifier
 from xgboost import XGBClassifier
+import numpy as np
 
 
 class XGBoostClassifier(CellClassifier):
     """
-    Implement an XGBoost classifier for the purpose of predicting cell classification in flow cytometry data
+    Implement an XGBoost classifier for the purpose of predicting cell classification in cytometry data
     """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -17,9 +18,16 @@ class XGBoostClassifier(CellClassifier):
         """
         Build an XGBoost model. Implements XGBoost classifier using the Sklearn wrapper, see
         https://xgboost.readthedocs.io/en/latest/python/python_api.html#module-xgboost.sklearn
-        :param kwargs: additional keyword arguments for XGBClassifier
-        (see https://xgboost.readthedocs.io/en/latest/parameter.html)
-        :return: None
+
+        Parameters
+        -----------
+        kwargs:
+            additional keyword arguments for XGBClassifier
+            (see https://xgboost.readthedocs.io/en/latest/parameter.html)
+
+        Returns
+        --------
+        None
         """
         if self.multi_label:
             self.classifier = XGBClassifier(objective=self.objective,
@@ -28,7 +36,27 @@ class XGBoostClassifier(CellClassifier):
         else:
             self.classifier = XGBClassifier(objective=self.objective, **kwargs)
 
-    def _fit(self, x, y, **kwargs):
+    def _fit(self,
+             x: np.array,
+             y: np.array,
+             **kwargs):
+        """
+        Overwrites fit from base class for XGBoost specific functionality
+
+        Parameters
+        ----------
+        x: Numpy.array
+            Feature space
+        y: Numpy.array
+            Labels
+        kwargs:
+            Additional keyword arguments to pass to call to MODEL.fit()
+            (see see https://xgboost.readthedocs.io)
+
+        Returns
+        -------
+        None
+        """
         assert self.classifier is not None, 'Must construct classifier prior to calling `fit` using the `build` method'
         if self.class_weights is not None:
             self.classifier.fit(x, y, sample_weight=self.class_weights, **kwargs)
