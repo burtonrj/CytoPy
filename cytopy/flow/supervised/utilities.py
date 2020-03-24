4b0b0c08cc8186052ed42c93d0fbda7cdc61b08c
burtonrossj <burtonrj@cardiff.ac.uk>
2020-03-24 15:16:11 +0000

    Refactor get_common_features; no longer fetches dataframes and infers from columns, instead uses the channel_mappings associated to each file; much quicker!
    Former-commit-id: d78130d3b9242f631f1b4de2a87df8a1e4e79b45 [formerly 987db88b8f8c6d9e31fabb4efc7b08e4545c49f4] [formerly ea2be615f21b2242c26bce3cfb1a67e9b6e85fb7] [formerly 39b4f3f156cce4af3112eb113b915b3766b96daf [formerly 7bd8374af7ed8cf04cbfbebf8d4a42e2edf53572] [formerly ea2be615f21b2242c26bce3cfb1a67e9b6e85fb7 [formerly ea2be615f21b2242c26bce3cfb1a67e9b6e85fb7 [formerly b0721f396635159875b401b37c28a43dff0e227c]]]]
    Former-commit-id: 6ca676067bdece7a73de0c3e05a80be60842f5a0 [formerly 0f9e56cc4ca5d9262281338434107059a0e8fbcf] [formerly a36c755713b1a2a0026c5f0b3db8c8f359721263 [formerly 9105780c2c66c7b9840db81ffaeafd2f3ddb33af]]
    Former-commit-id: 76cff801b99d836a882941191f863def33b69bf4 [formerly a36c755713b1a2a0026c5f0b3db8c8f359721263]
    Former-commit-id: 6aec51de87194b331e8f7bc24bae4cbf3e1003a9
    Former-commit-id: e8884341dddfdc17ed1d3b40344418e6783a250a

--- cytopy/flow/supervised/utilities.py
+++ cytopy/flow/supervised/utilities.py
@@ -6,24 +6,65 @@ from functools import partial
 import numpy as np
 
 
-def random_oversampling(x, y):
+def random_oversampling(x: np.array,
+                        y: np.array):
+    """
+    Wrapper for imblearn.over_sampling.RandomOverSampler
+
+    Parameters
+    ----------
+    x: Numpy.array
+        Feature space
+    y: Numpy.array
+        Labels
+
+    Returns
+    -------
+    (Numpy.array, Numpy.array)
+        Re-sampled data with unrepresented classes randomly oversampled
+    """
     ros = RandomOverSampler(random_state=42)
     return ros.fit_resample(x, y)
 
 
-def __pull_features(sid, experiment):
-    d = experiment.pull_sample_data(sample_id=sid, include_controls=False)
-    d = [x for x in d if x['typ'] == 'complete'][0]['data']
-    return list(d.columns)
+def _genetate_feature_list(channel_mappings: list):
+    features = list()
+    for cm in channel_mappings:
+        if cm.marker:
+            features.append(cm.marker)
+        else:
+            features.append(cm.channel)
+    return features
+
+
+def _get_features(experiment: FCSExperiment,
+                  sample_id: str or None = None):
+    """
+    For a given sample in a given experiment, return the list of
+    Parameters
+    ----------
+    sid
+    experiment
+
+    Returns
+    -------
+
+    """
+    if sample_id is None:
+        return _genetate_feature_list(experiment.panel.mappings)
+    assert sample_id in experiment.list_samples(), f'{sample_id} not found in experiment {experiment.experiment_id}'
+    sample = experiment.pull_sample(sample_id)
+    return _genetate_feature_list(sample.files[0].channel_mappings)
 
 
 def find_common_features(experiment: FCSExperiment, samples: list or None = None):
-    pool = Pool(cpu_count())
-    assert all([s in experiment.list_samples() for s in samples]), 'One or more samples specified do not belong to experiment'
-    pull = partial(__pull_features, experiment=experiment)
-    all_features = pool.map(pull, samples)
-    common_features = set(all_features[0])
-    for f in all_features[1:]:
+    if samples is None:
+        samples = experiment.list_samples()
+    assert all([s in experiment.list_samples() for s in samples]), \
+        'One or more samples specified do not belong to experiment'
+    features = [_get_features(experiment, sample_id=s) for s in samples]
+    common_features = set(features[0])
+    for f in features[1:]:
         common_features.intersection_update(f)
     return list(common_features)
 
