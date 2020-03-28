047de98aa14ac41f1eb4893ab77542c776846328
burtonrossj <burtonrj@cardiff.ac.uk>
2020-03-28 09:44:04 +0000

    FSC experiment tests
    Former-commit-id: 096486729c36110474e40fe112c3b28b265dc8a0

---
+++ cytopy/tests/test_data/test_fcs_experiment.py
@@ -0,0 +1,69 @@
+import sys
+sys.path.append('/home/ross/CytoPy')
+
+from cytopy.data.project import Project
+from cytopy.data.mongo_setup import global_init
+from cytopy.data.panel import Panel
+from mongoengine import connect
+import unittest
+
+db = connect('test')
+db.drop_database('test')
+global_init('test')
+
+
+def _setup():
+    # Create panel
+    test = Panel(panel_name='test')
+    test.create_from_excel('../data/test_panel.xlsx')
+    test.save()
+    # Create Project
+    test_project = Project(project_id='test', owner='test')
+    test_project.add_experiment('test_experiment_aml', panel_name='test')
+    test_project.add_subject('test_subject', testing=True)
+    test_project.save()
+
+
+class TextFCSExperiment(unittest.TestCase):
+
+    def test_FCSExperiment(self):
+        test_project = Project.objects(project_id='test').get()
+        test_exp = test_project.load_experiment('test_experiment_aml')
+        self.assertEqual(test_exp.experiment_id, 'test_experiment_aml')
+        self.assertEqual(test_exp.panel.panel_name, 'test')
+        test_exp.add_new_sample(sample_id='test_sample',
+                                file_path='../data/test.FCS',
+                                controls=[{'path': '../data/test.FCS', 'control_id': 'test_control'}],
+                                subject_id='test_subject',
+                                compensate=False)
+        self.assertEqual(test_exp.list_samples(), ['test_sample'])
+        self.assertTrue(test_exp.sample_exists('test_sample'))
+        self.assertEqual(test_exp.pull_sample('test_sample').primary_id, 'test_sample')
+        correct_mappings = {'FS Lin': 'FS Lin',
+                            'SS Log': 'SS Log',
+                            'FL1 Log': 'IgG1-FITC',
+                            'FL2 Log': 'IgG1-PE',
+                            'FL3 Log': 'CD45-ECD',
+                            'FL4 Log': 'IgG1-PC5',
+                            'FL5 Log': 'IgG1-PC7'}
+        correct_mappings = [{'channel': k, 'marker': v} for k, v in correct_mappings.items()]
+        self.assertListEqual(test_exp.pull_sample_mappings('test_sample').get('test_sample'), correct_mappings)
+        data = test_exp.pull_sample_data('test_sample', include_controls=True)
+        primary = [d for d in data if d.get('typ') == 'complete'][0]
+        ctrl = [d for d in data if d.get('typ') == 'control'][0]
+        self.assertEqual(type(data), list)
+        self.assertEqual(len(data), 2)
+        self.assertEqual(type(primary), dict)
+        self.assertEqual(type(ctrl), dict)
+        self.assertEqual(primary.get('data').shape, (30000, 7))
+        self.assertListEqual(primary.get('data').columns.tolist(),
+                             ['FS Lin', 'SS Log', 'IgG1-FITC', 'IgG1-PE', 'CD45-ECD', 'IgG1-PC5', 'IgG1-PC7'])
+        data = test_exp.pull_sample_data('test_sample', include_controls=False)
+        self.assertEqual(len(data), 1)
+        data = test_exp.pull_sample_data('test_sample', sample_size=5000, include_controls=False)
+        self.assertEqual(data[0].get('data').shape, (5000, 7))
+
+
+if __name__ == '__main__':
+    _setup()
+    unittest.main()
