06ebc59adb5f4341f86c6d551c002c330b062231
burtonrossj <burtonrj@cardiff.ac.uk>
2020-03-28 09:44:04 +0000

    FSC experiment tests
    Former-commit-id: 0a7dc4bf59dba923df6859c0de08c0ed3b9ca887 [formerly f9bd45e107bd98cd888ccabf942e4e14eab65307] [formerly 047de98aa14ac41f1eb4893ab77542c776846328] [formerly a636a4969643c6a9193a030bddb7588f813c91d8 [formerly af307be2d54522aa92c52d7ffcc9af38f5c5d10f] [formerly 047de98aa14ac41f1eb4893ab77542c776846328 [formerly 047de98aa14ac41f1eb4893ab77542c776846328 [formerly 096486729c36110474e40fe112c3b28b265dc8a0]]]]
    Former-commit-id: b87c0a68b71eeefdc7d34c4171e440721b2e2d41 [formerly dabe6ebfa3a9c498afd3bc295ac50b2ed1725ba5] [formerly a12a53b71cfe806e1d677fe88afc152bc3bacbd5 [formerly c2c7cb85feb2a16556fedf565a8832e93dda79a1]]
    Former-commit-id: a12a53b71cfe806e1d677fe88afc152bc3bacbd5
    Former-commit-id: b34dd07929af1a565160ac2c74d2a7f42fe01912
    Former-commit-id: 3fe5b56b779a16d09bbc758e0488c89dc514d780

---
+++ cytopy/tests/test1_data.py
@@ -0,0 +1,56 @@
+import sys
+sys.path.append('/home/ross/CytoPy')
+
+from cytopy.data.project import Project
+from cytopy.data.mongo_setup import global_init
+from cytopy.data.panel import NormalisedName, Panel, create_regex
+from cytopy.data.fcs import File, FileGroup, ChannelMap
+from cytopy.tests.utilities import make_example_date
+from mongoengine import connect
+import unittest
+
+db = connect('test')
+db.drop_database('test')
+global_init('test')
+
+
+    def test7_FileGroup(self):
+        # Create example data
+        example_data = make_example_date(n_samples=100, centers=3, n_features=2)
+        # Create dummy channel mappings
+        mappings = [ChannelMap(channel='var0', marker='feature0'),
+                    ChannelMap(channel='var1', marker='feature1'),
+                    ChannelMap(channel='var2', marker='blobID')]
+        # Populate data
+        test_project = Project.objects(project_id='test').get()
+        test_exp = test_project.load_experiment('test_experiment_dummy')
+        test_grp = FileGroup(primary_id='dummy_test',
+                             flags='dummy')
+        test_file = File(file_id='dummy_file', channel_mappings=mappings)
+        test_ctrl = File(file_id='dummy_ctrl', channel_mappings=mappings, file_type='control')
+        test_file.put(example_data)
+        test_ctrl.put(example_data)
+        test_grp.files = [test_file, test_ctrl]
+        test_grp.save()
+        test_exp.fcs_files.append(test_grp)
+        test_exp.save()
+
+        # Testing data retrieval
+        test_grp = test_exp.pull_sample('dummy_test')
+        test_file = test_grp.files[0]
+        self.assertEqual(test_file.pull().shape, (100, 3))
+        self.assertEqual(test_file.pull(sample=10).shape, (10, 3))
+        data = test_exp.pull_sample_data('dummy_test', include_controls=True)
+        primary = [d for d in data if d.get('typ') == 'complete'][0]
+        ctrl = [d for d in data if d.get('typ') == 'control'][0]
+        self.assertEqual(primary.get('data').shape, (100, 3))
+        self.assertEqual(ctrl.get('data').shape, (100, 3))
+        self.assertListEqual(ctrl.get('data').columns.tolist(), ['feature0', 'feature1', 'blobID'])
+        data = test_exp.pull_sample_data('dummy_test', include_controls=True, columns_default='channel')
+        primary = [d for d in data if d.get('typ') == 'complete'][0]
+        self.assertListEqual(primary.get('data').columns.tolist(), ['var0', 'var1', 'var2'])
+
+
+if __name__ == '__main__':
+    unittest.TestLoader.sortTestMethodsUsing = None
+    unittest.main()
