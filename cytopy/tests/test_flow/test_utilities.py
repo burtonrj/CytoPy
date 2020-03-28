71dbea9a8b3dfe8a91dd08f24b27ab56107ad745
burtonrossj <burtonrj@cardiff.ac.uk>
2020-03-28 14:26:26 +0000

    create test_utilities.py
    Former-commit-id: 3307a08b4431ae045844ea6130e162092d8d78ae [formerly b0a40a5c6b371ed00b14ebfe84ae2aa8386a4171] [formerly 001d36a76ffeec687a1272bc47487135cd1e5d5d [formerly 9130597ee214eb7e7c9e826d3ef557ba92cc65a4]] [formerly 0944dfd554da0e7632cd894adad1417b568b7518 [formerly d7007031f8b500363b06779e67166710adf0aa61]] [formerly 3c871263550c99dce7d0d39254ceda581cb5ee63 [formerly edd34fc3570c6863855e26b30548335c37553ae2] [formerly 3c8a59465a6f3a996ecb4ff2a3b8014a3227324c [formerly 114814abb180dd24105dce3bf8b4ad01c27df6bc]] [formerly 0944dfd554da0e7632cd894adad1417b568b7518 [formerly d7007031f8b500363b06779e67166710adf0aa61] [formerly 3aef2f204f1410764d972933231bc0c1c869fceb [formerly 56853d5a7e818bb50e569653a1e384649258e53f] [formerly 2462d9ccf74a7facf26b1e639c4fcd84193092dd] [formerly d7007031f8b500363b06779e67166710adf0aa61 [formerly d7007031f8b500363b06779e67166710adf0aa61 [formerly 7671fe313edb55e73fedb1ac1b3369a7b34568a6]]]]]]
    Former-commit-id: ada40afe7088987a57b126a2e9304353b1b0f05e [formerly 02c0204ce3ef2f61afef3f7f666684ce5b8903bc] [formerly eb805d35a0e4f4dcd252c9b9c9e41fbfc9cb83f3 [formerly 8aa5f01b7f4fef2ab13b94fb311d7a96fd7a6199]] [formerly 8f14de9c30e3e3c71b52f0daac10a8a43e3635e9 [formerly bd0f228054abde6b504728fc814e018dcaac2236] [formerly dac4a8c22575d1a4e2623ad40e7cda81fd01bf51 [formerly 8cef5e898f4083882d0c4ec44bf52c9a465d6074]]]
    Former-commit-id: 8f14de9c30e3e3c71b52f0daac10a8a43e3635e9 [formerly bd0f228054abde6b504728fc814e018dcaac2236]
    Former-commit-id: 5c8b29c59d3fef562cc84ebd776dda1303cd8e3b [formerly 80173f268be50851b3e2b30ae73d5da9a13f1e97]
    Former-commit-id: b1d0ba3fa2718858bb87447d5f25a05b1b78a894 [formerly e6ceaea265142d73ff5e443bc565d12380e46811 [formerly 00f96b1a84360e12f5b81e752f1407daa2f73404]]
    Former-commit-id: 9cf1f75232359d8a6623be0a731064c8722e270a
    Former-commit-id: 11e3103c231dab22666ac07d51eba525d75ca88d

---
+++ cytopy/tests/test_flow/test_utilities.py
@@ -0,0 +1,104 @@
+import sys
+sys.path.append('/home/ross/CytoPy')
+
+from cytopy.data.mongo_setup import global_init
+from cytopy.flow.gating import utilities
+from cytopy.tests.utilities import make_example_date
+from sklearn.neighbors import KernelDensity
+from scipy.signal import find_peaks
+from itertools import combinations
+import numpy as np
+import pandas as pd
+import unittest
+
+global_init('test')
+
+
+class TestCheckPeak(unittest.TestCase):
+    def test(self):
+        probs = np.array([0, 0, 0, 0.01, 0, 0, 2, 0, 0, 3, 0, 0, 0.01])
+        peaks = np.where(np.array(probs) > 0)
+        self.assertEqual(len(utilities.check_peak(peaks, probs, t=0.05)), 2)
+        self.assertEqual(len(utilities.check_peak(peaks, probs, t=0.5)), 4)
+
+
+class TestFindLocalMinima(unittest.TestCase):
+    @staticmethod
+    def _build():
+        data = make_example_date()
+        data = pd.concat([data[data.blobID != 2],
+                          data[data.blobID == 2].sample(frac=0.25)])
+        d = data['feature0'].values
+        density = KernelDensity(bandwidth=0.5, kernel='gaussian')
+        density.fit(d[:, None])
+        x_d = np.linspace(min(d), max(d), 1000)
+        prob = np.exp(density.score_samples(x_d[:, None]))
+        peaks = find_peaks(prob)[0]
+        return prob, peaks, x_d
+
+    def test(self):
+        prob, peaks, x_d = self._build()
+        self.assertAlmostEqual(utilities.find_local_minima(prob, x_d, peaks), 0.592, places=2)
+
+
+class TestInsideEllipse(unittest.TestCase):
+    @staticmethod
+    def _build():
+        data = make_example_date()
+        mask = utilities.inside_ellipse(data[['feature0', 'feature1']].values,
+                                        center=(4.5, 2.5),
+                                        width=2.3,
+                                        height=3,
+                                        angle=0)
+        return data, mask
+
+    def test(self):
+        data, mask = self._build()
+        correct = all(x == 1 for x in data.loc[mask].blobID.values)
+        self.assertTrue(correct)
+
+
+class TestRectangularFilter(unittest.TestCase):
+    def test(self):
+        data = make_example_date()
+        rect = dict(xmin=0, xmax=8, ymin=-2.5, ymax=6.0)
+        self.assertTrue(all(x == 1 for x in utilities.rectangular_filter(data,
+                                                                         x='feature0',
+                                                                         y='feature1',
+                                                                         definition=rect).blobID.values))
+
+
+class TestDensityDependentDownsample(unittest.TestCase):
+    @staticmethod
+    def _equal_ratio(data, samples):
+        ratios = [data[data.blobID == x[0]].shape[0] / data[data.blobID == x[1]].shape[0]
+                  for x in combinations(samples.blobID.unique(), 2)]
+        return combinations(ratios, 2)
+
+    def test(self):
+        data = make_example_date(n_samples=10000)
+        samples = utilities.density_dependent_downsample(data=data,
+                                                         features=['feature0', 'feature1'],
+                                                         mmd_sample_n=2000)
+        for x, y in self._equal_ratio(data, samples):
+            self.assertAlmostEqual(x, y, places=1)
+
+
+class TestGetParams(unittest.TestCase):
+    class MakeshiftClass:
+        def __init__(self, a, b, c, d='test', **kwargs):
+            pass
+
+    def test_basic(self):
+        self.assertListEqual(utilities.get_params(self.MakeshiftClass),
+                             ['a', 'b', 'c', 'd', 'kwargs'])
+
+    def test_requied_only(self):
+        self.assertListEqual(utilities.get_params(self.MakeshiftClass, required_only=True),
+                             ['a', 'b', 'c', 'kwargs'])
+
+    def test_required_only_exclude_kwargs(self):
+        self.assertListEqual(utilities.get_params(self.MakeshiftClass,
+                                                  required_only=True,
+                                                  exclude_kwargs=True),
+                             ['a', 'b', 'c'])
