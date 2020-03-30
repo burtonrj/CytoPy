9fc5b16ac507d44dfaf8c81f37d49f78e7e57023
burtonrossj <burtonrj@cardiff.ac.uk>
2020-03-30 14:59:24 +0100

    Minor refactor to defaults.py. Refactor tests for childpopulations
    Former-commit-id: f52e81f05d6b81fa84c35f57ec23d874091eb766 [formerly 919c142e67766665e7b7bd0b29ad2baa6ae2cb06] [formerly bf7a01a81caa1f4befe3a92497b2ee8fa6937c4c [formerly 6f7bf405716885c7006bc00e5ac59c97f799c5e1]] [formerly 03618180545ae4e40f28f51b995597b6fcfb9374 [formerly cc03cb6a7e3059963fea94099689ec89cd709eb3]] [formerly 0fdfc8219e8b643bad7f49e05f90c5d26a15a4d4 [formerly a0e22c6eb3a469c046c947b029f2ec7ec0ba1b88] [formerly c50a042556e5aaba5bb1b0a7a1635057dbdc22e0 [formerly a0bf01239ceb1d1c0412255cd783834ac119ea49]] [formerly 03618180545ae4e40f28f51b995597b6fcfb9374 [formerly cc03cb6a7e3059963fea94099689ec89cd709eb3] [formerly f5ba250aa58cbf9ed960a9815d1f0e4f73d08c08 [formerly d3fa8baf78f40b441a8af126ecae6f4a83210e5b] [formerly 1c5c2ae3d17e4433742392f97706777ae4679003] [formerly cc03cb6a7e3059963fea94099689ec89cd709eb3 [formerly cc03cb6a7e3059963fea94099689ec89cd709eb3 [formerly f4dd1703b05cae713a1b6cedbc4e979f7c10c0fa]]]]]]
    Former-commit-id: 5c0485a53bb5fdfcbc50cbf57467bc336a123a35 [formerly c341321009e164a279e29f50b9ef3d11932f92ba] [formerly 5f8c2473a685e1c1cb01585d0869c0fba3a6db16 [formerly d641a3fbf9d7a76c158302754b8a8d02d87ef83e]] [formerly c656d1433ae511f7325570c13d4be5ed29d5c421 [formerly 76a67ad60f281b35fadd57e6b9f5412ff39b6488] [formerly 35e9c6fbcdd2aec157b7f8b06dddd304f73efe1c [formerly 6df43d8e42bf8af128799030d1d3c756d6c493b5]]]
    Former-commit-id: 24265487600257e90758709e5b15cc7dbefc7364 [formerly 54f81a942fbc2b6712c346c960ada82d3f8cef4c] [formerly c656d1433ae511f7325570c13d4be5ed29d5c421 [formerly 76a67ad60f281b35fadd57e6b9f5412ff39b6488]]
    Former-commit-id: e2c9776735ed04575152c64e6c5e13c0ba349b91 [formerly 275234595fdd90571f39335495e26f3883c0241a]
    Former-commit-id: 13047c038febb0bf0bd07eca4c4610601cb20766 [formerly df3a598c4469d734b3e18869e40c3f1247702aab [formerly 7fe5a2ca986559afbdd776c858aa605d07fcba39]]
    Former-commit-id: ddaf0c0998379f364e7be1803695505b4f76bd5a
    Former-commit-id: bfa2d514825ef03c2f7b4e3c4c077e2bbb1c8998

--- cytopy/tests/test_flow/test_childpopulations.py
+++ cytopy/tests/test_flow/test_childpopulations.py
@@ -1,8 +1,9 @@
 import sys
-sys.path.append('/home/rossco/CytoPy')
+sys.path.append('/home/ross/CytoPy')
 # Data imports
 from cytopy.data.mongo_setup import global_init
-from cytopy.flow.gating.defaults import Geom, ChildPopulation, ChildPopulationCollection, ChildConstructError
+from cytopy.flow.gating.defaults import Geom, ChildPopulation, ChildPopulationCollection, _validate_input
+import numpy as np
 import unittest
 
 global_init('test')
@@ -12,7 +13,7 @@ class TestGeom(unittest.TestCase):
     def test_shape(self):
         err = False
         try:
-            test = Geom(shape='NONSENSE')
+            test = Geom(shape='NONSENSE', x='x', y='y')
         except AssertionError:
             err = True
         self.assertTrue(err)
@@ -26,32 +27,140 @@ class TestGeom(unittest.TestCase):
         self.assertDictEqual(test.as_dict(), dict(shape='ellipse', x='x', y='y', other='test'))
 
 
+class TestValidateInput(unittest.TestCase):
+    def _is_error(self, i, expected_outcome):
+        err = False
+        try:
+            _validate_input(**i)
+        except AssertionError:
+            err = True
+        self.assertEqual(err, expected_outcome)
+
+    def test1(self):
+        self._is_error(dict(gate_type='threshold_1d'), expected_outcome=True)
+
+    def test2(self):
+        self._is_error(dict(gate_type='threshold_2d', name='test'), expected_outcome=True)
+
+    def test3(self):
+        self._is_error(dict(gate_type='threshold_1d', definition='+'), expected_outcome=True)
+
+    def test4(self):
+        self._is_error(dict(gate_type='geom', definition='NONSENSE', name='test'), expected_outcome=True)
+
+    def test5(self):
+        self._is_error(dict(gate_type='threshold_1d', definition='NONSENSE', name='test'), expected_outcome=True)
+
+    def test6(self):
+        self._is_error(dict(gate_type='threshold_1d', definition=111, name='test'), expected_outcome=True)
+
+    def test7(self):
+        self._is_error(dict(gate_type='threshold_2d'), expected_outcome=True)
+
+    def test8(self):
+        self._is_error(dict(gate_type='threshold_2d', definition=[], name='test'), expected_outcome=True)
+
+    def test9(self):
+        self._is_error(dict(gate_type='threshold_2d', definition=['+'], name='test'), expected_outcome=True)
+
+    def test10(self):
+        self._is_error(dict(gate_type='threshold_2d', definition='+', name='test'), expected_outcome=True)
+
+    def test11(self):
+        self._is_error(dict(gate_type='cluster', target=[0, 1]), expected_outcome = True)
+
+    def test12(self):
+        self._is_error(dict(gate_type='cluster', target=[0, 1], weight='NONSENSE', name='test'),
+                       expected_outcome = True)
+
+    def test13(self):
+        self._is_error(dict(gate_type='cluster', target=['x', 1], weight='NONSENSE', name='test'),
+                       expected_outcome = True)
+
+    def test14(self):
+        self._is_error(dict(gate_type='cluster', target=[0, 1, 2], weight='NONSENSE', name='test'),
+                       expected_outcome = True)
+
+    def test15(self):
+        self._is_error(dict(gate_type='threshold_1d', definition='+', name='test'), expected_outcome = False)
+
+    def test16(self):
+        self._is_error(dict(gate_type='geom', definition='+', name='test'), expected_outcome = False)
+
+    def test17(self):
+        self._is_error(dict(gate_type='threshold_2d', definition='++', name='test'), expected_outcome = False)
+
+    def test18(self):
+        self._is_error(dict(gate_type='threshold_2d', definition=['++', '+-'], name='test'), expected_outcome = False)
+
+    def test19(self):
+        self._is_error(dict(gate_type='cluster', weight=2, name='test', target=[0, 1]), expected_outcome = False)
+
+    def test20(self):
+        self._is_error(dict(gate_type='cluster', weight=2, name='test', target=[0.5, 1.5]), expected_outcome = False)
+
+
 class TestChildPopulation(unittest.TestCase):
     def test_create(self):
-        invalid1 = dict(gate_type='threshold_1d')
-        invalid2 = dict(gate_type='threshold_1d', definition='+')
-        invalid3 = dict(gate_type='geom', definition='NONSENSE', name='test')
-        invalid4 = dict(gate_type='threshold_2d')
-        invalid5 = dict(gate_type='threshold_2d', definition=[], )
+        test = ChildPopulation(gate_type='geom', definition='+', name='test')
+        self.assertEqual(type(test), ChildPopulation)
+        self.assertDictEqual(test.properties, dict(name='test', definition='+'))
 
-        valid1 = dict(gate_type='geom', definition='+', name='test')
-        valid2 = dict(gate_type='threshold_1d', definition='+', name='test')
+    def test_update_index(self):
+        test = ChildPopulation(gate_type='geom', definition='+', name='test')
+        test.update_index(np.array([0, 1, 2, 3]))
+        self.assertListEqual([0, 1, 2, 3], list(test.index))
+        test.update_index(np.array([2, 3, 4, 5]), merge_options='overwrite')
+        self.assertListEqual([2, 3, 4, 5], list(test.index))
+        test.update_index(np.array([4, 5, 6, 7]), merge_options='merge')
+        self.assertListEqual([2, 3, 4, 5, 6, 7], list(test.index))
 
 
-class TestChildPopulationColleciton(unittest.TestCase):
+class TestChildPopulationCollectiecitonon(unittest.TestCase):
+    def test_create(self):
+        test = ChildPopulationCollection(gate_type='threshold_1d')
+        self.assertEqual(test.gate_type, 'threshold_1d')
 
-    def testChildPopulationCollection(self):
-        test1d = ChildPopulationCollection(gate_type='threshold_1d')
-        test1d.add_population('positive', definition='+')
-        test1d.add_population('negative', definition='-')
-        self.assertEqual(test1d.fetch_by_definition('+'), 'positive')
-        self.assertEqual(test1d.fetch_by_definition('++'), None)
-        test1d.remove_population('positive')
-        self.assertListEqual(list(test1d.populations.keys()), ['negative'])
+    def test_create_from_dict(self):
+        test = dict(gate_type='threshold_1d', populations=[dict(name='pos', definition='+'),
+                                                           dict(name='neg', definition='-')])
+        test = ChildPopulationCollection(json_dict=test)
+        self.assertListEqual(['pos', 'neg'], list(test.populations.keys()))
+        self.assertEqual(test.populations.get('pos').properties.get('definition'), '+')
+        self.assertEqual(test.populations.get('neg').properties.get('definition'), '-')
 
+    def test_serialise(self):
+        test = ChildPopulationCollection(gate_type='threshold_1d')
+        test.add_population('pos', definition='+')
+        test.add_population('neg', definition='-')
+        test = test.serialise()
+        self.assertDictEqual(test, dict(gate_type='threshold_1d',
+                                        populations=[dict(name='pos', definition='+'),
+                                                     dict(name='neg', definition='-')]))
+
+    def test_add_population(self):
+        test = ChildPopulationCollection(gate_type='threshold_1d')
+        test.add_population('pos', definition='+')
+        test.add_population('neg', definition='-')
+        self.assertListEqual(['pos', 'neg'], list(test.populations.keys()))
+        self.assertEqual(test.populations.get('pos').properties.get('definition'), '+')
+        self.assertEqual(test.populations.get('neg').properties.get('definition'), '-')
+
+    def test_remove_population(self):
+        test = ChildPopulationCollection(gate_type='threshold_1d')
+        test.add_population('pos', definition='+')
+        test.add_population('neg', definition='-')
+        test.remove_population('pos')
+        self.assertListEqual(['neg'], list(test.populations.keys()))
+
+    def test_fetch_by_def(self):
         test2d = ChildPopulationCollection(gate_type='threshold_2d')
         test2d.add_population('pospos', definition='++')
         test2d.add_population('other', definition=['--', '-+', '+-'])
-        self.assertEqual(test1d.fetch_by_definition('+'), None)
-        self.assertEqual(test1d.fetch_by_definition('++'), 'pospos')
-        self.assertEqual(test1d.fetch_by_definition('--'), 'other')
+        self.assertEqual(test2d.fetch_by_definition('++'), 'pospos')
+        self.assertEqual(test2d.fetch_by_definition('--'), 'other')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
