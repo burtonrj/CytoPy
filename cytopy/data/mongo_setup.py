b63ae1a35138c28b87469a4d62f66c9008a2d2aa
burtonrossj <burtonrj@cardiff.ac.uk>
2020-04-03 14:48:49 +0100

    Added setup.py and updated docs for global_init
    Former-commit-id: 7f6436dc21b9108c6414239acd12341fab91aa57 [formerly 39c832c776f164ddd9c2ec2348fa07cda99bde53] [formerly ef55ef1d6289346a5f3a6ff6aea0397431e82314 [formerly 83d3fa111eeff634956de9d48572129ecd3c7a3d]] [formerly 0faa089403f60a493198f59b4f3d14f1b69abcc9 [formerly 91d911e56916da911dcd37aee020ca480dec1074]] [formerly 85f67a1674fa13a7827cbb41237f6692a98036e0 [formerly 57ea251986b0c371b51a63a423747e6031db7e7d] [formerly 487ea16ec10a20b697ad9e70992db8dd8344237c [formerly a9255078b22097c388494b4004c160f4a9a7de99]] [formerly 0faa089403f60a493198f59b4f3d14f1b69abcc9 [formerly 91d911e56916da911dcd37aee020ca480dec1074] [formerly e3a4506df7274617389a6c1d12b973e65b60e209 [formerly f11d120c32352be7c68e8cf38bd522d5e6fb30de] [formerly 0920dd633f6387bed681e4c081644c40eae4150f] [formerly 91d911e56916da911dcd37aee020ca480dec1074 [formerly 91d911e56916da911dcd37aee020ca480dec1074 [formerly 2a45dabfa99a8cb5dd4241dfaeac1ebfea5ea30b]]]]]]
    Former-commit-id: 4d3db3af5b585425470a1d79801616f7c35805df [formerly 90c8acf1a0b5045f4846d0f7118c04e6186b0837] [formerly 9a0c7068e2c4cccc9b8f06a83cd0dcf30e9c0a92 [formerly d110eca9325922c1b320549608818dcbbd3e7d95]] [formerly 894fd9bc224bc369cf7d05e16fa8e17f52e77b56 [formerly 30f9fd652c15716084ce8a37682960ad6f28e6a4] [formerly c311d2e02edb6d9d2239291cfbeaef136a6794c8 [formerly a084f6086804601105b2213e695d9d0e84af33cc]]]
    Former-commit-id: 623382ee5adf25873cdcae13a2df38ec77b8b6f0 [formerly 431342ad1a922b9054e384955df2c15dc2e1d77a] [formerly 894fd9bc224bc369cf7d05e16fa8e17f52e77b56 [formerly 30f9fd652c15716084ce8a37682960ad6f28e6a4]]
    Former-commit-id: ee236fbbcd48fee5ff844762707b862eb8c4d06b [formerly 28a45639be3414484e24c3481e72bdd151d3dcd8]
    Former-commit-id: 72ae71f9b6846895c0d2bae579fd31388673bdee [formerly 543b061556011d4e7e5bbbcbc8681bd38979b219 [formerly 61ae3ce5e07227a304b4659096d8d8e9dd786577]]
    Former-commit-id: 127b683568b2f98d8574982e317a2fd7ae222422
    Former-commit-id: e48f1766df893ed7f8161f7881324f02b353615b

--- cytopy/data/mongo_setup.py
+++ cytopy/data/mongo_setup.py
@@ -1,9 +1,15 @@
 import mongoengine
 
 
-def global_init(database_name: str) -> None:
+def global_init(database_name: str,
+                **kwargs) -> None:
     """
-    Global initializer for mongogengine ORM
+    Global initializer for mongogengine ORM. See mongoengine.register_connection for additional keyword arguments and
+    mongoengine documentation for extensive details about registering connections. In brief, database connections are
+    registered globally and refered to using an alias. By default CytoPy uses the alias 'core'.
+
+    The database is assumed to be hosted locally, but if a remote server is used the user should provide the host
+    address and port. If authentication is needed then a username and password should also be provided.
 
     Parameters
     -----------
@@ -14,4 +20,4 @@ def global_init(database_name: str) -> None:
     --------
     None
     """
-    mongoengine.register_connection(alias='core', name=database_name)
+    mongoengine.register_connection(alias='core', name=database_name, **kwargs)
