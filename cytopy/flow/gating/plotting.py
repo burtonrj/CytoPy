4d87f2862a9dc1038332d7d429d16c666b492608
burtonrossj <burtonrj@cardiff.ac.uk>
2020-04-05 15:30:16 +0100

    Removed control gate functionality for now. Currently redundant in pipeline but might reintroduce later. Also added control_comparison plotting functionality.
    Former-commit-id: d2c66cdba578a9bee7c6eecbca19504b3d3de148

--- cytopy/flow/gating/plotting.py
+++ cytopy/flow/gating/plotting.py
@@ -44,14 +44,11 @@ def transform_axes(data: pd.DataFrame, axes_vars: dict, transforms: dict) -> pd.
     return data[axes_vars.values()]
 
 
-def plot_axis_lims(data: dict, x: str, y: str, xlim: tuple, ylim: tuple) -> tuple and tuple:
+def plot_axis_lims(x: str, y: str, xlim: tuple, ylim: tuple) -> tuple and tuple:
     """Establish axis limits
 
     Parameters
     ----------
-    data : dict
-        dictionary of single cell data where 'priamry' is the dataframe for the main single cell data,
-        and other keys correspond to FMO data
     x : str
         name of x axis
     y : str
@@ -67,14 +64,11 @@ def plot_axis_lims(data: dict, x: str, y: str, xlim: tuple, ylim: tuple) -> tupl
         x-axis limits, y-axis limits
 
     """
-    is_fmo = any(['fmo' in k for k in data.keys()])
 
     def update_lim(a, lim):
         if not lim:
             if any([a.find(c) != -1 for c in ['FSC', 'SSC']]):
                 return 0, 250000
-            if is_fmo:
-                return data['primary'][a].min(), data['primary'][a].max()
         return lim
     return update_lim(x, xlim), update_lim(y, ylim)
 
@@ -94,27 +88,6 @@ class Plot:
         self.gating = gating_object
         self.default_axis = default_axis
         self.colours = cycle(plt.get_cmap('tab20c').colors)
-
-    def _get_gate_data(self, gate: Gate) -> dict:
-        """Given a gate, return all associated data that gate acts upon. Data is returned as raw with no transformations.
-
-        Parameters
-        ----------
-        gate : Gate
-
-        Returns
-        -------
-        dict
-            Dictionary of pandas dataframes
-        """
-        kwargs = {k: v for k, v in gate.kwargs}
-        data = dict(primary=self.gating.get_population_df(gate.parent, transform=False))
-        for x in ['fmo_x', 'fmo_y']:
-            if x in kwargs.keys():
-                data[x] = self.gating.search_ctrl_cache(target_population=gate.parent,
-                                                        ctrl_id=kwargs[x],
-                                                        return_dataframe=True)
-        return data
 
     @staticmethod
     def _plot_asthetics(ax: matplotlib.pyplot.axes, x: str, y: str,
@@ -197,24 +170,30 @@ class Plot:
 
         geoms = {c: self.gating.populations[c].geom for c in gate.children
                  if self.gating.populations[c].geom is not None}
-        data = {k: transform_axes(v, axes_vars, transforms) for k, v in self._get_gate_data(gate).items()}
-        xlim, ylim = plot_axis_lims(data=data, x=axes_vars['x'], y=y, xlim=xlim, ylim=ylim)
-        num_axes = len(data.keys())
+        data = transform_axes(data=self.gating.get_population_df(gate.parent, transform=False),
+                              axes_vars=axes_vars,
+                              transforms=transforms)
+        xlim, ylim = plot_axis_lims(x=axes_vars['x'], y=y, xlim=xlim, ylim=ylim)
+        num_axes = 1
         fig, axes = plt.subplots(ncols=num_axes, figsize=figsize)
         self._geom_plot(data=data, fig=fig, axes=axes,
                         geoms=geoms, axes_vars=axes_vars,
                         xlim=xlim, ylim=ylim, name=gate_name)
 
-    def _geom_plot(self, data: dict, fig: matplotlib.pyplot.figure,
+    def _geom_plot(self, data: pd.DataFrame, fig: matplotlib.pyplot.figure,
                    axes: np.array or matplotlib.pyplot.axes, geoms: dict,
                    axes_vars: dict, xlim: tuple, ylim: tuple, name: str) -> None:
         """
         Wrapper function for producing a plot of a gate that generates a geometric object
 
+        This wrapper has been created because future versions of CytoPy might allow for creation
+        of multiple axis in figure for comparison of gates within one figure. Also might expand to
+        allow for control driven gates.
+
         Parameters
         -----------
-        data: dict
-            dictionary of data to plot (primary and controls)
+        data: Pandas.DataFrame
+            population data
         geoms: dict
             dictionary object; keys correspond to child population names and values their geometric gate definition
         axes_vars: dict
@@ -235,15 +214,8 @@ class Plot:
         --------
         None
         """
-        if hasattr(axes, '__iter__'):
-            for ax, (name_, d) in zip(axes, data.items()):
-                self._build_geom_plot(data=d, x=axes_vars['x'], y=axes_vars['y'], geoms=geoms,
-                                      ax=ax, xlim=xlim, ylim=ylim,
-                                      title=f'{name}_{name_}')
-        else:
-            d = data['primary']
-            self._build_geom_plot(data=d, x=axes_vars['x'], y=axes_vars['y'], geoms=geoms,
-                                  ax=axes, xlim=xlim, ylim=ylim, title=name)
+        self._build_geom_plot(data=data, x=axes_vars['x'], y=axes_vars['y'], geoms=geoms,
+                              ax=axes, xlim=xlim, ylim=ylim, title=name)
         fig.tight_layout()
         fig.show()
 
@@ -291,24 +263,24 @@ class Plot:
                 print(f'Population {child_name} has no associated gate, skipping...')
                 continue
             if geom['shape'] == 'threshold':
-                ax.axvline(geom['threshold'], c=colour, lw=5)
+                ax.axvline(geom['threshold'], c=colour, lw=2.5)
             if geom['shape'] == '2d_threshold':
-                ax.axvline(geom['threshold_x'], c=colour, lw=5)
-                ax.axhline(geom['threshold_y'], c=colour, lw=5)
+                ax.axvline(geom['threshold_x'], c=colour, lw=2.5)
+                ax.axhline(geom['threshold_y'], c=colour, lw=2.5)
             if geom['shape'] == 'ellipse':
                 ellipse = patches.Ellipse(xy=geom['centroid'], width=geom['width'], height=geom['height'],
-                                          angle=geom['angle'], fill=False, edgecolor=colour, lw=5)
+                                          angle=geom['angle'], fill=False, edgecolor=colour, lw=2.5)
                 ax.add_patch(ellipse)
             if geom['shape'] == 'rect':
                 rect = patches.Rectangle(xy=(geom['x_min'], geom['y_min']),
                                          width=((geom['x_max']) - (geom['x_min'])),
                                          height=(geom['y_max'] - geom['y_min']),
-                                         fill=False, edgecolor=colour, lw=5)
+                                         fill=False, edgecolor=colour, lw=2.5)
                 ax.add_patch(rect)
             if geom['shape'] == 'poly':
                 x = geom['cords']['x']
                 y = geom['cords']['y']
-                ax.plot(x, y, '-k', c=colour, label=child_name, lw=5)
+                ax.plot(x, y, '-k', c=colour, label=child_name, lw=2.5)
 
     @staticmethod
     def _2dhist(ax: matplotlib.pyplot.axes, data: pd.DataFrame, x: str, y: str) -> matplotlib.pyplot.axes:
@@ -348,7 +320,8 @@ class Plot:
                         ylim: tuple = None,
                         transforms: dict or None = None,
                         sample: float or None = None,
-                        figsize: tuple = (5, 5)):
+                        figsize: tuple = (5, 5),
+                        ctrl_id: str or None = None):
         """
         Generate a static plot of a population
 
@@ -371,24 +344,27 @@ class Plot:
             if a float value is provided, given proportion of data is sampled prior to plotting (optional)
         figsize: tuple, (default=(5,5))
             Figure size passed to matplotlib.pyplot.subplots call
+        ctrl_id: str, optional
+            If given, the equivalent population within the associated control data is plotted
 
         Returns
         -------
         None
         """
         fig, ax = plt.subplots(figsize=figsize)
-        if population_name in self.gating.populations.keys():
-            data = self.gating.get_population_df(population_name, transform=False).copy()
-        else:
-            print(f'Invalid population name, must be one of {self.gating.populations.keys()}')
-            return None
+        data = self.gating.get_population_df(population_name,
+                                             transform=False,
+                                             ctrl_id=ctrl_id).copy()
+        assert data is not None, 'Invalid population'
+
         if transforms is None:
+            print('No transforms provided, defaulting to logicle')
             transforms = dict(x='logicle', y='logicle')
         data = transform_axes(data=data, axes_vars={'x': x, 'y': y}, transforms=transforms)
         if sample is not None:
             data = data.sample(frac=sample)
 
-        xlim, ylim = plot_axis_lims(data={'primary': data}, x=x, y=y, xlim=xlim, ylim=ylim)
+        xlim, ylim = plot_axis_lims(x=x, y=y, xlim=xlim, ylim=ylim)
         if data.shape[0] < 1000:
             ax.scatter(x=data[x], y=data[y], s=3)
             ax = self._plot_asthetics(ax, x, y, xlim, ylim, title=population_name)
@@ -397,11 +373,76 @@ class Plot:
             ax = self._plot_asthetics(ax, x, y, xlim, ylim, title=population_name)
         fig.show()
 
-    def backgate(self, base_population: str, x: str, y: str, pgeoms: list or None = None, poverlay: list or None = None,
-                 cgeoms: list or None = None, coverlay: list or None = None,
-                 cluster_root_population: str or None = None, meta_clusters: bool = True,
-                 xlim: tuple or None = None, ylim: tuple or None = None,
-                 transforms: dict or None = None, title: str or None = None,
+    def compare_control(self,
+                        population: str,
+                        ctrl_id: list,
+                        x: str,
+                        y: str,
+                        transforms: dict or None = None,
+                        figsize: tuple = (8, 8)):
+        """
+        Under the assumption that control gating has been performed on the underlying object,
+        plot the target population (overlaid on its parent population) and on the same plot, show
+        the predicted population for controls
+
+        Parameters
+        ----------
+        population: str
+            Population of interest
+        ctrl_id: list
+            One or more controls to plot for comparison (must be valid control IDs)
+        x: str
+            X-axis variable
+        y: str
+            Y-axis variable
+        transforms: dict, optional
+            How to transform axis variables (defaults to logicle transform if not given)
+        figsize: tuple
+            Figure size
+        Returns
+        -------
+        Matplotlib.axes
+
+        """
+        if transforms is None:
+            print('No transforms provided, defaulting to logicle')
+            transforms = dict(x='logicle', y='logicle')
+        axes_vars = {'x': x, 'y': y}
+        base_pop = transform_axes(data=self.gating.get_population_df(population, transform=False),
+                                  axes_vars=axes_vars,
+                                  transforms=transforms)
+        parent = transform_axes(data=self.gating.get_population_df(self.gating.populations[population].parent.name,
+                                                                   transform=False),
+                                axes_vars=axes_vars,
+                                transforms=transforms)
+        ctrls = [transform_axes(data=self.gating.get_population_df(population,
+                                                                   transform=False,
+                                                                   ctrl_id=c),
+                                axes_vars=axes_vars,
+                                transforms=transforms) for c in ctrl_id]
+
+        fig, ax = plt.subplots(figsize=figsize)
+        self._2dhist(ax, parent, x, y)
+        ax.scatter(x=base_pop[x], y=base_pop[y], s=3, c='red', alpha=0.7, label=population)
+        for ct, co, cid in zip(ctrls, self.colours, ctrl_id):
+            ax.scatter(x=ct[x], y=ct[y], s=3, c=co, alpha=0.5, label=cid)
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        return ax
+
+    def backgate(self,
+                 base_population: str,
+                 x: str,
+                 y: str,
+                 pgeoms: list or None = None,
+                 poverlay: list or None = None,
+                 cgeoms: list or None = None,
+                 coverlay: list or None = None,
+                 cluster_root_population: str or None = None,
+                 meta_clusters: bool = True,
+                 xlim: tuple or None = None,
+                 ylim: tuple or None = None,
+                 transforms: dict or None = None,
+                 title: str or None = None,
                  figsize: tuple = (8, 8)) -> None:
         """
         This function allows for plotting of multiple populations on the backdrop of some population upstream of
