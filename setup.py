68f5b81bd692e6120829f145854b6c9b39be056f
burtonrossj <burtonrj@cardiff.ac.uk>
2020-04-07 15:39:16 +0100

    Corrected errors with setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,15 @@ if sys.version_info.major != 3:
 setup(
     name='CytoPy',
     version='0.0.1',
-    packages=['flow', 'flow.gating', 'flow.gating.plotting', 'flow.clustering', 'flow.supervised', 'tests'],
+    packages=['flow', 'flow.gating', 'flow.clustering', 'flow.supervised', 'tests', 'tests.data',
+              'tests.test_clustering', 'tests.test_data', 'tests.test_flow'],
     package_dir={'': 'cytopy'},
     url='https://github.com/burtonrj/CytoPy',
     license='MIT',
     author='Ross Burton',
     author_email='burtonrj@cardiff.ac.uk',
     description='Python framework for data-centric autonomous cytometry analysis',
-    install_requires=open("requirements.txt").read()
+    install_requires=open("requirements.txt").read(),
+    dependency_links=['https://github.com/jacoblevine/PhenoGraph.git#egg=PhenoGraph',
+                      'https://github.com/burtonrj/FlowUtilsPandas.git#egg=flowutilspd']
 )
