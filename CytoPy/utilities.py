8c6bcbb0f266b6980ce8fd690637906ace562921
burtonrj <burtonrj@cardiff.ac.uk>
2020-08-15 13:22:11 +0100

    Batch effects refactors complete: dim reduction implemented into simularity matrix

--- CytoPy/utilities.py
+++ CytoPy/utilities.py
@@ -1,5 +1,8 @@
-import os
+from scipy.spatial.distance import euclidean
+from scipy.linalg import norm
 import mongoengine
+import numpy as np
+import os
 
 
 def valid_directory(val: str):
@@ -11,3 +14,18 @@ def indexed_parallel_func(x: tuple,
                           func: callable,
                           **kwargs):
     x[0], func(x[1].values, **kwargs)
+
+
+def hellinger_dist(p: np.array, q: np.array):
+    """
+    https://gist.github.com/larsmans/3116927
+    Parameters
+    ----------
+    p
+    q
+
+    Returns
+    -------
+
+    """
+    return np.sqrt(np.sum((np.sqrt(p) - np.sqrt(q)) ** 2)) / np.sqrt(2)
