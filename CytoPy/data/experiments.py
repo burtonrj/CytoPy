72531dbce1a3944390c627722ee59748b7ff5ab0
burtonrj <burtonrj@cardiff.ac.uk>
2020-09-03 10:58:30 +0100

    check data directory on creation of experiment and format path

--- CytoPy/data/experiments.py
+++ CytoPy/data/experiments.py
@@ -390,8 +390,7 @@ class Experiment(mongoengine.Document):
         List of IDs for meta clusters belonging to this experiment
     """
     experiment_id = mongoengine.StringField(required=True, unique=True)
-    data_directory = mongoengine.StringField(required=True,
-                                             validation=valid_directory)
+    data_directory = mongoengine.StringField(required=True)
     panel = mongoengine.ReferenceField(Panel, reverse_delete_rule=4)
     fcs_files = mongoengine.ListField(mongoengine.ReferenceField(FileGroup, reverse_delete_rule=4))
     flags = mongoengine.StringField(required=False)
@@ -405,6 +404,7 @@ class Experiment(mongoengine.Document):
     def __init__(self, *args, **kwargs):
         panel_definition = kwargs.pop("panel_definition", None)
         panel_name = kwargs.pop("panel_name", None)
+        data_directory = kwargs.pop("data_directory", None)
         super().__init__(*args, **kwargs)
         if not self.panel:
             if panel_definition is None and panel_name is None:
@@ -418,6 +418,18 @@ class Experiment(mongoengine.Document):
             self.panel = self._generate_panel(panel_definition=panel_definition,
                                               panel_name=panel_name)
             self.panel.save()
+        if data_directory is not None:
+            assert os.path.isdir(data_directory), f"{data_directory} does not exist"
+            leading_char = data_directory[len(data_directory) - 1]
+            if leading_char not in ["\\", "/"]:
+                if len(data_directory.split("\\")) > 0:
+                    # Assuming a windows OS
+                    data_directory = data_directory + "\\"
+                else:
+                    # Assuming unix OS
+                    data_directory = data_directory + "/"
+            self.data_directory = data_directory
+
 
     def _generate_panel(self,
                         panel_definition: str or None,
