ed54d97f13864668a47e2630825af2e081be7a62
burtonrj <burtonrj@cardiff.ac.uk>
2020-09-21 17:57:05 +0100

    Moved valid_directory to clustering

--- CytoPy/utilities.py
+++ CytoPy/utilities.py
@@ -1,34 +1,7 @@
 from shapely.geometry import Polygon, Point
 import pandas as pd
 import mongoengine
-import numpy as np
 import os
-
-
-def valid_directory(val: str):
-    if not os.path.isdir(val):
-        raise mongoengine.errors.ValidationError(f"{val} is not a valid directory")
-
-
-def indexed_parallel_func(x: tuple,
-                          func: callable,
-                          **kwargs):
-    x[0], func(x[1].values, **kwargs)
-
-
-def hellinger_dist(p: np.array, q: np.array):
-    """
-    https://gist.github.com/larsmans/3116927
-    Parameters
-    ----------
-    p
-    q
-
-    Returns
-    -------
-
-    """
-    return np.sqrt(np.sum((np.sqrt(p) - np.sqrt(q)) ** 2)) / np.sqrt(2)
 
 
 def inside_polygon(df: pd.DataFrame,
