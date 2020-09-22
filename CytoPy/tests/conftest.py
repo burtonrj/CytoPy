94a21d45bfe177362ee5bd65d51fa24e2e5bc9fc
burtonrj <burtonrj@cardiff.ac.uk>
2020-09-22 11:21:01 +0100

    Test cluster init

--- CytoPy/tests/conftest.py
+++ CytoPy/tests/conftest.py
@@ -1,19 +1,24 @@
 from mongoengine.connection import disconnect, _get_db
 from ..data.setup import global_init
 import pytest
+import h5py
 import sys
+import os
 
 
 @pytest.fixture(scope='session', autouse=True)
-def mongo_setup():
+def test_setup():
     sys.path.append("/home/rossco/CytoPy")
     global_init(database_name="test", alias="core")
+    f = h5py.File('test.hdf5', 'w')
+    f.close()
     yield
     db = _get_db(alias="core")
     try:
         db.drop_database()
     except TypeError:
         pass
+    os.remove("test.hdf5")
     disconnect(alias="core")
 
 
