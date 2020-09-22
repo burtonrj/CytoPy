68d7ae448fd1030030f96d1f58370f74408560c9
burtonrj <burtonrj@cardiff.ac.uk>
2020-09-22 14:59:59 +0100

    Tested new shapes

--- CytoPy/tests/test_population.py
+++ CytoPy/tests/test_population.py
@@ -1,5 +1,5 @@
-from ..data.populations import Cluster, PopulationGeometry, Population
-from shapely.geometry import Polygon
+from ..data.populations import Cluster, Population, Threshold, Ellipse, Polygon
+from shapely.geometry import Polygon as Poly
 import numpy as np
 import pytest
 
@@ -15,33 +15,28 @@ def test_cluster_init():
     assert np.array_equal(np.array([0, 1, 2, 3, 4]), x.index)
 
 
-def test_population_geometry_shape():
-    poly = PopulationGeometry(x_values=[0, 0, 5, 5, 0],
-                              y_values=[0, 5, 5, 0, 0])
-    assert isinstance(poly.shape, Polygon)
+def test_polygon_shape():
+    poly = Polygon(x_values=[0, 0, 5, 5, 0],
+                   y_values=[0, 5, 5, 0, 0])
+    assert isinstance(poly.shape, Poly)
     assert np.array_equal(poly.shape.exterior.xy[0], np.array([0, 0, 5, 5, 0]))
     assert np.array_equal(poly.shape.exterior.xy[1], np.array([0, 5, 5, 0, 0]))
-    circle = PopulationGeometry(width=5,
-                                height=5,
-                                center=(10, 10),
-                                angle=0)
-    assert isinstance(circle.shape, Polygon)
+
+
+def test_ellipse_shape():
+    circle = Ellipse(width=5,
+                     height=5,
+                     center=(10, 10),
+                     angle=0)
+    assert isinstance(circle.shape, Poly)
     assert circle.shape.area == pytest.approx(np.pi * (circle.width ** 2), 1.)
-    threshold = PopulationGeometry(x_threshold=2.5,
-                                   y_threshold=2.5)
-    assert threshold.shape is None
 
 
-def test_population_geometry_overlap():
-    threshold = PopulationGeometry(x_threshold=2.5,
-                                   y_threshold=2.5)
-    poly1 = PopulationGeometry(x_values=[0, 0, 5, 5, 0],
-                               y_values=[0, 5, 5, 0, 0])
-    poly2 = PopulationGeometry(x_values=[2.5, 2.5, 5, 5, 2.5],
-                               y_values=[0, 5, 5, 0, 0])
-    with pytest.warns(UserWarning) as w:
-        threshold.overlap(poly1)
-    assert str(w.list[0].message) == "PopulationGeometry properties are incomplete. Cannot determine shape."
+def test_overlap():
+    poly1 = Polygon(x_values=[0, 0, 5, 5, 0],
+                    y_values=[0, 5, 5, 0, 0])
+    poly2 = Polygon(x_values=[2.5, 2.5, 5, 5, 2.5],
+                    y_values=[0, 5, 5, 0, 0])
     assert poly1.overlap(poly2.shape) == 0.5
     assert poly2.overlap(poly1.shape) == 1.0
     assert poly1.overlap(poly2.shape, 0.6) == 0.0
@@ -67,6 +62,4 @@ def test_population_ctrl_idx_error(ctrl_idx, err):
     with pytest.raises(AssertionError) as exp:
         x.set_ctrl_index(**{ctrl_idx[0]: ctrl_idx[1]})
     assert str(exp.value) == err
-
-
 
