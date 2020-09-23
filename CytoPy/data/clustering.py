f7e5b8dde779454a116c1a941761eb354fa25ecb
burtonrj <burtonrj@cardiff.ac.uk>
2020-09-23 16:07:40 +0100

    Class for meta clustering definition

--- CytoPy/data/clustering.py
+++ CytoPy/data/clustering.py
@@ -1,4 +1,3 @@
-from .experiments import Experiment
 import mongoengine
 
 
@@ -7,13 +6,30 @@ class ClusteringDefinition(mongoengine.Document):
     features = mongoengine.ListField(required=True)
     transform_method = mongoengine.StringField(required=False, default="logicle")
     root_population = mongoengine.StringField(required=True, default="root")
-    experiment = mongoengine.ReferenceField(Experiment, reverse_delete_rule=mongoengine.CASCADE)
     method = mongoengine.StringField(required=True, choices=["PhenoGraph", "FlowSOM"])
     kwargs = mongoengine.DictField()
     prefix = mongoengine.StringField(default="cluster")
 
     meta = {
         "db_alias": "core",
-        "collection": "clustering_experiments"
+        "collection": "clustering_definitions"
     }
+
+
+class MetaClusteringDefinition:
+    name = mongoengine.StringField(required=True, unique=True)
+    target = mongoengine.StringField(required=True)
+    transform_method = mongoengine.StringField(required=False, default="logicle")
+    norm_method = mongoengine.StringField(required=True, default="norm", choices=[None, "norm", "standardise"])
+    summary_method = mongoengine.StringField(required=True, default="median", choices=["mean", "median"])
+    cluster_method = mongoengine.StringField(required=True, default="PhenoGraph", choices=["PhenoGraph", "FlowSOM",
+                                                                                           "ConsensusClustering", "Agglomerative"])
+    kwargs = mongoengine.DictField()
+    prefix = mongoengine.StringField(default="meta")
+
+    meta = {
+        "db_alias": "core",
+        "collection": "meta_clustering_definitions"
+    }
+
 
