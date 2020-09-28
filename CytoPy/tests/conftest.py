3dabe277659f57aba22fab815f4386b9eaefa324
burtonrj <burtonrj@cardiff.ac.uk>
2020-09-28 18:30:17 +0100

    clean up tests for conftest and tests passed for filegroup load

--- CytoPy/tests/conftest.py
+++ CytoPy/tests/conftest.py
@@ -1,17 +1,23 @@
-from mongoengine.connection import connect, disconnect
-import numpy as np
+from mongoengine.connection import connect, disconnect, _get_db
 import pytest
-import h5py
+import shutil
 import sys
 import os
 
 
 @pytest.fixture(scope='session', autouse=True)
-def test_setup():
+def setup():
     sys.path.append("/home/rossco/CytoPy")
     os.mkdir(f"{os.getcwd()}/test_data")
     connect("test", host="mongomock://localhost", alias="core")
     yield
-    os.rmdir(f"{os.getcwd()}/test_data")
+    shutil.rmtree(f"{os.getcwd()}/test_data", ignore_errors=True)
     disconnect(alias="core")
 
+
+@pytest.fixture(autouse=True)
+def clean_up():
+    for x in os.listdir(f"{os.getcwd()}/test_data"):
+        os.remove(f"{os.getcwd()}/test_data/{x}")
+    db = _get_db("core")
+    db.drop_collection('fcs_files')
--- CytoPy/tests/test_fcs.py
+++ CytoPy/tests/test_fcs.py
@@ -17,6 +17,14 @@ def create_example_data(file_id: str):
         f.create_group("clusters")
         f.create_group("clusters/test_pop")
         f.create_dataset("clusters/test_pop/cluster1", data=np.array([1, 2, 3, 4, 5]))
+    for x in ["primary", "ctrl1", "ctrl2"]:
+        pd.DataFrame(np.random.rand(100, 5)).to_hdf(f"{os.getcwd()}/test_data/{file_id}.hdf5", key=x)
+
+
+def create_example_filegroup():
+    x = FileGroup(primary_id="test", data_directory=f"{os.getcwd()}/test_data", controls=["ctrl1", "ctrl2"])
+    x.channel_mappings = [ChannelMap(marker=f"Marker{i+1}", channel=f"Channel{i+1}") for i in range(5)]
+    return x
 
 
 def delete_example_data(file_id: str):
@@ -43,14 +51,14 @@ def test_filegroup_init():
 
 
 def test_filegroup_load_populations_error():
-    x = FileGroup(primary_id="test", data_directory=f"{os.getcwd()}/test_data")
+    x = create_example_filegroup()
     with pytest.raises(AssertionError) as exp:
         x._load_populations()
     assert str(exp.value) == f"Could not locate FileGroup HDF5 record {x.h5path}"
 
 
 def test_filegroup_load_populations():
-    x = FileGroup(primary_id="test", data_directory=f"{os.getcwd()}/test_data")
+    x = create_example_filegroup()
     create_example_data(x.id)
     pop = Population(population_name="test_pop")
     pop.clusters.append(Cluster(cluster_id="cluster1"))
@@ -62,3 +70,66 @@ def test_filegroup_load_populations():
     assert np.array_equal(x.populations[0].ctrl_index.get("ctrl1"), np.array([1, 2, 3, 4, 5]))
     assert np.array_equal(x.populations[0].clusters[0].index, np.array([1, 2, 3, 4, 5]))
     delete_example_data(x.id)
+
+
+@pytest.mark.parametrize("sample_size,include_ctrls,columns,keys",
+                         [(None, False, "marker", ["primary"]),
+                          (50, False, "marker", ["primary"]),
+                          (50, True, "marker", ["primary", "controls"]),
+                          (80, True, "channel", ["primary", "controls"])])
+def test_filegroup_load(sample_size, include_ctrls, columns, keys):
+    x = create_example_filegroup()
+    create_example_data(x.id)
+    df = x.load(sample_size=sample_size,
+                include_controls=include_ctrls,
+                columns=columns)
+    sample_size = sample_size or 100
+    assert isinstance(df, dict)
+    assert all(x in df.keys() for x in keys)
+    assert df["primary"].columns.tolist() == [f"{columns.capitalize()}{i+1}" for i in range(5)]
+    assert df["primary"].shape[0] == sample_size
+    if "controls" in df.keys():
+        assert all(x in df["controls"].keys() for x in ["ctrl1", "ctrl2"])
+        assert df["controls"]["ctrl1"].shape[0] == sample_size
+    delete_example_data(x.id)
+
+
+def test_add_file():
+    pass
+
+
+def test_valid_mappings():
+    pass
+
+
+def test_delete_pop():
+    pass
+
+
+def test_update_pop():
+    pass
+
+
+def test_get_pop():
+    pass
+
+
+def test_get_pop_by_parent():
+    pass
+
+
+def test_write_pop():
+    pass
+
+
+def test_create_pop_grps():
+    pass
+
+
+def test_reset_pop_data():
+    pass
+
+
+def test_delete():
+    pass
+
