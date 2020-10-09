477dab01ab75aa706841333cdf116b1bbab8088e
Ross Burton <ross@pop-os.localdomain>
2020-10-09 16:10:56 +0100

    Fixed minor bugs in fcs.py; now passing

--- CytoPy/tests/conftest.py
+++ CytoPy/tests/conftest.py
@@ -7,7 +7,7 @@ import os
 
 @pytest.fixture(scope='session', autouse=True)
 def setup():
-    sys.path.append("/home/rossco/CytoPy")
+    sys.path.append("/home/ross/CytoPy")
     os.mkdir(f"{os.getcwd()}/test_data")
     connect("test", host="mongomock://localhost", alias="core")
     yield
