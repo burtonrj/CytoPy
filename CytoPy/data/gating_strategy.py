270dce50f5cb4d8ef541b4f1d16b432c4f736483
Ross Burton <burtonrj@cardiff.ac.uk>
2020-10-31 10:14:56 +0000

    Header updated for gating_strategy

--- CytoPy/data/gating_strategy.py
+++ CytoPy/data/gating_strategy.py
@@ -1,3 +1,33 @@
+#!/usr/bin.env/python
+# -*- coding: utf-8 -*-
+"""
+In a traditional analysis, an immunologist would apply a 'gating strategy';
+a series of 'gates' that separate single cell data into the populations of
+interest. CytoPy provides autonomous gates (see CytoPy.data.gate) to
+emulate this process and these gates can be packaged together for bulk
+analysis using the GatingStrategy class, housed within this module.
+
+Copyright 2020 Ross Burton
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 from ..flow.plotting import CreatePlot
 from ..feedback import progress_bar, vprint
 from .gate import Gate, ThresholdGate, PolygonGate, EllipseGate, ThresholdGeom, \
@@ -6,6 +36,15 @@ from .experiment import Experiment
 from .fcs import FileGroup
 from datetime import datetime
 import mongoengine
+
+__author__ = "Ross Burton"
+__copyright__ = "Copyright 2020, CytoPy"
+__credits__ = ["Ross Burton", "Simone Cuff", "Andreas Artemiou", "Matthias Eberl"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Ross Burton"
+__email__ = "burtonrj@cardiff.ac.uk"
+__status__ = "Production"
 
 
 class Action(mongoengine.EmbeddedDocument):
