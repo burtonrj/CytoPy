fe918ace5d1537755f7471ef98ceebc87bf980a1
Ross Burton <burtonrj@cardiff.ac.uk>
2020-10-31 11:16:56 +0000

    Header updated for read_write

--- CytoPy/data/read_write.py
+++ CytoPy/data/read_write.py
@@ -1,3 +1,35 @@
+#!/usr/bin.env/python
+# -*- coding: utf-8 -*-
+"""
+The read_write module contains tools for accessing *.fcs files and
+relies on the Python library FlowIO by Scott White. This is used by
+Experiment to population FileGroups.
+
+Projects also house the subjects (represented by the Subject class;
+see CytoPy.data.subject) of an analysis which can contain multiple
+meta-data.
+
+Copyright 2020 Ross Burton
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 from multiprocessing import Pool, cpu_count
 import flowio
 import dateutil.parser as date_parser
@@ -5,6 +37,15 @@ import numpy as np
 import pandas as pd
 import json
 import os
+
+__author__ = "Ross Burton"
+__copyright__ = "Copyright 2020, CytoPy"
+__credits__ = ["Ross Burton", "Scott White", "Simone Cuff", "Andreas Artemiou", "Matthias Eberl"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Ross Burton"
+__email__ = "burtonrj@cardiff.ac.uk"
+__status__ = "Production"
 
 
 def filter_fcs_files(fcs_dir: str,
