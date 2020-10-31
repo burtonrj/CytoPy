0e5c4c993e1a8c686002c504ddc8a90513be1871
Ross Burton <burtonrj@cardiff.ac.uk>
2020-10-31 11:10:52 +0000

    Header updated for population

--- CytoPy/data/project.py
+++ CytoPy/data/project.py
@@ -1,8 +1,51 @@
+#!/usr/bin.env/python
+# -*- coding: utf-8 -*-
+"""
+Every analysis is controlled using the Project class, the highest
+structure in the hierarchy of documents in the central MongoDB database.
+You can create multiple experiments for a Project, each attaining to
+a different staining panel. Experiments are accessed and managed through
+the Project class.
+
+Projects also house the subjects (represented by the Subject class;
+see CytoPy.data.subject) of an analysis which can contain multiple
+meta-data.
+
+Copyright 2020 Ross Burton
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 from .experiment import Experiment
 from .subject import Subject
 from typing import Generator
 import mongoengine
 import datetime
+
+__author__ = "Ross Burton"
+__copyright__ = "Copyright 2020, CytoPy"
+__credits__ = ["Ross Burton", "Simone Cuff", "Andreas Artemiou", "Matthias Eberl"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Ross Burton"
+__email__ = "burtonrj@cardiff.ac.uk"
+__status__ = "Production"
 
 
 class Project(mongoengine.Document):
