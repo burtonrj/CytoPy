47f6294f17936ce34851722a52ba3f74e0d75bd5
Ross Burton <burtonrj@cardiff.ac.uk>
2020-10-31 10:53:27 +0000

    Header updated for population

--- CytoPy/data/population.py
+++ CytoPy/data/population.py
@@ -1,3 +1,36 @@
+#!/usr/bin.env/python
+# -*- coding: utf-8 -*-
+"""
+When analysing single cell data we are ultimately interested in populations
+of cells. This module contains the Population class, which controls the
+data attaining to a single cell population. A FileGroup (see CytoPy.data.fcs)
+can contain many Populations (which are embedded within the FileGroup).
+These Populations can also contain many Clusters, generated from a
+high-dimensional clustering algorithm applied to a population of cells
+e.g. FlowSOM or Phenograph. The cluster results are embedded within
+the Population as a collection of Clusters.
+
+Copyright 2020 Ross Burton
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 from ..flow.transforms import scaler
 from .geometry import PopulationGeometry, ThresholdGeom, PolygonGeom
 from functools import reduce
@@ -7,6 +40,15 @@ from _warnings import warn
 import numpy as np
 import pandas as pd
 import mongoengine
+
+__author__ = "Ross Burton"
+__copyright__ = "Copyright 2020, CytoPy"
+__credits__ = ["Ross Burton", "Simone Cuff", "Andreas Artemiou", "Matthias Eberl"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Ross Burton"
+__email__ = "burtonrj@cardiff.ac.uk"
+__status__ = "Production"
 
 
 class Cluster(mongoengine.EmbeddedDocument):
