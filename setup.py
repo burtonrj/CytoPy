f4a05fa5c29418b10d6c5b6a570d837b46719ebb
burtonrj <burtonrj@cardiff.ac.uk>
2020-11-06 17:10:07 +0000

    Updating setup and docs

--- setup.py
+++ setup.py
@@ -5,20 +5,22 @@ try:
     import cython
 except ModuleNotFoundError:
     raise RuntimeError('CytoPy requires that Cython>=0.27 be installed.')
-if sys.version_info.major >= 3.6:
-    raise RuntimeError('CytoPy requires Python version >= 3.6')
+if sys.version_info.major >= 3.7:
+    raise RuntimeError('CytoPy requires Python version >= 3.7')
 setup(
     name='CytoPy',
-    version='0.0.1',
-    packages=['CytoPy', 'CytoPy.flow', 'CytoPy.flow.gating', 'CytoPy.flow.clustering', 'CytoPy.data',
-              'CytoPy.flow.supervised', 'CytoPy.tests', 'CytoPy.tests.data',
-              'CytoPy.tests.test_clustering', 'CytoPy.tests.test_data', 'CytoPy.tests.test_flow'],
+    version='1.0.0',
+    packages=['CytoPy',
+              'CytoPy.flow',
+              'CytoPy.flow.clustering', 
+              'CytoPy.data',
+              'CytoPy.tests',
+              'CytoPy.tests/assets'],
     url='https://github.com/burtonrj/CytoPy',
     license='MIT',
     author='Ross Burton',
     author_email='burtonrj@cardiff.ac.uk',
     description='Python framework for data-centric autonomous cytometry analysis',
     install_requires=open("requirements.txt").read(),
-    dependency_links=['https://github.com/jacoblevine/PhenoGraph.git#egg=PhenoGraph',
-                      'https://github.com/burtonrj/FlowUtilsPandas.git#egg=flowutilspd']
+    dependency_links=['https://github.com/jacoblevine/PhenoGraph.git#egg=PhenoGraph']
 )
