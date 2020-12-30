d4bc61aa215fd6e2e4707bdcea9cbf2aff6ae351
burtonrj <burtonrj@cardiff.ac.uk>
2020-12-30 20:08:25 +0000

    Migrated AssayTools functionality to lmfit objects

--- CytoPy/tests/test_ext_tools.py
+++ CytoPy/tests/test_ext_tools.py
@@ -1,11 +1,10 @@
 from CytoPy.tests import assets
 from CytoPy.flow import ext_tools
+from lmfit.model import ModelResult
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import pytest
-
-FUNCTIONS = ["linear", "poly2", "poly3", "4pl", "5pl"]
 
 
 @pytest.fixture()
@@ -79,34 +78,44 @@ def test_invalid_conc(load_example_data):
     assert str(err.value), "One or more of the specified standards missing from concentrations dataframe"
 
 
-@pytest.mark.parametrize("func,transform",
-                         [(f, "log10") for f in FUNCTIONS] + [(f, None) for f in FUNCTIONS])
-def test_fit_one_analyte(load_example_data, func, transform):
+@pytest.mark.parametrize("model,transform",
+                         [("linear", "log10"),
+                          ("poly", "log10"),
+                          ("quad", "log10"),
+                          ("logit", "log10"),
+                          ("linear", None),
+                          ("poly", None),
+                          ("quad", None),
+                          ("logit", None)])
+def test_fit_one_analyte(load_example_data, model, transform):
     example = ext_tools.AssayTools(data=load_example_data.get("data"),
                                    conc=load_example_data.get("conc"),
                                    background_id="Background0",
                                    standards=[f"Standard{i + 1}" for i in range(6)][::-1])
-    example.fit(func=func,
+    example.fit(model=model,
                 transform=transform,
                 analyte="CXCL10")
     assert "CXCL10" in example.standard_curves.keys()
     assert isinstance(example.standard_curves.get("CXCL10"), dict)
-    assert isinstance(example.standard_curves.get("CXCL10").get("params"), np.ndarray)
     assert example.standard_curves.get("CXCL10").get("transform") == transform
-    assert hasattr(example.standard_curves.get("CXCL10").get("function"), '__call__')
-    assert isinstance(example.standard_curves.get("CXCL10").get("residuals"), np.ndarray)
-    assert isinstance(example.standard_curves.get("CXCL10").get("sigma"), np.ndarray)
-    assert isinstance(example.standard_curves.get("CXCL10").get("r_squared"), float)
+    assert isinstance(example.standard_curves.get("CXCL10").get("model_result"), ModelResult)
 
 
-@pytest.mark.parametrize("func,transform",
-                         [(f, "log10") for f in FUNCTIONS] + [(f, None) for f in FUNCTIONS])
-def test_fit_all(load_example_data, func, transform):
+@pytest.mark.parametrize("model,transform",
+                         [("linear", "log10"),
+                          ("poly", "log10"),
+                          ("quad", "log10"),
+                          ("logit", "log10"),
+                          ("linear", None),
+                          ("poly", None),
+                          ("quad", None),
+                          ("logit", None)])
+def test_fit_all(load_example_data, model, transform):
     example = ext_tools.AssayTools(data=load_example_data.get("data"),
                                    conc=load_example_data.get("conc"),
                                    background_id="Background0",
                                    standards=[f"Standard{i + 1}" for i in range(6)][::-1])
-    example.fit(func=func,
+    example.fit(model=model,
                 transform=transform,
                 analyte=None)
     assert all([x in example.standard_curves.keys() for x in example.analytes])
@@ -120,20 +129,27 @@ def test_assert_fitted(load_example_data):
     with pytest.raises(AssertionError) as err:
         example.predict(analyte="CXCL10")
     assert str(err.value) == "Standard curves have not been computed; call 'fit' prior to additional functions"
-    example.fit(func="linear", analyte="CXCL10")
+    example.fit(model="linear", analyte="CXCL10")
     with pytest.raises(AssertionError) as err:
         example.predict(analyte="beta-NGF")
     assert str(err.value) == "Standard curve not detected for beta-NGF; call 'fit' prior to additional functions"
 
 
-@pytest.mark.parametrize("func,transform,analyte",
-                         [(f, "log10", "CXCL10") for f in FUNCTIONS] + [(f, None, "CXCL10") for f in FUNCTIONS])
-def test_fit_predict_one_analyte(load_example_data, func, transform, analyte):
+@pytest.mark.parametrize("model,transform",
+                         [("linear", "log10"),
+                          ("poly", "log10"),
+                          ("quad", "log10"),
+                          ("logit", "log10"),
+                          ("linear", None),
+                          ("poly", None),
+                          ("quad", None),
+                          ("logit", None)])
+def test_fit_predict_one_analyte(load_example_data, model, transform):
     example = ext_tools.AssayTools(data=load_example_data.get("data"),
                                    conc=load_example_data.get("conc"),
                                    background_id="Background0",
                                    standards=[f"Standard{i + 1}" for i in range(6)][::-1])
-    example.fit_predict(func=func, transform=transform, analyte=analyte)
+    example.fit_predict(model=model, transform=transform, analyte="CXCL10")
     assert "CXCL10" in example.predictions.columns
     assert not example.predictions["CXCL10"].isna().any()
 
@@ -143,6 +159,50 @@ def test_plot_standard_curve(load_example_data):
                                    conc=load_example_data.get("conc"),
                                    background_id="Background0",
                                    standards=[f"Standard{i + 1}" for i in range(6)][::-1])
-    example.fit_predict(func="4pl", transform="log10", analyte="CXCL10")
+    example.fit_predict(model="logit", transform="log10", analyte="CXCL10")
     example.plot_standard_curve(analyte="CXCL10")
     plt.show()
+
+
+def test_plot_repeat_measures(load_example_data):
+    example = ext_tools.AssayTools(data=load_example_data.get("data"),
+                                   conc=load_example_data.get("conc"),
+                                   background_id="Background0",
+                                   standards=[f"Standard{i + 1}" for i in range(6)][::-1])
+    example.fit_predict(model="logit", transform="log10", analyte="CXCL10")
+    example.plot_repeat_measures(analyte="CXCL10")
+    plt.show()
+
+
+@pytest.mark.parametrize("linear_scale", [True, False])
+def test_cv(load_example_data, linear_scale):
+    example = ext_tools.AssayTools(data=load_example_data.get("data"),
+                                   conc=load_example_data.get("conc"),
+                                   background_id="Background0",
+                                   standards=[f"Standard{i + 1}" for i in range(6)][::-1])
+    example.fit_predict(model="logit", transform="log10", analyte="CXCL10")
+    x = example.coef_var(analyte="CXCL10", linear_scale=linear_scale)
+    assert isinstance(x, pd.DataFrame)
+
+
+def test_plot_shift(load_example_data):
+    example = ext_tools.AssayTools(data=load_example_data.get("data"),
+                                   conc=load_example_data.get("conc"),
+                                   background_id="Background0",
+                                   standards=[f"Standard{i + 1}" for i in range(6)][::-1])
+    example.fit_predict(model="logit", transform="log10", analyte="CXCL10")
+    example.raw["RandomMeta"] = np.random.randint(0, 2, size=example.raw.shape[0])
+    example.plot_shift(analyte="CXCL10", factor="RandomMeta")
+    plt.show()
+
+
+def test_corr_matrix(load_example_data):
+    example = ext_tools.AssayTools(data=load_example_data.get("data"),
+                                   conc=load_example_data.get("conc"),
+                                   background_id="Background0",
+                                   standards=[f"Standard{i + 1}" for i in range(6)][::-1])
+    example.fit_predict(model="logit", transform="log10", analyte="CXCL10")
+    example.fit_predict(model="logit", transform="log10", analyte="beta-NGF")
+    example.corr_matrix()
+    plt.show()
+
