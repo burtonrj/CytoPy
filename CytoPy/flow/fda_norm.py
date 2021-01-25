5a21dc71d140d05c35e0e1edac59bfa124b9ab8c
burtonrj <burtonrj@cardiff.ac.uk>
2021-01-25 21:26:12 +0000

    Landmark registration functionality complete

--- CytoPy/flow/fda_norm.py
+++ CytoPy/flow/fda_norm.py
@@ -1,10 +1,8 @@
-from skfda.preprocessing.registration import landmark_registration
+from skfda.preprocessing.registration import landmark_registration_warping, landmark_shift_deltas
 from skfda.representation.grid import FDataGrid
-from multiprocessing import Pool, cpu_count
-from functools import partial
-from ..data.fcs import FileGroup
 from detecta import detect_peaks
 from KDEpy import FFTKDE
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
@@ -68,6 +66,8 @@ def update_landmarks(landmarks: list):
     List
     """
     l1, l2 = landmarks
+    if len(l1) == len(l2):
+        return [l1, l2]
     if len(l1) < len(l2):
         u = match_landmarks(l1, l2)
         return [l1, u]
@@ -75,96 +75,96 @@ def update_landmarks(landmarks: list):
     return [u, l2]
 
 
-def find_nearest_x(value, y, grid):
-    return grid[(np.abs(y - value)).argmin()]
-
-
-def estimate_new_x(data: np.ndarray,
-                   before: FDataGrid,
-                   after: FDataGrid,
-                   n_jobs: int = -1):
-    if n_jobs <= 0:
-        n_jobs = cpu_count()
-    before = before.evaluate(data)[0].reshape(-1)
-    y_after = after.data_matrix[0].reshape(-1)
-    find_func = partial(find_nearest_x, y=y_after, grid=after.grid_points[0])
-    with Pool(n_jobs) as pool:
-        return np.array(list(pool.map(find_func, before)))
-
-
-def align_data(data: pd.DataFrame,
-               ref_data: pd.DataFrame,
-               dims: list,
-               n_jobs: int = -1,
-               **kwargs):
+def estimate_pdfs(target: pd.DataFrame,
+                  ref: pd.DataFrame,
+                  var: str):
     """
-    Given some new data and reference data to align to, estimate the
-    probability density function of each dimension (dims) within each
-    of these dataframes. A peak finding algorithm (detecta.detect_peaks)
-    is used to identify "landmarks" which are used to align each
-    dimension in data with the equivalent in ref_data using landmark
-    registration.
+    Given some target and reference DataFrame, estimate PDF for each using convolution based
+    kernel density estimation (see KDEpy). 'var' is the variable of interest and should be a
+    column in both ref and target
 
     Parameters
     ----------
-    data: Pandas.DataFrame
-    ref_data: Pandas.DataFrame
-    dims: List
-    kwargs:
-        Additional keyword arguments passed to detecta.detect_peaks
+    target: Pandas.DataFrame
+    ref: Pandas.DataFrame
+    var: str
 
     Returns
     -------
-
+    (Numpy.Array, Numpy.Array, Numpy.Array)
+        Target PDF, reference PDF, and grid space
     """
-    mph = kwargs.pop("mph", lambda y: 0.001 * np.max(y))
-    data = data.copy()
-    for d in dims:
-        x = np.linspace(np.min([data[d].min(), ref_data[d].min()]) - 0.01,
-                        np.max([data[d].max(), ref_data[d].max()]) + 0.01,
+    min_ = np.min([target[var].min(), ref[var].min()])
+    max_ = np.max([target[var].max(), ref[var].max()])
+    x = np.linspace(min_ - 0.1,
+                    max_ + 0.1,
+                    100000)
+    y1 = (FFTKDE(kernel="gaussian",
+                 bw="silverman")
+          .fit(target[var].values)
+          .evaluate(x))
+    y2 = (FFTKDE(kernel="gaussian",
+                 bw="silverman")
+          .fit(ref[var].values)
+          .evaluate(x))
+    return y1, y2, x
+
+
+class LandmarkReg:
+    def __init__(self,
+                 target: pd.DataFrame,
+                 ref: pd.DataFrame,
+                 var: str,
+                 mpt: float = 0.001,
+                 **kwargs):
+        y1, y2, x = estimate_pdfs(target, ref, var)
+        self.landmarks = update_landmarks([peaks(y, x, mph=mpt * y.max(), **kwargs) for y in [y1, y2]])
+        self.original_functions = FDataGrid([y1, y2], grid_points=x)
+        self.warping_function = None
+        self.adjusted_functions = None
+        self.landmark_shift_deltas = None
+
+    def __call__(self):
+        self.warping_function = landmark_registration_warping(self.original_functions,
+                                                              self.landmarks,
+                                                              location=np.mean(self.landmarks, axis=0))
+        self.adjusted_functions = self.original_functions.compose(self.warping_function)
+        self.landmark_shift_deltas = landmark_shift_deltas(self.original_functions, self.landmarks)
+        return self
+
+    def plot_warping(self, ax: list or None = None):
+        assert self.warping_function is not None, "Call object prior to plot"
+        ax = ax or plt.subplots(1, 3, figsize=(15, 4))[1]
+        assert len(ax) == 3, "Must provide exactly 3 axis objects"
+        self.original_functions.plot(axes=ax[0])
+        ax[0].set_title("Before")
+        self.warping_function.plot(axes=ax[1])
+        ax[1].set_title("Warping function")
+        self.adjusted_functions.plot(axes=ax[2])
+        ax[2].set_title("After")
+        ax[0].legend(labels=["Target", "Reference"])
+        return ax
+
+    def shift_data(self,
+                   x: np.ndarray):
+        return self.warping_function.evaluate(x)[1].reshape(-1)
+
+    def plot_shift(self,
+                   x: np.ndarray,
+                   ax: plt.Axes or None = None):
+        ax = ax or plt.subplots(figsize=(5, 5))[1]
+        shifted = self.shift_data(x)
+        x = np.linspace(np.min(x) - 0.1,
+                        np.max(x) + 0.1,
                         10000)
-        y1 = (FFTKDE(kernel="gaussian",
-                     bw="silverman")
-              .fit(data[d].values)
-              .evaluate(x))
         y2 = (FFTKDE(kernel="gaussian",
                      bw="silverman")
-              .fit(ref_data[d].values)
+              .fit(shifted)
               .evaluate(x))
-        landmarks = update_landmarks([peaks(y, x, mph=mph(y), **kwargs) for y in [y1, y2]])
-        fdata = FDataGrid([y1, y2], grid_points=x)
-        shifted = landmark_registration(fdata, landmarks)
-        data[d] = estimate_new_x(data=data[d].values,
-                                 before=fdata,
-                                 after=shifted,
-                                 n_jobs=n_jobs)
-    return data
 
-
-def _load_dataframes(target: FileGroup,
-                     ref: FileGroup,
-                     population: str,
-                     transform: dict or str or None = None,
-                     ctrl: str or None = None):
-    if ctrl is not None:
-        return (target.load_ctrl_population_df(ctrl=ctrl,
-                                               population=population,
-                                               transform=transform),
-                ref.load_ctrl_population_df(ctrl=ctrl,
-                                            population=population,
-                                            transform=transform))
-    return (target.load_population_df(population=population,
-                                      transform=transform),
-            ref.load_population_df(population=population,
-                                   transform=transform))
-
-
-def normalise_data(target: FileGroup,
-                   ref: FileGroup,
-                   dims: list,
-                   population: str,
-                   transform: dict or str or None = "logicle",
-                   ctrl: str or None = None,
-                   **kwargs):
-    target, ref = _load_dataframes(target, ref, population, transform, ctrl)
-    return align_data(target, ref, dims, **kwargs)
+        self.original_functions.plot(axes=ax)
+        ax.plot(x, y2)
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        ax.legend(labels=["Before", "Ref", "After"])
+        return ax
