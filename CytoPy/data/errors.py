66e7c72c9c8338acaddb0e0dd98e07e816de40c5
Ross Burton <burtonrj@cardiff.ac.uk>
2021-02-24 16:14:22 +0000

    Updated project docs

---
+++ CytoPy/data/errors.py
@@ -0,0 +1,18 @@
+class MissingExperimentError(Exception):
+    pass
+
+
+class DuplicateExperimentError(Exception):
+    pass
+
+
+class DuplicateSubjectError(Exception):
+    pass
+
+
+class InvalidDataDirectory(Exception):
+    pass
+
+
+class MissingSubjectError(Exception):
+    pass
